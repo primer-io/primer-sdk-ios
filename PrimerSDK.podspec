7b2c3c7d5e7c68b73236312cb558cd769c7cf4c4
Evangelos Pittas <evangelospittas@gmail.com>
2021-03-12 16:35:48 +0200

    [CHE-279] Restructure Pod.

--- PrimerSDK.podspec
+++ PrimerSDK.podspec
@@ -1,20 +1,28 @@
 Pod::Spec.new do |spec|
-  spec.name         = "PrimerSDK"
-  spec.version      = "1.2.2"
-  spec.summary      = "Official iOS SDK for Primer"
-  spec.description  = <<-DESC
-  This library contains the official iOS SDK for Primer. Install this Cocoapod to seemlessly integrate the Primer Checkout & API platform in your app.
-                   DESC
-  spec.homepage     = "https://www.primer.io"
-  spec.license      = { :type => "MIT", :file => "LICENSE" }
-  spec.author       = { "Primer" => "carl@primer.io" }
-  spec.platform     = :ios, "10.0"
-  spec.source       = { :git => "https://github.com/primer-io/primer-sdk-ios.git", :tag => "#{spec.version}" }
-  spec.source_files  = "PrimerSDK/**/*.{swift}"
-  spec.resource_bundles = {
-    'PrimerSDK' => ['PrimerSDK/**/*.xcassets']
-  }
-  spec.swift_version = "4.2"
-  spec.framework  = "UIKit"
-  # spec.dependency 'CardScan'
+    spec.name         = "PrimerSDK"
+    spec.version      = "1.2.2"
+    spec.summary      = "Official iOS SDK for Primer"
+    spec.description  = <<-DESC
+    This library contains the official iOS SDK for Primer. Install this Cocoapod to seemlessly integrate the Primer Checkout & API platform in your app.
+    DESC
+    spec.homepage     = "https://www.primer.io"
+    spec.license      = { :type => "MIT", :file => "LICENSE" }
+    spec.author       = { "Primer" => "carl@primer.io" }
+    spec.source       = { :git => "https://github.com/primer-io/primer-sdk-ios.git", :tag => "#{spec.version}" }
+    # spec.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+    
+    spec.ios.deployment_target = '10.0'
+    
+    spec.source_files = 'PrimerSDK/Classes/**/*'
+    
+    spec.swift_version = "4.2"
+    
+    spec.resource_bundles = {
+        'PrimerSDK' => ['PrimerSDK/Assets/*.xcassets']
+    }
+    
+    # spec.public_header_files = 'Pod/Classes/**/*.h'
+    # spec.frameworks = 'UIKit', 'MapKit'
+    # spec.dependency 'AFNetworking', '~> 2.3'
+    
 end
---
+++ PrimerSDK/Classes/Core/DataModels/Enums/PrimerSessionFlow.swift
@@ -0,0 +1,94 @@
+//
+//  SessionType.swift
+//  PrimerSDK
+//
+//  Created by Carl Eriksson on 13/01/2021.
+//
+
+/**
+ Enum that contains possible values for the drop-in UI flow.
+ 
+ *Values*
+ 
+ `default`: Cannot be added in vault and uses the checkout flow.
+ 
+ `defaultWithVault`: Can be added in vault and uses the vault flow.
+ 
+ `completeDirectCheckout`: Cannot be added in vault and uses the checkout flow.
+ 
+ `addPayPalToVault`: Can be added in vault and uses the vault flow.
+ 
+ `addCardToVault`: Can be added in vault and uses the vault flow.
+ 
+ `addDirectDebitToVault`: Can be added in vault and uses the vault flow.
+ 
+ `addKlarnaToVault`: Can be added in vault and uses the vault flow.
+ 
+ `addDirectDebit`: Can be added in vault and uses the vault flow.
+ 
+ `checkoutWithKlarna`: Can be added in vault and uses the checkout flow.
+ 
+ - Author:
+ Primer
+ - Version:
+ 1.2.2
+ */
+
+public enum PrimerSessionFlow {
+    
+    case `default`
+    case defaultWithVault
+    case completeDirectCheckout
+    case addPayPalToVault
+    case addCardToVault
+    case addDirectDebitToVault
+    case addKlarnaToVault
+    case addDirectDebit
+    case checkoutWithKlarna
+    
+    var vaulted: Bool {
+        switch self {
+        case .addCardToVault:
+            return true
+        case .addPayPalToVault:
+            return true
+        case .default:
+            return false
+        case .addDirectDebit:
+            return true
+        case .completeDirectCheckout:
+            return false
+        case .checkoutWithKlarna:
+            return true
+        case .addDirectDebitToVault:
+            return true
+        case .addKlarnaToVault:
+            return true
+        case .defaultWithVault:
+            return true
+        }
+    }
+    
+    var uxMode: UXMode {
+        switch self {
+        case .addCardToVault:
+            return .VAULT
+        case .addPayPalToVault:
+            return .VAULT
+        case .default:
+            return .CHECKOUT
+        case .addDirectDebit:
+            return .VAULT
+        case .completeDirectCheckout:
+            return .CHECKOUT
+        case .checkoutWithKlarna:
+            return .CHECKOUT
+        case .addDirectDebitToVault:
+            return .VAULT
+        case .addKlarnaToVault:
+            return .VAULT
+        case .defaultWithVault:
+            return .VAULT
+        }
+    }
+}
