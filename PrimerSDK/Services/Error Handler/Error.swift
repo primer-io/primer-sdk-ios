358d6e848cfa7c9bc3ab1ceedc444fabca9cfd1c
Evangelos Pittas <evangelospittas@gmail.com>
2021-03-16 15:29:14 +0200

    [CHE-280] Gather error enums. Define primer error response structure and serialize it.

--- PrimerSDK/Services/Error Handler/Error.swift
+++ PrimerSDK/Services/Error Handler/Error.swift
@@ -38,9 +38,44 @@ enum KlarnaException: Error {
 
 enum NetworkServiceError: Error {
     case invalidURL
-    case unauthorised
-    case statusError(_ statusCode: Int, data: Data?)
+    case unauthorised(_ info: PrimerErrorResponse?)
+    case clientError(_ statusCode: Int, info: PrimerErrorResponse?)
+    case serverError(_ statusCode: Int, info: PrimerErrorResponse?)
     case noData
     case parsing(_ error: Error, _ data: Data)
     case underlyingError(_ error: Error)            // Use this error when we have received an error JSON from the backend.
+}
+
+enum PrimerError: String, Error {
+    case ClientTokenNull = "Client token is missing."
+    case CustomerIDNull = "Customer ID is missing."
+    case PayPalSessionFailed = "PayPal checkout session failed. Your account has not been charged."
+    case VaultFetchFailed = "Failed to fetch saved payment methods."
+    case VaultDeleteFailed = "Failed to delete saved payment method."
+    case VaultCreateFailed = "Failed to save payment method."
+    case DirectDebitSessionFailed = "Failed to create a direct debit mandate.\n\n Please try again."
+    case ConfigFetchFailed = "Failed to setup session."
+    case TokenizationPreRequestFailed = "Failed to complete action. Your payment method was not processed."
+    case TokenizationRequestFailed = "Connection error, your payment method was not saved. Please try again."
+}
+
+enum PrimerAPIError: String, LocalizedError {
+    case userEmailAlreadyExists = "UserEmailAlreadyExists"
+    case ClientTokenNull = "Client token is missing."
+    case CustomerIDNull = "Customer ID is missing."
+    case PayPalSessionFailed = "PayPal checkout session failed. Your account has not been charged."
+    case VaultFetchFailed = "Failed to fetch saved payment methods."
+    case VaultDeleteFailed = "Failed to delete saved payment method."
+    case VaultCreateFailed = "Failed to save payment method."
+    case DirectDebitSessionFailed = "Failed to create a direct debit mandate.\n\n Please try again."
+    case ConfigFetchFailed = "Failed to setup session."
+    case TokenizationPreRequestFailed = "Failed to complete action. Your payment method was not processed."
+    case TokenizationRequestFailed = "Connection error, your payment method was not saved. Please try again."
+}
+
+struct PrimerErrorResponse: Codable {
+    var errorId: String
+    var `description`: String
+    var diagnosticsId: String
+    var validationErrors: [String]
 }
