81ec1f09889cecb55a9d35e3b11048c809cfee66
Evangelos Pittas <evangelospittas@gmail.com>
2021-03-18 10:23:28 +0200

    [CHE-302] Modify order of functions. Mark up code. Move extensions to separate files.

--- PrimerSDK/Classes/Core/UI/Views/Form/FormView.swift
+++ PrimerSDK/Classes/Core/UI/Views/Form/FormView.swift
@@ -21,6 +21,8 @@ class FormView: UIView {
     
     @Dependency private(set) var theme: PrimerThemeProtocol
     
+    // MARK: - PROPERTIES
+    
     let navBar = UINavigationBar()
     let title = UILabel()
     let link = UILabel()
@@ -38,6 +40,14 @@ class FormView: UIView {
     
     weak var delegate: FormViewDelegate?
     
+    // MARK: - LIFE-CYCLE
+    
+    deinit {
+        log(logLevel: .debug, message: "🧨 destroyed: \(self.self)")
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -50,11 +60,7 @@ class FormView: UIView {
         
     }
     
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
-    deinit {
-        log(logLevel: .debug, message: "🧨 destroyed: \(self.self)")
-    }
+    // MARK: - VIEW CONFIGURATION
     
     func render() {
         if (textFields.isEmpty) {
@@ -101,32 +107,7 @@ class FormView: UIView {
             }
         }
     }
-}
-
-// MARK: Picker
-extension FormView: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return countries.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countries[row].country
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let textFieldColumn: Int = pickerView.tag % 10
-        let textFieldRow: Int = (pickerView.tag - textFieldColumn) / 10
-        textFields[textFieldRow][textFieldColumn].text = countries[row].rawValue
-//        textFields[textFieldRow][textFieldColumn].resignFirstResponder()
-    }
-}
-
-// MARK: Configuration
-extension FormView {
     private func configureNavbar() {
         guard let delegate = delegate else { return }
         
@@ -167,22 +148,6 @@ extension FormView {
         navBar.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
     }
     
-    @objc
-    private func back() {
-        delegate?.back()
-    }
-    
-    @objc
-    func toolbarCancelButtonTapped(_ sender: UIBarButtonItem) {
-        countryPickerTextField?.text = nil
-        countryPickerTextField?.resignFirstResponder()
-    }
-    
-    @objc
-    func toolbarSubmitButtonTapped(_ sender: UIBarButtonItem) {
-        countryPickerTextField?.resignFirstResponder()
-    }
-    
     private func configureTitle() {
         guard let delegate = delegate else { return }
         if (theme.layout.showMainTitle) {
@@ -193,7 +158,6 @@ extension FormView {
         title.textColor = theme.colorTheme.text1
     }
     
-    // Textfield
     private func configureTextField(_ model: FormTextFieldType, column: Int, row: Int, index: Int) -> Int {
         guard let delegate = delegate else { return 0 }
         let textField = PrimerTextField()
@@ -286,61 +250,6 @@ extension FormView {
         return index + 1
     }
     
-    @objc private func onTextFieldEditingDidEnd(_ sender: UITextField) {
-        validateTextField(sender)
-        validateForm()
-    }
-    
-    @objc private func onTextFieldEditingChanged(_ sender: UITextField) {
-        guard let delegate = delegate else { return }
-        guard let text = sender.text?.withoutWhiteSpace else { return }
-        let column: Int = sender.tag % 10
-        let row: Int = (sender.tag - column) / 10
-        let mask = delegate.formType.textFields[row][column].mask
-        
-        if (mask.exists) {
-            sender.text = mask?.apply(on: text.uppercased())
-        }
-        
-        validateTextField(sender, markError: false)
-        validateForm()
-    }
-    
-    private func validateTextField(_ sender: UITextField, markError: Bool = true, showValidity: Bool = true) {
-        guard let delegate = delegate else { return }
-        guard let textField = sender as? PrimerTextField else { return }
-        
-        if textField.validationIsOptional {
-            textField.renderSubViews(validationState: .default)
-            return
-        }
-        
-        guard let text = textField.text?.withoutWhiteSpace else { return }
-        
-        let column: Int = textField.tag % 10
-        let row: Int = (textField.tag - column) / 10
-        
-        if (delegate.formType.textFields.count > row) {
-            let validation = delegate.formType.textFields[row][column].validate(text)
-            validatedFields[row][column] = validation.0
-            
-            let mask = delegate.formType.textFields[row][column].mask
-            sender.text = mask?.apply(on: text.uppercased()) ?? sender.text
-            
-            if (validation.0) {
-                textField.renderSubViews(validationState: showValidity ? .valid : .default)
-            } else {
-                textField.renderSubViews(validationState: markError ? .invalid : .default)
-                textField.errorMessage.text = markError ? validation.1 : ""
-            }
-        }
-    }
-    
-    private func validateForm() {
-        let isAllValid = validatedFields.allSatisfy({ row in return row.allSatisfy { return $0 == true } })
-        button.toggleValidity(isAllValid, validColor: theme.colorTheme.tint1, defaultColor: theme.colorTheme.disabled1)
-    }
-    
     private func configureLink() {
         guard let delegate = delegate else { return }
         link.text = delegate.formType.subtitle
@@ -350,10 +259,6 @@ extension FormView {
         let tapRecogniser = UITapGestureRecognizer(target: self, action: #selector(onLinkTap))
         link.isUserInteractionEnabled = true
         link.addGestureRecognizer(tapRecogniser)
-    }
-    
-    @objc private func onLinkTap(_ sender: UITapGestureRecognizer?) {
-        delegate?.openLink()
     }
     
     private func configureButton() {
@@ -376,26 +281,6 @@ extension FormView {
         default:
             break
         }
-    }
-    
-    @objc private func onButtonPressed(_ button: UIButton) {
-        textFields.forEach { row in
-            row.forEach {
-                guard let textField = $0 as? PrimerTextField else { return }
-                let column: Int = textField.tag % 10
-                let row: Int = (textField.tag - column) / 10
-                guard let type = delegate?.formType.textFields[row][column] else { return }
-                delegate?.submit(textField.text, type: type)
-            }
-        }
-        guard let delegate = delegate else { return }
-        switch delegate.formType {
-        case .cardForm:
-            button.setBusy(theme: theme)
-        default:
-            break
-        }
-        delegate.onSubmit()
     }
     
     private func configureScannerButton() {
@@ -429,24 +314,7 @@ extension FormView {
         iconView.widthAnchor.constraint(equalToConstant: iconView.intrinsicContentSize.width * 0.75).isActive = true
     }
     
-    @objc private func showScanner() {
-        delegate?.onBottomLinkTapped()
-    }
-}
-
-// MARK: Anchoring
-extension FormView {
-//    func anchorNavbar() {
-//        navBar.translatesAutoresizingMaskIntoConstraints = false
-//
-//        if #available(iOS 13.0, *) {
-//            navBar.topAnchor.constraint(equalTo: topAnchor, constant: 6).isActive = true
-//        } else {
-//            navBar.topAnchor.constraint(equalTo: topAnchor, constant: 18).isActive = true
-//        }
-//
-//        navBar.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-//    }
+    // MARK: Anchoring
     
     func anchorContentView() {
         contentView.translatesAutoresizingMaskIntoConstraints = false
@@ -532,22 +400,147 @@ extension FormView {
         }
         
     }
-}
-
-extension String {
-    var isValidAccountNumber: Bool {
-        print(!self.isEmpty)
-        return !self.isEmpty
+    
+//    func anchorNavbar() {
+//        navBar.translatesAutoresizingMaskIntoConstraints = false
+//
+//        if #available(iOS 13.0, *) {
+//            navBar.topAnchor.constraint(equalTo: topAnchor, constant: 6).isActive = true
+//        } else {
+//            navBar.topAnchor.constraint(equalTo: topAnchor, constant: 18).isActive = true
+//        }
+//
+//        navBar.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+//    }
+    
+    // MARK: - ACTIONS
+    
+    @objc
+    private func back() {
+        delegate?.back()
     }
+    
+    @objc
+    private func onTextFieldEditingDidEnd(_ sender: UITextField) {
+        validateTextField(sender)
+        validateForm()
+    }
+    
+    @objc
+    private func onTextFieldEditingChanged(_ sender: UITextField) {
+        guard let delegate = delegate else { return }
+        guard let text = sender.text?.withoutWhiteSpace else { return }
+        let column: Int = sender.tag % 10
+        let row: Int = (sender.tag - column) / 10
+        let mask = delegate.formType.textFields[row][column].mask
+        
+        if (mask.exists) {
+            sender.text = mask?.apply(on: text.uppercased())
+        }
+        
+        validateTextField(sender, markError: false)
+        validateForm()
+    }
+    
+    @objc
+    private func onLinkTap(_ sender: UITapGestureRecognizer?) {
+        delegate?.openLink()
+    }
+    
+    @objc
+    func toolbarCancelButtonTapped(_ sender: UIBarButtonItem) {
+        countryPickerTextField?.text = nil
+        countryPickerTextField?.resignFirstResponder()
+    }
+    
+    @objc
+    func toolbarSubmitButtonTapped(_ sender: UIBarButtonItem) {
+        countryPickerTextField?.resignFirstResponder()
+    }
+    
+    private func validateTextField(_ sender: UITextField, markError: Bool = true, showValidity: Bool = true) {
+        guard let delegate = delegate else { return }
+        guard let textField = sender as? PrimerTextField else { return }
+        
+        if textField.validationIsOptional {
+            textField.renderSubViews(validationState: .default)
+            return
+        }
+        
+        guard let text = textField.text?.withoutWhiteSpace else { return }
+        
+        let column: Int = textField.tag % 10
+        let row: Int = (textField.tag - column) / 10
+        
+        if (delegate.formType.textFields.count > row) {
+            let validation = delegate.formType.textFields[row][column].validate(text)
+            validatedFields[row][column] = validation.0
+            
+            let mask = delegate.formType.textFields[row][column].mask
+            sender.text = mask?.apply(on: text.uppercased()) ?? sender.text
+            
+            if (validation.0) {
+                textField.renderSubViews(validationState: showValidity ? .valid : .default)
+            } else {
+                textField.renderSubViews(validationState: markError ? .invalid : .default)
+                textField.errorMessage.text = markError ? validation.1 : ""
+            }
+        }
+    }
+    
+    private func validateForm() {
+        let isAllValid = validatedFields.allSatisfy({ row in return row.allSatisfy { return $0 == true } })
+        button.toggleValidity(isAllValid, validColor: theme.colorTheme.tint1, defaultColor: theme.colorTheme.disabled1)
+    }
+    
+    
+    
+    @objc private func onButtonPressed(_ button: UIButton) {
+        textFields.forEach { row in
+            row.forEach {
+                guard let textField = $0 as? PrimerTextField else { return }
+                let column: Int = textField.tag % 10
+                let row: Int = (textField.tag - column) / 10
+                guard let type = delegate?.formType.textFields[row][column] else { return }
+                delegate?.submit(textField.text, type: type)
+            }
+        }
+        guard let delegate = delegate else { return }
+        switch delegate.formType {
+        case .cardForm:
+            button.setBusy(theme: theme)
+        default:
+            break
+        }
+        delegate.onSubmit()
+    }
+    
+    @objc
+    private func showScanner() {
+        delegate?.onBottomLinkTapped()
+    }
+    
 }
 
-extension UIButton {
-    func setBusy(theme: PrimerThemeProtocol) {
-        let indicator = UIActivityIndicatorView()
-        self.setTitle("", for: .normal)
-        self.addSubview(indicator)
-        indicator.pin(to: self)
-        indicator.color = theme.colorTheme.text2
-        indicator.startAnimating()
+// MARK: - PICKER VIEW DELEGATE & DATA SOURCE
+
+extension FormView: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row].country
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let textFieldColumn: Int = pickerView.tag % 10
+        let textFieldRow: Int = (pickerView.tag - textFieldColumn) / 10
+        textFields[textFieldRow][textFieldColumn].text = countries[row].rawValue
+//        textFields[textFieldRow][textFieldColumn].resignFirstResponder()
     }
 }
