334ffa4103972f0f7ec71a08ef6c014630506380
Evangelos Pittas <evangelospittas@gmail.com>
2021-04-02 15:13:46 +0300

    [CHE-377] Refactor Netcetera initialization. Fetch security warnings.

--- Sources/PrimerSDK/Classes/Core/Payment Services/VaultService.swift
+++ Sources/PrimerSDK/Classes/Core/Payment Services/VaultService.swift
@@ -1,6 +1,9 @@
-import Foundation
+
 
 #if canImport(UIKit)
+
+import Foundation
+import ThreeDS_SDK
 
 protocol VaultServiceProtocol {
     func loadVaultedPaymentMethods(_ completion: @escaping (Error?) -> Void)
@@ -25,6 +28,54 @@ class VaultService: VaultServiceProtocol {
                 self?.state.paymentMethods = paymentMethods.data
 
                 guard let paymentMethods = self?.state.paymentMethods else { return }
+                
+                if !paymentMethods.isEmpty {
+                    print(paymentMethods)
+                    if let paymentMethod = paymentMethods.first {
+                        let threeDS2Service: ThreeDS_SDK.ThreeDS2Service = ThreeDS2ServiceSDK()
+                        
+                        
+                        do {
+                            let configParameters = ConfigParameters()
+                            try configParameters.addParam(group:nil, paramName:"license-key", paramValue: Primer.netceteraLicenseKey)
+                            try threeDS2Service.initialize(configParameters, locale: nil, uiCustomization: nil)
+                            let warnings = try threeDS2Service.getWarnings()
+                            for w in warnings {
+                                print("Warning \(w.getID()) [\(w.getSeverity().rawValue)]: \(w.getMessage())")
+                            }
+                        } catch {
+                            print(error)
+                        }
+                        
+                        
+                        
+                        var req = ThreeDSecureBeginAuthRequest.demoAuthRequest
+                        req.testScenario = ThreeDSecureTestScenario.threeDS_V2_AUTO_CHALLENGE_FAIL
+                        
+                        let threeDSService = ThreeDSecureService()
+                        threeDSService.threeDSecureBeginAuthentication(paymentMethodToken: paymentMethod,
+                                                                       threeDSecureBeginAuthRequest: req) { (res, err) in
+                            if let err = err {
+                                print(err)
+                            } else if let val = res?.authentication as? ThreeDSSkippedAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSMethodAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSBrowserV2ChallengeAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSAppV2ChallengeAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSBrowserV1ChallengeAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSDeclinedAPIResponse {
+                                print(val)
+                            } else if let val = res?.authentication as? ThreeDSSuccessAPIResponse {
+                                print(val)
+                            }
+                        }
+                    }
+
+                }
 
                 if self?.state.selectedPaymentMethod.isEmpty == true && paymentMethods.isEmpty == false {
                     guard let id = paymentMethods[0].token else { return }
