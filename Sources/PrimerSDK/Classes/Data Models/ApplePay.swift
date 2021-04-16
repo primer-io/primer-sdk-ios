a9f71b2daab05123e0fbd8bbdf33c3ca967266ee
Evangelos Pittas <evangelospittas@gmail.com>
2021-04-16 17:19:37 +0300

    [CHE-408] Modify Apple Pay data models.

--- Sources/PrimerSDK/Classes/Data Models/ApplePay.swift
+++ Sources/PrimerSDK/Classes/Data Models/ApplePay.swift
@@ -1,10 +1,38 @@
-struct ApplePayToken: Codable {
-    let paymentData: ApplePayTokenPaymentData
-    let paymentMethod: ApplePayTokenPaymentMethod
-    let transactionIdentifier: String
+#if canImport(UIKit)
+
+public enum MerchantCapability {
+    case capability3DS
+    case capabilityEMV
+    case capabilityCredit
+    case capabilityDebit
 }
 
-struct ApplePayTokenPaymentData: Codable {
+struct ApplePayRequest {
+    var currency: Currency
+    var merchantIdentifier: String
+    var countryCode: CountryCode
+    var supportedNetworks: [PaymentNetwork]
+    var items: [OrderItem]
+    var merchantCapabilities: [MerchantCapability]
+}
+
+struct ApplePayPaymentResponse {
+    let token: ApplePayPaymentResponseToken
+}
+
+struct ApplePayPaymentResponseToken: Codable {
+    let paymentMethod: ApplePayPaymentResponsePaymentMethod
+    let transactionIdentifier: String
+    let paymentData: ApplePayPaymentResponseTokenPaymentData
+}
+
+struct ApplePayPaymentResponsePaymentMethod: Codable {
+    let displayName: String?
+//    let network: String
+//    let type: String
+}
+
+struct ApplePayPaymentResponseTokenPaymentData: Codable {
     let data: String
     let signature: String
     let version: String
@@ -17,8 +45,4 @@ struct ApplePayTokenPaymentDataHeader: Codable {
     let transactionId: String
 }
 
-struct ApplePayTokenPaymentMethod: Codable {
-    let displayName: String
-    let network: String
-    let type: String
-}
+#endif
