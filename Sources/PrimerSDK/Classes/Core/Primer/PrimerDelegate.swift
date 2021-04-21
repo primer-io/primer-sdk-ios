533732b1bf83407db545659d036cc9807ce34ece
Evangelos Pittas <evangelospittas@gmail.com>
2021-04-21 15:36:52 +0300

    [CHE-533] Add inline documentation of exposed functionality and explain better how the authorizePayment delegate method should be used.

--- Sources/PrimerSDK/Classes/Core/Primer/PrimerDelegate.swift
+++ Sources/PrimerSDK/Classes/Core/Primer/PrimerDelegate.swift
@@ -12,15 +12,16 @@ import UIKit
  `clientTokenCallback(_:)`: This function will be called once Primer can provide you a client token. Provide the token to
  your backend in order retrieve a session token.
  
- `authorizePayment(_:)`: This function will return a token for any selected payment method. Provide this token to your backend
- in order to authorize the payment.
+ `tokenAddedToVault(_:)`: This function will be called only when a payment method has beed successfully added in vault.
+ 
+ `authorizePayment(_:)`: This function will be called only on checkout flows. Use it to provide the payment method token to your backend and call the completion when your API is called is finished. Pass an error if needed.
  
  `onCheckoutDismissed(_:)`: This function notifies you when the drop-in UI is dismissed.
  
  - Author:
  Primer
  - Version:
- 1.2.2
+ 1.4.3
  */
 
 public protocol PrimerDelegate: class {
