b5323cfaf4d4d7de249c5fb055b93b2f6226f95a
Aladin Taleb <aladin.taleb@gmail.com>
2021-04-27 09:12:09 +0200

    [CHE-485] Some cleaning + optional for future-proofness

--- Sources/PrimerSDK/Classes/User Interface/Checkout/DirectCheckoutViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/Checkout/DirectCheckoutViewModel.swift
@@ -104,15 +104,15 @@ struct PaymentMethodViewModel {
         }
     }
 
-    func toIconName() -> ImageName {
+    func toIconName() -> ImageName? {
         log(logLevel: .debug, title: nil, message: "Payment option: \(self.type)", prefix: "🦋", suffix: nil, bundle: nil, file: #file, className: String(describing: Self.self), function: #function, line: #line)
         switch type {
-        case .applePay: return ImageName.appleIcon
-        case .payPal: return  ImageName.paypal3
-        case .goCardlessMandate: return ImageName.rightArrow
-        case .klarna: return ImageName.klarna
-        case .paymentCard: return ImageName.creditCard
-        default: return  ImageName.creditCard
+        case .applePay: return .appleIcon
+        case .payPal: return  .paypal3
+        case .goCardlessMandate: return .rightArrow
+        case .klarna: return .klarna
+        case .paymentCard: return .creditCard
+        default: return nil
         }
     }
 
