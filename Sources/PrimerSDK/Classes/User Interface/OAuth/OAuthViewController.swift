bf564be3dc778d9dc763d079709b1b0d6a527e69
Evangelos Pittas <evangelospittas@gmail.com>
2021-05-07 09:25:39 +0300

    [CHE-631] Remove alert

--- Sources/PrimerSDK/Classes/User Interface/OAuth/OAuthViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/OAuth/OAuthViewController.swift
@@ -47,11 +47,11 @@ class OAuthViewController: UIViewController {
             switch result {
             case .failure(let error):
                 _ = ErrorHandler.shared.handle(error: error)
-                let alert = AlertController(title: "ERROR!", message: error.localizedDescription, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
-                    self?.dismiss(animated: true, completion: nil)
-                }))
-                alert.show()
+//                let alert = AlertController(title: "ERROR!", message: error.localizedDescription, preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+//                    self?.dismiss(animated: true, completion: nil)
+//                }))
+//                alert.show()
             case .success(let urlString):
                 DispatchQueue.main.async {
                     // if klarna show webview, otherwise oauth
