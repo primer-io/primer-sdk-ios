43ea17220dd9bc7c866543a2b815db9c5f0ad3ad
Evangelos Pittas <evangelospittas@gmail.com>
2021-05-19 12:25:21 +0300

    [CHE-710] Remove country code and currency from example's Klarna settings

--- Example/PrimerSDK/MerchantCheckoutViewController.swift
+++ Example/PrimerSDK/MerchantCheckoutViewController.swift
@@ -28,8 +28,6 @@ class MerchantCheckoutViewController: UIViewController {
     )
     
     let vaultKlarnaSettings = PrimerSettings(
-        currency: .SEK,
-        countryCode: .se,
         klarnaSessionType: .recurringPayment,
         hasDisabledSuccessScreen: true,
         isInitialLoadingHidden: true
@@ -64,25 +62,25 @@ class MerchantCheckoutViewController: UIViewController {
     }
     
     func configurePrimer() {
-        Primer.shared.configure(settings: generalSettings)
+        Primer.shared.configure(settings: vaultKlarnaSettings)
         
-        let theme = generatePrimerTheme()
-        Primer.shared.configure(theme: theme)
-
-        Primer.shared.setDirectDebitDetails(
-            firstName: "John",
-            lastName: "Doe",
-            email: "test@mail.com",
-            iban: "FR1420041010050500013M02606",
-            address: Address(
-                addressLine1: "1 Rue",
-                addressLine2: "",
-                city: "Paris",
-                state: "",
-                countryCode: "FR",
-                postalCode: "75001"
-            )
-        )
+//        let theme = generatePrimerTheme()
+//        Primer.shared.configure(theme: theme)
+//
+//        Primer.shared.setDirectDebitDetails(
+//            firstName: "John",
+//            lastName: "Doe",
+//            email: "test@mail.com",
+//            iban: "FR1420041010050500013M02606",
+//            address: Address(
+//                addressLine1: "1 Rue",
+//                addressLine2: "",
+//                city: "Paris",
+//                state: "",
+//                countryCode: "FR",
+//                postalCode: "75001"
+//            )
+//        )
     }
 
     // MARK: - ACTIONS
@@ -90,9 +88,7 @@ class MerchantCheckoutViewController: UIViewController {
     @IBAction func addCardButtonTapped(_ sender: Any) {
         Primer.shared.showCheckout(self, flow: .addCardToVault)
     }
-    
-    var klarnaNumberOfTimesPresented = 0
-    
+        
     @IBAction func addKlarnaButtonTapped(_ sender: Any) {
         Primer.shared.showCheckout(self, flow: .addKlarnaToVault)
     }
