98c322a58253e6221977a3f9c5f99dd7b30d3d82
Evangelos Pittas <evangelospittas@gmail.com>
2021-06-11 17:05:12 +0300

    Bump

--- Sources/PrimerSDK/Classes/Core/3DS/ThreeDSecureService.swift
+++ Sources/PrimerSDK/Classes/Core/3DS/ThreeDSecureService.swift
@@ -85,18 +85,6 @@ class ThreeDSecureService: ThreeDSecureServiceProtocol {
     
     func netceteraAuth(paymentMethod: PaymentMethodToken, completion: @escaping (Result<Transaction, Error>) -> Void) {
         do {
-//            var scheme: Scheme!
-//            switch paymentMethod.paymentInstrumentData?.binData?.network?.lowercased() {
-//            case "visa":
-//                scheme = .visa()
-//            default:
-//                let err = NSError(domain: "netcetera", code: 100, userInfo: [NSLocalizedDescriptionKey: "Only Visa for now"])
-//                completion(.failure(err))
-//                return
-//
-//            }
-//
-//            let directoryServerId = ThreeDSecureService.directoryServerIdFor(scheme: scheme)
             transaction = try threeDS2Service.createTransaction(directoryServerId: "A999999999",
                                                                     messageVersion: "2.1.0")
             completion(.success(transaction!))
@@ -139,43 +127,6 @@ class ThreeDSecureService: ThreeDSecureServiceProtocol {
         }
     }
     
-//    func submitButtonTapped() {
-//        var progressView: ProgressDialog?
-//        var requestSentTime: Date?
-//        var directoryServerId: String!
-//
-//        requestSentTime = Date()
-//        do {
-//            directoryServerId = "" //try directoryServerIdFor(accountNumber: paymentDetails.accountNumber)
-//            let transaction = try self.paymentDetailsUseCase.createTransaction(directoryServerId: directoryServerId)
-//            progressView = try self.transaction!.getProgressView()
-//            progressView!.start()
-//        } catch let error {
-//            print(error)
-//            self.view?.showErrorScreen(with: "\(error)")
-//            return
-//        }
-//        let authenticatingPromise = paymentDetailsUseCase.sendAuthenticationRequest(transaction: self.transaction!,
-//                                                                                    paymentDetails: paymentDetails)
-//        authenticatingPromise.done { authenticationResponse in
-//            self.handleResponseReceived(requestSentTime: requestSentTime!, {
-//                progressView!.stop()
-//                self.handleAuthenticationResponse(authenticationResponse, for: self.transaction!)
-//            })
-//            }.catch { (error) in
-//                if let requestSentTime = requestSentTime {
-//                    self.handleResponseReceived(requestSentTime: requestSentTime, {
-//                        if let progressView = progressView {
-//                            progressView.stop()
-//                        }
-//                        self.view?.showErrorScreen(with: "Transaction failed")
-//                    })
-//                } else {
-//                    self.view?.showErrorScreen(with: "Transaction failed")
-//                }
-//        }
-//    }
-    
     func threeDSecureBeginAuthentication(paymentMethodToken: PaymentMethodToken,
                                          threeDSecureBeginAuthRequest: ThreeDS.BeginAuthRequest,
                                          completion: @escaping (ThreeDS.BeginAuthResponse?, Error?) -> Void) {
@@ -192,24 +143,6 @@ class ThreeDSecureService: ThreeDSecureServiceProtocol {
                 completion(res, nil)
             }
         })
-        
-        //        api.vaultFetchPaymentMethods(clientToken: clientToken) { [weak self] (result) in
-        //            switch result {
-        //            case .failure:
-        //                completion(PrimerError.vaultFetchFailed)
-        //            case .success(let paymentMethods):
-        //                self?.state.paymentMethods = paymentMethods.data
-        //
-        //                guard let paymentMethods = self?.state.paymentMethods else { return }
-        //
-        //                if self?.state.selectedPaymentMethod.isEmpty == true && paymentMethods.isEmpty == false {
-        //                    guard let id = paymentMethods[0].token else { return }
-        //                    self?.state.selectedPaymentMethod = id
-        //                }
-        //
-        //                completion(nil)
-        //            }
-        //        }
     }
 }
 
@@ -234,43 +167,6 @@ extension ThreeDSecureService {
         default:
             return ""
         }
-//        let visaRegex = try NSRegularExpression(pattern: "^4[0-9]*", options: [.anchorsMatchLines])
-//        if visaRegex.matches(accountNumber) {
-//            return DsRidValues.visa
-//        }
-//
-//        let mastercardRegex = try NSRegularExpression(pattern: "^(2[0-1]|220[5-9]|22[1-9]|2[3-9]|5|6)[0-9]*",
-//                                                      options: [.anchorsMatchLines])
-//        if mastercardRegex.matches(accountNumber) {
-//            return DsRidValues.mastercard
-//        }
-//
-//        let amexRegex = try NSRegularExpression(pattern: "^(34|37)[0-9]*", options: [.anchorsMatchLines])
-//        if amexRegex.matches(accountNumber) {
-//            return DsRidValues.amex
-//        }
-//
-//        let jcbRegex = try NSRegularExpression(pattern: "^35[0-9]*", options: [.anchorsMatchLines])
-//        if jcbRegex.matches(accountNumber) {
-//            return DsRidValues.jcb
-//        }
-//
-//        let dinersRegex = try NSRegularExpression(pattern: "^36[0-9]*", options: [.anchorsMatchLines])
-//        if dinersRegex.matches(accountNumber) {
-//            return DsRidValues.diners
-//        }
-//
-//        let mirRegex = try NSRegularExpression(pattern: "^220[0-4][0-9]*", options: [.anchorsMatchLines])
-//        if mirRegex.matches(accountNumber) {
-//            return "A000000658"
-//        }
-//
-//        let unionRegex = try NSRegularExpression(pattern: "^(62[0-9]{14,17})$", options: [.anchorsMatchLines])
-//        if unionRegex.matches(accountNumber) {
-//            return DsRidValues.union
-//        }
-//
-//        throw AppError.InvalidCard
     }
 }
 
