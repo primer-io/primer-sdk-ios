57a47de55694a7227a7882118f02e314ea402aca
Evangelos Pittas <evangelospittas@gmail.com>
2021-06-22 12:42:54 +0300

    Implement mocked 3DS service layer.

--- Sources/PrimerSDK/Classes/Core/3DS/3DSService.swift
+++ Sources/PrimerSDK/Classes/Core/3DS/3DSService.swift
@@ -93,3 +93,54 @@ class ThreeDSService: ThreeDSServiceProtocol {
     }
     
 }
+
+class MockThreeDSService: ThreeDSServiceProtocol {
+    
+    var response: Data?
+    let throwsError: Bool = false
+    var isCalled: Bool = false
+    
+    init(with response: Data? = nil) {
+        self.response = response
+    }
+    
+    func initializeSDK(_ sdk: ThreeDSSDKProtocol, completion: @escaping (Result<Void, Error>) -> Void) {
+        sdk.initializeSDK(completion: completion)
+    }
+    
+    func sdkAuth(sdk: ThreeDSSDKProtocol, cardNetwork: CardNetwork, protocolVersion: ThreeDS.ProtocolVersion, completion: @escaping (Result<Transaction, Error>) -> Void) {
+        sdk.sdkAuth(cardNetwork: cardNetwork, protocolVersion: protocolVersion, completion: completion)
+    }
+    
+    func beginRemoteAuth(paymentMethodToken: PaymentMethodToken, threeDSecureBeginAuthRequest: ThreeDS.BeginAuthRequest, completion: @escaping (Result<ThreeDS.BeginAuthResponse, Error>) -> Void) {
+        let state: AppStateProtocol = DependencyContainer.resolve()
+        
+        guard let clientToken = state.decodedClientToken else {
+            return completion(.failure(PrimerError.vaultFetchFailed))
+        }
+        
+        let api = MockPrimerAPIClient()
+        DependencyContainer.register(api as PrimerAPIClientProtocol)
+        api.response = response
+        
+        api.threeDSBeginAuth(clientToken: clientToken, paymentMethodToken: paymentMethodToken, threeDSecureBeginAuthRequest: threeDSecureBeginAuthRequest, completion: completion)
+    }
+    
+    func performChallenge(with sdk: ThreeDSSDKProtocol, on transaction: Transaction, with threeDSecureAuthResponse: ThreeDSAuthenticationProtocol, presentOn viewController: UIViewController, completion: @escaping (Result<ThreeDS.ThreeDSSDKAuthCompletion, Error>) -> Void) {
+        
+    }
+    
+    func continueRemoteAuth(threeDSTokenId: String, completion: @escaping (Result<ThreeDS.PostAuthResponse, Error>) -> Void) {
+        let state: AppStateProtocol = DependencyContainer.resolve()
+        
+        guard let clientToken = state.decodedClientToken else {
+            return completion(.failure(PrimerError.vaultFetchFailed))
+        }
+        
+        let api = MockPrimerAPIClient()
+        DependencyContainer.register(api as PrimerAPIClientProtocol)
+        api.response = response
+        
+        api.threeDSContinueAuth(clientToken: clientToken, threeDSTokenId: threeDSTokenId, completion: completion)
+    }
+}
