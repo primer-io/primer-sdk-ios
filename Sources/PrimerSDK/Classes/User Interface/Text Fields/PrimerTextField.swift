7c38f6084ce6f9f2a35af29cb54c524245f9ba14
Evangelos Pittas <evangelospittas@gmail.com>
2021-06-29 10:38:37 +0300

    Implement textfield subclass.

---
+++ Sources/PrimerSDK/Classes/User Interface/Text Fields/PrimerTextField.swift
@@ -0,0 +1,37 @@
+//
+//  PrimerTextField.swift
+//  Pods-PrimerSDK_Example
+//
+//  Created by Evangelos Pittas on 29/6/21.
+//
+
+import UIKit
+
+internal class PrimerTextField: UITextField {
+    
+    internal enum Validation {
+        case valid, invalid(_ error: Error?), notAvailable
+    }
+    
+    override var delegate: UITextFieldDelegate? {
+        get {
+            return super.delegate
+        }
+        set {
+
+        }
+    }
+    
+    internal var _text: String?
+    
+    override var text: String? {
+        get {
+            return "****"
+        }
+        set {
+            super.text = newValue
+            _text = super.text
+        }
+    }
+        
+}
