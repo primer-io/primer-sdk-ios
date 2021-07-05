cb2451e7dbed379b6147fab9529b63a2ce2fee7d
Evangelos Pittas <evangelospittas@gmail.com>
2021-07-05 13:07:46 +0300

    Test in Example project.

--- Example/PrimerSDK/AppViewController.swift
+++ Example/PrimerSDK/AppViewController.swift
@@ -6,8 +6,25 @@
 //  Copyright © 2021 CocoaPods. All rights reserved.
 //
 
+import PrimerSDK
 import UIKit
 
-class AppViewController: UIViewController {
+class AppViewController: UIViewController, PrimerTextFieldViewDelegate {
     
+    @IBOutlet weak var primerTextField: PrimerCardNumberFieldView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        primerTextField.backgroundColor = .orange
+        primerTextField.delegate = self
+    }
+    
+    func isTextValid(_ isValid: Bool?) {
+        print("isTextValid: \(isValid)")
+    }
+    
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, didDetectCardNetwork cardNetwork: CardNetwork) {
+        print(cardNetwork)
+    }
 }
