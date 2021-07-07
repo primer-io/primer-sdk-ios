b47515ef09282d879ff6491c3a0a108b8fdd7247
Evangelos Pittas <evangelospittas@gmail.com>
2021-07-07 15:59:33 +0300

    Implement card components on the example app.

--- Example/PrimerSDK/AppViewController.swift
+++ Example/PrimerSDK/AppViewController.swift
@@ -9,12 +9,15 @@
 import PrimerSDK
 import UIKit
 
-class AppViewController: UIViewController, PrimerTextFieldViewDelegate {
-    
+class AppViewController: UIViewController, PrimerTextFieldViewDelegate, CardComponentsManagerDelegate {
+
     @IBOutlet weak var primerTextField: PrimerCardNumberFieldView!
     @IBOutlet weak var expiryDateFieldView: PrimerExpiryDateFieldView!
     @IBOutlet weak var cvvFieldView: PrimerCVVFieldView!
     @IBOutlet weak var cardholderFieldView: PrimerCardholderFieldView!
+    
+//    var cardComponentsManager: CardComponentsManager!
+    let endpoint = "https://us-central1-primerdemo-8741b.cloudfunctions.net"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +36,70 @@ class AppViewController: UIViewController, PrimerTextFieldViewDelegate {
         cardholderFieldView.delegate = self
     }
     
+    @IBAction func tokenize(_ sender: Any) {
+//        clientTokenCallback { result in
+//            switch result {
+//            case .success(let token):
+        let cardComponentsManager = CardComponentsManager(accessToken: "token", flow: .vault, cardnumberField: self.primerTextField, expiryDateField: self.expiryDateFieldView, cvvField: self.cvvFieldView, cardholderField: self.cardholderFieldView)
+                cardComponentsManager.delegate = self
+                cardComponentsManager.tokenize()
+                
+//            case .failure(let err):
+//                break
+//            }
+//        }
+    }
+    
     func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, didDetectCardNetwork cardNetwork: CardNetwork) {
         print(cardNetwork)
     }
     
     func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, isValid: Bool?) {
         print("isTextValid: \(isValid)")
+    }
+    
+    func clientTokenCallback(_ completion: @escaping (Result<String, Error>) -> Void) {
+        guard let url = URL(string: "\(endpoint)/clientToken") else {
+            return completion(.failure(NetworkError.missingParams))
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body = CreateClientTokenRequest(customerId: "customer123", customerCountryCode: nil)
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(body)
+        } catch {
+            return completion(.failure(NetworkError.missingParams))
+        }
+        
+        callApi(request, completion: { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let token = (try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: String])["clientToken"]!
+                    print("🚀🚀🚀 token:", token)
+                    completion(.success(token))
+
+                } catch {
+                    completion(.failure(NetworkError.serializationError))
+                    
+                }
+            case .failure(let err): completion(.failure(err))
+            }
+        })
+    }
+    
+    func onTokenizeSuccess(_ paymentMethodToken: PaymentMethodToken) {
+        print("*** onTokenizeSuccess: \(paymentMethodToken)")
+    }
+    
+    func tokenizationFailed(with error: Error) {
+        print("*** tokenizationFailed: \(error)")
+    }
+    
+    func isLoading(_ isLoading: Bool) {
+        print("*** isLoading: \(isLoading)")
     }
 }
