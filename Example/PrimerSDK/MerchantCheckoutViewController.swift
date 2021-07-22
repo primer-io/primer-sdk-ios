f42035d22a76a120c91471bc81e3091543a96c37
Evangelos Pittas <evangelospittas@gmail.com>
2021-07-22 17:29:21 +0300

    Bump

--- Example/PrimerSDK/MerchantCheckoutViewController.swift
+++ Example/PrimerSDK/MerchantCheckoutViewController.swift
@@ -73,7 +73,6 @@ class MerchantCheckoutViewController: UIViewController {
         isInitialLoadingHidden: false
     )
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Primer"
