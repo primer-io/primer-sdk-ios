4173e65e3c61e1ab6b564f6fb1a3e5eedf726cfe
Evangelos Pittas <evangelospittas@gmail.com>
2021-07-30 10:19:12 +0300

    Clean up project

---
+++ Sources/PrimerSDK/Classes/User Interface/Root/PrimerContainerViewController.swift
@@ -0,0 +1,62 @@
+//
+//  PrimerContainerViewController.swift
+//  PrimerSDK
+//
+//  Created by Evangelos Pittas on 30/7/21.
+//
+
+import UIKit
+
+/// PrimerContainerViewController gets a view controller as input. The input view controller must use autolayout.
+/// It then sets this view controller as its child, wraps it within a scrollview and sets the constraints needed.
+class PrimerContainerViewController: PrimerViewController {
+    
+    internal var scrollView = UIScrollView()
+    internal var childView = UIView()
+    internal var childViewController: UIViewController
+    
+    init(childViewController: UIViewController) {
+        self.childViewController = childViewController
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    var scrollViewHeightConstraint: NSLayoutConstraint!
+    var childViewHeightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.bounces = false
+                
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 11.0, *) {
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        } else {
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        }
+        if #available(iOS 11.0, *) {
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        } else {
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        }
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        scrollView.addSubview(childView)
+        childView.translatesAutoresizingMaskIntoConstraints = false
+        childView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
+        childView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0).isActive = true
+        childView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0).isActive = true
+        childView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0).isActive = true
+        childView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        childViewHeightConstraint = childView.heightAnchor.constraint(equalToConstant: childViewController.view.bounds.size.height)
+        childViewHeightConstraint.isActive = true
+
+        childView.addSubview(childViewController.view)
+        childViewController.didMove(toParent: self)
+    }
+}
--- Sources/PrimerSDK/Classes/User Interface/Root/PrimerRootViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/Root/PrimerRootViewController.swift
@@ -54,24 +54,19 @@ class PrimerRootViewController: UIViewController {
     }
     
     func testNavFlow() {
-        let lvc = PrimerLoadingViewController()
-//        lvc.view.translatesAutoresizingMaskIntoConstraints = false
-        lvc.view.widthAnchor.constraint(equalToConstant: self.childContainerView.frame.width).isActive = true
-        lvc.view.heightAnchor.constraint(equalToConstant: 300).isActive = true
-//        lvc.view.layoutIfNeeded()
-        self.show(viewController: lvc)
+        let cfvc = PrimerCardFormViewController(flow: .checkout)
+        cfvc.view.widthAnchor.constraint(equalToConstant: self.childContainerView.frame.width).isActive = true
+        show(viewController: cfvc)
         
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
-            let lvc2 = PrimerLoadingViewController()
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+            let lvc2 = PrimerLoadingViewController(withHeight: 600)
     //        lvc.view.translatesAutoresizingMaskIntoConstraints = false
             lvc2.view.widthAnchor.constraint(equalToConstant: self.childContainerView.frame.width).isActive = true
-            lvc2.view.heightAnchor.constraint(equalToConstant: 3000).isActive = true
+//            lvc2.view.heightAnchor.constraint(equalToConstant: 3000).isActive = true
     //        lvc.view.layoutIfNeeded()
             self.show(viewController: lvc2)
         }
     }
-    
-    var heightConstraint: NSLayoutConstraint?
     
     func show(viewController: UIViewController) {
         viewController.view.translatesAutoresizingMaskIntoConstraints = false
@@ -114,7 +109,7 @@ class PrimerRootViewController: UIViewController {
             let navigationControllerHeight: CGFloat = (viewController.view.bounds.size.height + nc!.navigationBar.bounds.height) > availableScreenHeight ? availableScreenHeight : (viewController.view.bounds.size.height + nc!.navigationBar.bounds.height)
             let containerViewHeight: CGFloat = navigationControllerHeight
             
-            nc!.view.widthAnchor.constraint(equalToConstant: childContainerView.frame.width).isActive = true
+//            nc!.view.widthAnchor.constraint(equalToConstant: childContainerView.frame.width).isActive = true
             heightConstraint?.isActive = false
             heightConstraint = nc!.view.heightAnchor.constraint(equalToConstant: navigationControllerHeight)
             heightConstraint!.isActive = true
@@ -170,98 +165,6 @@ class PrimerRootViewController: UIViewController {
         default:
             break
         }
-    }
-    
-}
-
-class PrimerContainerViewController: PrimerViewController {
-    
-    internal var scrollView = UIScrollView()
-    internal var childView = UIView()
-    internal var childViewController: UIViewController
-    
-    init(childViewController: UIViewController) {
-        self.childViewController = childViewController
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
-    }
-    
-    var scrollViewHeightConstraint: NSLayoutConstraint!
-    var childViewHeightConstraint: NSLayoutConstraint!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        scrollView.bounces = false
-                
-        var topPadding: CGFloat = 0.0
-        var bottomPadding: CGFloat = 0.0
-        
-        if #available(iOS 13.0, *) {
-            let window = UIApplication.shared.windows[0]
-            topPadding = window.safeAreaInsets.top
-            bottomPadding = window.safeAreaInsets.bottom
-        } else if #available(iOS 11.0, *) {
-            let window = UIApplication.shared.keyWindow
-            topPadding = window?.safeAreaInsets.top ?? 0
-            bottomPadding = window?.safeAreaInsets.bottom ?? 0
-        } else {
-            topPadding = 20.0
-            bottomPadding = 0.0
-        }
-        
-//        let availableScreenHeight = UIScreen.main.bounds.size.height - (topPadding + bottomPadding)
-//        let scrollViewHeight: CGFloat = childViewController.view.bounds.size.height > availableScreenHeight ? availableScreenHeight : childViewController.view.bounds.size.height
-        
-        view.addSubview(scrollView)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        if #available(iOS 11.0, *) {
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        } else {
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        }
-        if #available(iOS 11.0, *) {
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
-        } else {
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        }
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-//        scrollViewHeightConstraint = scrollView.heightAnchor.constraint(equalToConstant: scrollViewHeight)
-//        scrollViewHeightConstraint.isActive = true
-        
-        scrollView.addSubview(childView)
-        childView.translatesAutoresizingMaskIntoConstraints = false
-        childView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
-        childView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0).isActive = true
-        childView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0).isActive = true
-        childView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0).isActive = true
-        childView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-        childViewHeightConstraint = childView.heightAnchor.constraint(equalToConstant: childViewController.view.bounds.size.height)
-        childViewHeightConstraint.isActive = true
-
-        childView.addSubview(childViewController.view)
-        childViewController.didMove(toParent: self)
-    }
-}
-
-class PrimerLoadingViewController: PrimerViewController {
-    
-    private var activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        view.addSubview(activityIndicatorView)
-        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
-        activityIndicatorView.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
-        activityIndicatorView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
-        activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        activityIndicatorView.startAnimating()
     }
     
 }
