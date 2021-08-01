db7e05a38d6b0d40d0554ecc60a2bde26f551b0f
Evangelos Pittas <evangelospittas@gmail.com>
2021-08-01 22:43:09 +0300

    Add helper pin view function.

--- Sources/PrimerSDK/Classes/User Interface/Root/PrimerContainerViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/Root/PrimerContainerViewController.swift
@@ -60,3 +60,14 @@ class PrimerContainerViewController: PrimerViewController {
         childViewController.didMove(toParent: self)
     }
 }
+
+extension UIView {
+    
+    func pin(view: UIView, leading: CGFloat = 0, top: CGFloat = 0, trailing: CGFloat = 0, bottom: CGFloat = 0) {
+        topAnchor.constraint(equalTo: view.topAnchor, constant: top).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottom).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leading).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: trailing).isActive = true
+    }
+    
+}
