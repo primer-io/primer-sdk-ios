553360065d414d2c5323c88de758070cc32cedea
Evangelos Pittas <evangelospittas@gmail.com>
2021-08-03 15:36:58 +0300

    Start working on vault manager list view controller.

---
+++ Sources/PrimerSDK/Classes/User Interface/Root/PrimerVaultManagerViewController.swift
@@ -0,0 +1,138 @@
+//
+//  PrimerVaultManagerViewController.swift
+//  PrimerSDK
+//
+//  Created by Evangelos Pittas on 2/8/21.
+//
+
+import UIKit
+
+internal class PrimerVaultManagerViewController: PrimerFormViewController {
+    
+    override var title: String? {
+        didSet {
+            (parent as? PrimerContainerViewController)?.title = title
+        }
+    }
+    
+    // swiftlint:disable function_body_length
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let checkoutViewModel: VaultCheckoutViewModelProtocol = DependencyContainer.resolve()
+        let availablePaymentMethods = checkoutViewModel.availablePaymentOptions
+        
+        verticalStackView.spacing = 14.0
+        
+        if !availablePaymentMethods.isEmpty {
+            let otherPaymentMethodsTitleLabel = UILabel()
+            otherPaymentMethodsTitleLabel.text = NSLocalizedString("primer-vault-payment-method-available-payment-methods",
+                                                                   tableName: nil,
+                                                                   bundle: Bundle.primerResources,
+                                                                   value: "Available payment methods",
+                                                                   comment: "Available payment methods - Vault Checkout 'Available payment methods' Title").uppercased()
+            otherPaymentMethodsTitleLabel.textColor = PrimerColor(rgb: 0x808080)
+            otherPaymentMethodsTitleLabel.font = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+            otherPaymentMethodsTitleLabel.textAlignment = .left
+            verticalStackView.addArrangedSubview(otherPaymentMethodsTitleLabel)
+            
+            for paymentMethod in availablePaymentMethods {
+                if paymentMethod.type == .applePay || paymentMethod.type == .googlePay {
+                    continue
+                }
+                
+                let paymentMethodButton = UIButton()
+                paymentMethodButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+                paymentMethodButton.backgroundColor = .white
+                paymentMethodButton.setTitle(paymentMethod.toString(), for: .normal)
+                paymentMethodButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.0, weight: .medium)
+                paymentMethodButton.setImage(paymentMethod.toIconName()?.image?.withRenderingMode(.alwaysTemplate), for: .normal)
+                paymentMethodButton.imageEdgeInsets = UIEdgeInsets(top: -2, left: 0, bottom: 0, right: 10)
+                paymentMethodButton.layer.cornerRadius = 4.0
+                paymentMethodButton.clipsToBounds = true
+                
+                switch paymentMethod.type {
+                case .paymentCard:
+                    paymentMethodButton.setTitleColor(.black, for: .normal)
+                    paymentMethodButton.tintColor = .black
+                    paymentMethodButton.layer.borderWidth = 1.0
+                    paymentMethodButton.layer.borderColor = UIColor.black.cgColor
+                    paymentMethodButton.addTarget(self, action: #selector(cardButtonTapped), for: .touchUpInside)
+                    
+                case .payPal:
+                    if #available(iOS 11.0, *) {
+                        paymentMethodButton.backgroundColor = UIColor(red: 0.745, green: 0.894, blue: 0.996, alpha: 1)
+                        paymentMethodButton.setTitleColor(.white, for: .normal)
+                        paymentMethodButton.setImage(paymentMethod.toIconName()?.image, for: .normal)
+                        paymentMethodButton.tintColor = .white
+                        paymentMethodButton.addTarget(self, action: #selector(payPalButtonTapped), for: .touchUpInside)
+                    }
+                    
+                case .goCardlessMandate:
+                    paymentMethodButton.setTitleColor(.white, for: .normal)
+                    paymentMethodButton.tintColor = .white
+                    paymentMethodButton.layer.borderWidth = 1.0
+                    paymentMethodButton.layer.borderColor = UIColor.black.cgColor
+                    
+                case .klarna:
+                    paymentMethodButton.backgroundColor = UIColor(red: 1, green: 0.702, blue: 0.78, alpha: 1)
+                    paymentMethodButton.setTitleColor(.black, for: .normal)
+                    paymentMethodButton.tintColor = .white
+                    paymentMethodButton.setImage(nil, for: .normal)
+                    paymentMethodButton.addTarget(self, action: #selector(klarnaButtonTapped), for: .touchUpInside)
+                    
+                default:
+                    break
+                }
+                
+                verticalStackView.addArrangedSubview(paymentMethodButton)
+            }
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = NSLocalizedString("primer-vault-nav-bar-title",
+                                  tableName: nil,
+                                  bundle: Bundle.primerResources,
+                                  value: "Add payment method",
+                                  comment: "Add payment method - Vault Navigation Bar Title")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        title = NSLocalizedString("primer-vault-nav-bar-title",
+                                  tableName: nil,
+                                  bundle: Bundle.primerResources,
+                                  value: "Add payment method",
+                                  comment: "Add payment method - Vault Navigation Bar Title")
+    }
+    
+    @objc
+    func klarnaButtonTapped() {
+        if #available(iOS 11.0, *) {
+            let oavc = OAuthViewController(host: .klarna)
+            oavc.modalPresentationStyle = .fullScreen
+            present(oavc, animated: true, completion: nil)
+        }
+    }
+    
+    @objc
+    func payPalButtonTapped() {
+        if #available(iOS 11.0, *) {
+            let oavc = OAuthViewController(host: .paypal)
+            oavc.modalPresentationStyle = .fullScreen
+            present(oavc, animated: true, completion: nil)
+        }
+    }
+    
+    @objc
+    func cardButtonTapped() {
+        let cfvc = PrimerCardFormViewController(flow: .checkout)
+        Primer.shared.primerRootVC?.show(viewController: cfvc)
+    }
+    
+}
--- Sources/PrimerSDK/Classes/User Interface/Vault/VaultPaymentMethodViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/Vault/VaultPaymentMethodViewController.swift
@@ -2,6 +2,116 @@
 
 import UIKit
 
+internal class VaultedPaymentInstrumentCell: UITableViewCell {
+    
+    
+    func configure() {
+        
+    }
+    
+}
+
+internal class VaultedPaymentInstrumentsViewController: PrimerViewController {
+    
+    private var isDeleting: Bool = false
+    private var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let theme: PrimerThemeProtocol = DependencyContainer.resolve()
+        
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.pin(view: view)
+        tableView.accessibilityIdentifier = "payment_methods_table_view"
+        tableView.backgroundColor = theme.colorTheme.main1
+        tableView.rowHeight = 46
+//        tableView.tableFooterView = UIView()
+        tableView.alwaysBounceVertical = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell3")
+        
+        let viewModel: VaultPaymentMethodViewModelProtocol = DependencyContainer.resolve()
+        viewModel.reloadVault { [weak self] _ in
+            
+        }
+    }
+    
+}
+
+extension VaultedPaymentInstrumentsViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let viewModel: VaultPaymentMethodViewModelProtocol = DependencyContainer.resolve()
+        // That's actually payment instruments
+        return viewModel.paymentMethods.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let theme: PrimerThemeProtocol = DependencyContainer.resolve()
+        let viewModel: VaultPaymentMethodViewModelProtocol = DependencyContainer.resolve()
+        
+        let cell = UITableViewCell()
+
+//        if indexPath.row == viewModel.paymentMethods.count {
+//            let addButton = UIButton()
+//
+//            addButton.setTitle(theme.content.vaultPaymentMethodView.addButtonText, for: .normal)
+//            addButton.setTitleColor(theme.colorTheme.tint1, for: .normal)
+//            addButton.setTitleColor(theme.colorTheme.disabled1, for: .highlighted)
+//            addButton.contentHorizontalAlignment = .left
+//            addButton.addTarget(self, action: #selector(showCardForm), for: .touchUpInside)
+//
+//            addButton.translatesAutoresizingMaskIntoConstraints = false
+//
+//            cell.selectionStyle = .none
+//            cell.contentView.addSubview(addButton)
+//
+//            addButton.leadingAnchor.constraint(equalTo: cell.leadingAnchor, constant: 10).isActive = true
+//            addButton.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+//
+//            return cell
+//        }
+
+        let token = viewModel.paymentMethods[indexPath.row]
+
+        let cardView = CardButton()
+        cardView.isUserInteractionEnabled = false
+
+        cardView.render(model: token.cardButtonViewModel)
+
+        let isEnabled = viewModel.selectedId == viewModel.paymentMethods[indexPath.row].token ?? ""
+
+        if isDeleting {
+            cardView.toggleError(isEnabled: isDeleting)
+        } else {
+            cardView.hideIcon(isEnabled)
+            cardView.toggleIcon()
+        }
+
+        cardView.hideBorder()
+
+        cardView.addSeparatorLine()
+
+        cell.addSubview(cardView)
+
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        cardView.topAnchor.constraint(equalTo: cell.topAnchor).isActive = true
+        cardView.leadingAnchor.constraint(equalTo: cell.leadingAnchor).isActive = true
+        cardView.trailingAnchor.constraint(equalTo: cell.trailingAnchor).isActive = true
+        cardView.bottomAnchor.constraint(equalTo: cell.bottomAnchor).isActive = true
+
+        cell.backgroundColor = theme.colorTheme.main1
+
+        return cell
+    }
+    
+}
+
 internal class VaultPaymentMethodViewController: PrimerViewController {
     
     var showDeleteIcon = false
@@ -14,9 +124,28 @@ internal class VaultPaymentMethodViewController: PrimerViewController {
 
     override func viewDidLoad() {
         view.addSubview(subView)
+        navigationController?.setNavigationBarHidden(true, animated: false)
         subView.delegate = self
         subView.pin(to: view)
         subView.render()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = NSLocalizedString("primer-vault-nav-bar-title",
+                                  tableName: nil,
+                                  bundle: Bundle.primerResources,
+                                  value: "Add payment method",
+                                  comment: "Add payment method - Vault Navigation Bar Title")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        title = NSLocalizedString("primer-vault-nav-bar-title",
+                                  tableName: nil,
+                                  bundle: Bundle.primerResources,
+                                  value: "Add payment method",
+                                  comment: "Add payment method - Vault Navigation Bar Title")
     }
 
     // FIXME: Do not handle logic based on UI events.
