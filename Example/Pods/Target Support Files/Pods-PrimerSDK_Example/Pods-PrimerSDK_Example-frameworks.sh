a76db0aa51072929f16fc221f915ec6d6723473a
Evangelos Pittas <evangelospittas@gmail.com>
2021-08-04 11:17:39 +0300

    [run] pod install

--- Example/Pods/Target Support Files/Pods-PrimerSDK_Example/Pods-PrimerSDK_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PrimerSDK_Example/Pods-PrimerSDK_Example-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PrimerSDK/PrimerSDK.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ThreeDS_SDK/ThreeDS_SDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PrimerSDK/PrimerSDK.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ThreeDS_SDK/ThreeDS_SDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
