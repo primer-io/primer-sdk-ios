5a59c881ecbfd76d1f564e812e166f1b3445c523
Evangelos Pittas <evangelospittas@gmail.com>
2021-08-06 16:04:22 +0300

    Bump

--- Example/PrimerSDK/AppViewController.swift
+++ Example/PrimerSDK/AppViewController.swift
@@ -9,20 +9,10 @@
 import PrimerSDK
 import UIKit
 
-class AppViewController: UIViewController, PrimerTextFieldViewDelegate {
-    
-    let endpoint = "https://us-central1-primerdemo-8741b.cloudfunctions.net"
-    
+class AppViewController: UIViewController {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-//        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
-//            Primer.shared.show(flow: .default)
-//        }
     }
 
 }
