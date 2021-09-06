bcc5de39d19c63dac3cbe9f17312e478ede0fd82
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-06 15:35:11 +0300

    Move clientToken storing functionality to a helper function.

--- Sources/PrimerSDK/Classes/Core/Payment Services/ClientTokenService.swift
+++ Sources/PrimerSDK/Classes/Core/Payment Services/ClientTokenService.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 internal protocol ClientTokenServiceProtocol {
+    static func storeClientToken(_ clientToken: String) throws
     func loadCheckoutConfig(_ completion: @escaping (Error?) -> Void)
 }
 
@@ -11,33 +12,37 @@ internal class ClientTokenService: ClientTokenServiceProtocol {
     deinit {
         log(logLevel: .debug, message: "🧨 deinit: \(self) \(Unmanaged.passUnretained(self).toOpaque())")
     }
+    
+    static func storeClientToken(_ clientToken: String) throws {
+        guard let jwtTokenPayload = clientToken.jwtTokenPayload,
+              let expDate = jwtTokenPayload.expDate
+        else {
+            throw PrimerError.clientTokenNull
+        }
+        
+        if expDate < Date() {
+            throw PrimerError.clientTokenExpired
+        }
+        
+        let state: AppStateProtocol = DependencyContainer.resolve()
+        state.decodedClientToken = jwtTokenPayload
+    }
 
     /**
     performs asynchronous call passed in by app developer, decodes the returned Base64 Primer client token string and adds it to shared state.
      */
     func loadCheckoutConfig(_ completion: @escaping (Error?) -> Void) {
-        let state: AppStateProtocol = DependencyContainer.resolve()
         let settings: PrimerSettingsProtocol = DependencyContainer.resolve()
 
         settings.clientTokenRequestCallback({ [weak self] (token, err) in
             if let err = err {
                 completion(PrimerError.clientTokenNull)
             } else if let token = token {
-                guard let jwtTokenPayload = token.jwtTokenPayload,
-                      let expDate = jwtTokenPayload.expDate
-                else {
-                    return completion(PrimerError.clientTokenNull)
-                }
-                
-                if expDate < Date() {
-                    return completion(PrimerError.clientTokenExpired)
-                }
-                
-                if let jwtTokenPayload = token.jwtTokenPayload {
-                    state.decodedClientToken = jwtTokenPayload
+                do {
+                    try ClientTokenService.storeClientToken(token)
                     completion(nil)
-                } else {
-                    completion(PrimerError.clientTokenNull)
+                } catch {
+                    completion(error)
                 }
             }
         })
