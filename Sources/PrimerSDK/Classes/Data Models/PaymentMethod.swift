c1af6e1d135545fc29759c787153cc289dc52440
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-07 12:21:04 +0300

    Add protocol and details' structures

--- Sources/PrimerSDK/Classes/Data Models/PaymentMethod.swift
+++ Sources/PrimerSDK/Classes/Data Models/PaymentMethod.swift
@@ -45,6 +45,48 @@ struct PaymentMethod {
     }
 }
 
-//struct PaymentMethod
+protocol PaymentMethodDetailsProtocol: Encodable {}
+
+struct CardDetails: PaymentMethodDetailsProtocol {
+    var number: String
+    var cvv: String
+    var expirationMonth: String
+    var expirationYear: String
+    var cardholderName: String?
+}
+
+struct PayPalDetails: PaymentMethodDetailsProtocol {
+    var paypalOrderId: String?
+    var paypalBillingAgreementId: String?
+    var shippingAddress: ShippingAddress?
+    var externalPayerInfo: PayPalExternalPayerInfo?
+}
+
+struct ApplePayDetails: PaymentMethodDetailsProtocol {
+    var paymentMethodConfigId: String?
+    var token: ApplePayPaymentResponseToken?
+    var sourceConfig: ApplePaySourceConfig?
+}
+
+struct GoCardlessDetails: PaymentMethodDetailsProtocol {
+    var gocardlessMandateId: String?
+}
+
+struct KlarnaDetails: PaymentMethodDetailsProtocol {
+    // Klarna payment session
+    var klarnaAuthorizationToken: String?
+    // Klarna customer token
+    var klarnaCustomerToken: String?
+    var sessionData: KlarnaSessionData?
+}
+
+struct ApayaDetails: PaymentMethodDetailsProtocol {
+    var mx: String
+    var mnc: String
+    var mcc: String
+    var hashedIdentifier: String
+    var productId: String
+    var currencyCode: String
+}
 
 #endif
