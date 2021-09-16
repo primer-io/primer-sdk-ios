4c39b6d51441df21e6ec2537ae0b2e6f6ad06365
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-16 16:46:44 +0300

    Add completion handler so we can notify that the webview was dismissed

--- Sources/PrimerSDK/Classes/User Interface/OAuth/PrimerWebViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/OAuth/PrimerWebViewModel.swift
@@ -17,6 +17,11 @@ internal protocol PrimerWebViewModelProtocol: AnyObject {
 internal class ApayaWebViewModel: PrimerWebViewModelProtocol {
 
     var result: Result<Apaya.WebViewResult, ApayaException>?
+    var onCompletion: ((Result<Apaya.WebViewResult, ApayaException>) -> Void)?
+    
+    deinit {
+        log(logLevel: .debug, message: "🧨 deinit: \(self) \(Unmanaged.passUnretained(self).toOpaque())")
+    }
 
     private func setResult(_ value: Result<Apaya.WebViewResult, ApayaException>?) {
         result = value
@@ -28,6 +33,7 @@ internal class ApayaWebViewModel: PrimerWebViewModelProtocol {
 
     func onDismiss() {
         let result = result ?? .failure(ApayaException.webViewFlowCancelled)
+        onCompletion?(result)
         let state: AppStateProtocol = DependencyContainer.resolve()
         state.setApayaResult(result)
         setResult(nil)
