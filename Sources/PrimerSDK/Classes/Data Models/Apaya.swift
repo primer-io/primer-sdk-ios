d625c279137b45646743cf4430daa8a7996676b9
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-20 15:07:52 +0300

    Add Apaya view models, both in Example app and in the SDK

--- Sources/PrimerSDK/Classes/Data Models/Apaya.swift
+++ Sources/PrimerSDK/Classes/Data Models/Apaya.swift
@@ -82,4 +82,91 @@ public struct Apaya {
             )
         }
     }
+    
+    class ViewModel {
+        
+        var carrier: Apaya.Carrier
+        var hashedIdentifier: String?
+        
+        init?(paymentMethod: PaymentMethodToken) {
+            guard paymentMethod.paymentInstrumentType == .apayaToken else { return nil }
+            guard let mcc = paymentMethod.paymentInstrumentData?.mcc,
+                  let mnc = paymentMethod.paymentInstrumentData?.mnc,
+                  let carrier = Apaya.Carrier(mcc: mcc, mnc: mnc)
+            else { return nil }
+            
+            self.carrier = carrier
+            self.hashedIdentifier = paymentMethod.paymentInstrumentData?.hashedIdentifier
+        }
+        
+    }
+
+    enum Carrier: String, Codable {
+        // swiftlint:disable identifier_name
+        case EE_UK, O2_UK, Vodafone_UK, Three_UK, Strex_Norway
+        // swiftlint:enable identifier_name
+        
+        var name: String {
+            switch self {
+            case .EE_UK:
+                return "EE UK"
+            case .O2_UK:
+                return "O2 UK"
+            case .Vodafone_UK:
+                return "Vodafone UK"
+            case .Three_UK:
+                return "Three UK"
+            case .Strex_Norway:
+                return "Strex Norway"
+            }
+        }
+        
+        var mcc: Int {
+            switch self {
+            case .EE_UK:
+                return 234
+            case .O2_UK:
+                return 234
+            case .Vodafone_UK:
+                return 234
+            case .Three_UK:
+                return 234
+            case .Strex_Norway:
+                return 242
+            }
+        }
+        
+        var mnc: Int {
+            switch self {
+            case .EE_UK:
+                return 99
+            case .O2_UK:
+                return 11
+            case .Vodafone_UK:
+                return 15
+            case .Three_UK:
+                return 20
+            case .Strex_Norway:
+                return 99
+            }
+        }
+        
+        init?(mcc: Int, mnc: Int) {
+            switch (mcc, mnc) {
+            case (234, 99):
+                self = .EE_UK
+            case (234, 11):
+                self = .O2_UK
+            case (234, 15):
+                self = .Vodafone_UK
+            case (234, 20):
+                self = .Three_UK
+            case (242, 99):
+                self = .Strex_Norway
+            default:
+                return nil
+            }
+        }
+        
+    }
 }
