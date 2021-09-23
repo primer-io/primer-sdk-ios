41e94724219aa07c5cca1fed9ac2fa2d5a990f49
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-23 11:21:11 +0300

    Add a BodyParameters struct that defines which parameters are needed to be passed on an API call.

---
+++ Sources/PrimerSDK/Classes/Core/APM/BodyParameters.swift
@@ -0,0 +1,13 @@
+//
+//  BodyParameters.swift
+//  PrimerSDK
+//
+//  Created by Evangelos Pittas on 20/9/21.
+//
+
+import Foundation
+
+struct BodyParameters {
+    var requiredParameters: [String: Any?]
+    var optionalParameters: [String]
+}
