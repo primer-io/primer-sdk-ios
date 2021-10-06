25ad840b4b03de8486ed20a07dbfc297ed6a0504
Carl <carl@primer.io>
2021-10-06 15:56:46 +0100

    remove comments

--- Sources/PrimerSDK/Classes/Extensions & Utilities/WebViewUtil.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/WebViewUtil.swift
@@ -1,10 +1,3 @@
-//
-//  WebViewUtil.swift
-//  PrimerSDK
-//
-//  Created by Carl Eriksson on 06/10/2021.
-//
-
 internal class WebViewUtil {
     static func allowedHostsContain(_ host: String?) -> Bool {
         guard let host = host else { return false }
