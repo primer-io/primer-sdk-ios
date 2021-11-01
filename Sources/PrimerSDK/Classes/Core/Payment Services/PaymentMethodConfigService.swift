d0c6285f775d6f77f4247235cd7cfb2b0bdb2ecf
Evangelos Pittas <evangelospittas@gmail.com>
2021-11-01 17:40:38 +0200

    Add fetch config if needed promise.

--- Sources/PrimerSDK/Classes/Core/Payment Services/PaymentMethodConfigService.swift
+++ Sources/PrimerSDK/Classes/Core/Payment Services/PaymentMethodConfigService.swift
@@ -31,6 +31,49 @@ internal class PaymentMethodConfigService: PaymentMethodConfigServiceProtocol {
         }
     }
     
+    private func fetchConfig() -> Promise<Void> {
+        return Promise { seal in
+            self.fetchConfig { err in
+                if let err = err {
+                    seal.reject(err)
+                } else {
+                    seal.fulfill(())
+                }
+            }
+        }
+    }
+    
+    func fetchPrimerConfigurationIfNeeded(enforce: Bool) -> Promise<PrimerConfiguration> {
+        return Promise { seal in
+            let clientTokenService: ClientTokenService = DependencyContainer.resolve()
+            
+            firstly {
+                clientTokenService.fetchClientTokenIfNeeded(enforce: false)
+            }
+            .then { () -> Promise<Void> in
+                let state: AppStateProtocol = DependencyContainer.resolve()
+                
+                if !enforce, state.paymentMethodConfig != nil {
+                    return Promise()
+                } else {
+                    return self.fetchConfig()
+                }
+            }
+            .done {
+                let state: AppStateProtocol = DependencyContainer.resolve()
+                
+                guard let config = state.paymentMethodConfig else {
+                    throw PrimerError.configFetchFailed
+                }
+                
+                seal.fulfill(config)
+            }
+            .catch { err in
+                seal.reject(err)
+            }
+        }
+    }
+    
 }
 
 #endif
