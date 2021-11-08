b048bf55abfeb1ef953a7ea442c43cdb7b16af8f
Evangelos Pittas <evangelospittas@gmail.com>
2021-11-08 15:07:36 +0000

    Refactor Bank model

--- Sources/PrimerSDK/Classes/Data Models/Bank.swift
+++ Sources/PrimerSDK/Classes/Data Models/Bank.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 struct Bank: Decodable {
-    let id: String?
-    let name: String?
-    let issuer: String?
-    let logoUrlStr: String?
-    lazy var logoUrl: URL? = {
-        guard let logoUrlStr = logoUrlStr else { return nil }
-        return URL(string: logoUrlStr)
+    let id: String
+    let name: String
+    let iconUrlStr: String?
+    lazy var iconUrl: URL? = {
+        guard let iconUrlStr = iconUrlStr else { return nil }
+        return URL(string: iconUrlStr)
     }()
+    let disabled: Bool
     
     private enum CodingKeys: String, CodingKey {
         case id
         case name
-        case issuer
-        case logoUrlStr = "logoUrl"
+        case iconUrlStr = "iconUrl"
+        case disabled
     }
 }
