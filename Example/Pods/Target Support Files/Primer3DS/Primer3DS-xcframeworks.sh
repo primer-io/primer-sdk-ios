5ec94e83810a9c931bdec8a6cc66de9118cb3b6c
Evangelos Pittas <evangelos@primer.io>
2021-11-19 10:09:36 +0200

    Run pod install

--- Example/Pods/Target Support Files/Primer3DS/Primer3DS-xcframeworks.sh
+++ Example/Pods/Target Support Files/Primer3DS/Primer3DS-xcframeworks.sh
@@ -105,5 +105,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/Primer3DS/Sources/Frameworks/ThreeDS_SDK.xcframework" "Primer3DS" "framework" "ios-arm64_i386_x86_64-simulator" "ios-arm64_armv7"
+install_xcframework "${PODS_ROOT}/Primer3DS/Sources/Frameworks/ThreeDS_SDK.xcframework" "Primer3DS" "framework" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator"
 
