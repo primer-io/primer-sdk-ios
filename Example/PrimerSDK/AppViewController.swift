1a4c23da7bd867559e27580bd49ba5ac7e56af74
Evangelos Pittas <evangelos@primer.io>
2021-11-23 16:37:30 +0200

    Bump

--- Example/PrimerSDK/AppViewController.swift
+++ Example/PrimerSDK/AppViewController.swift
@@ -33,7 +33,7 @@ class AppViewController: UIViewController, UIPickerViewDataSource, UIPickerViewD
         currencyTextField.accessibilityIdentifier = "currency_txt_field"
         amountTextField.text = "1.00"
         amountTextField.accessibilityIdentifier = "amount_txt_field"
-        performPaymentSwitch.isOn = false
+        performPaymentSwitch.isOn = true
         performPaymentSwitch.accessibilityIdentifier = "perform_payment_switch"
         
         let countryPicker = UIPickerView()
