9a9f43891423ea6d3ef9ae3e58496e75eae8c7b1
Evangelos Pittas <evangelos@primer.io>
2021-12-27 15:03:00 +0200

    Implement PrimerInternalError protocol. Add major error groups, and their cases.

--- Sources/PrimerSDK/Classes/Error Handler/ErrorHandler.swift
+++ Sources/PrimerSDK/Classes/Error Handler/ErrorHandler.swift
@@ -17,99 +17,32 @@ internal class ErrorHandler {
     func handle(error: Error) -> Bool {
         log(logLevel: .error, title: "ERROR!", message: error.localizedDescription, prefix: nil, suffix: nil, bundle: nil, file: nil, className: nil, function: nil, line: nil)
 
-        if let networkServiceError = error as? NetworkServiceError {
-            switch networkServiceError {
-            default:
-                break
-            }
-
-        } else if let primerError = error as? PrimerError {
-            switch primerError {
-            case .generic:
-                break
-            case .clientTokenNull:
-                break
-            case .customerIDNull:
-                break
-            case .clientTokenExpired:
-                break
-            case .payPalSessionFailed:
-                break
-            case .vaultFetchFailed:
-                break
-            case .vaultDeleteFailed:
-                break
-            case .vaultCreateFailed:
-                break
-            case .directDebitSessionFailed:
-                break
-            case .configFetchFailed:
-                break
-            case .tokenizationPreRequestFailed:
-                break
-            case .tokenizationRequestFailed:
-                break
-            case .threeDSFailed:
-                break
-            case .failedToLoadSession:
-                break
-            case .missingURLScheme:
-                break
-            case .requestFailed:
-                break
-            case .userCancelled:
-                break
-            case .amountCannotBeNullForNonPendingOrderItems:
-                break
-            case .amountShouldBeNullForPendingOrderItems:
-                break
-            case .currencyMissing:
-                break
-            case .amountMissing:
-                break
-            case .billingAddressMissing:
-                break
-            case .billingAddressCityMissing:
-                break
-            case .billingAddressPostalCodeMissing:
-                break
-            case .billingAddressCountryCodeMissing:
-                break
-            case .orderIdMissing:
-                break
-            case .billingAddressAddressLine1Missing:
-                break
-            case .userDetailsMissing:
-                break
-            case .userDetailsAddressMissing:
-                break
-            case .userDetailsCityMissing:
-                break
-            case .userDetailsAddressLine1Missing:
-                break
-            case .userDetailsPostalCodeMissing:
-                break
-            case .userDetailsCountryCodeMissing:
-                break
-            case .dataMissing:
-                break
-            case .directoryServerIdMissing:
-                break
-            case .threeDSSDKKeyMissing:
-                break
-            default:
-                break
-            }
-
-        } else if let klarnaException = error as? KlarnaException {
-            switch klarnaException {
+        var event: Analytics.Event!
+        
+        if let error = error as? PrimerErrorProtocol {
+            event = Analytics.Event(
+                eventType: .message,
+                properties: MessageEventProperties(
+                    message: error.localizedDescription,
+                    messageType: .error,
+                    severity: .error))
+            
+            switch error {
             default:
                 break
             }
 
         } else {
             let nsError = error as NSError
+            event = Analytics.Event(
+                eventType: .message,
+                properties: MessageEventProperties(
+                    message: nsError.localizedDescription,
+                    messageType: .error,
+                    severity: .error))
         }
+        
+        Analytics.Service.record(event: event)
 
         return false
     }
