dc7082835e008b92ad9545b9249559ac6e0b7112
Dario Carlomagno <dario@primer.io>
2022-02-16 16:07:48 +0100

    Added back image supporting "right to left"

---
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UILocalizableUtil.swift
@@ -0,0 +1,13 @@
+//
+//  UILocalizableUtil.swift
+//  PrimerSDK
+//
+//  Created by Dario Carlomagno on 16/02/22.
+//
+
+import Foundation
+
+internal struct UILocalizableUtil {
+    
+    static var isRightToLeftLocale =  UIApplication.shared.userInterfaceLayoutDirection == .rightToLeft
+}
