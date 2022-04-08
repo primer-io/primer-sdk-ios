d3bbb559cd8ebaf791ba4be024e6b0a1d6674777
Evangelos Pittas <evangelos@primer.io>
2022-04-08 16:13:35 +0100

    Bump

--- Sources/PrimerSDK/Classes/Data Models/Payment Method/PaymentMethod.swift
+++ Sources/PrimerSDK/Classes/Data Models/Payment Method/PaymentMethod.swift
@@ -34,25 +34,39 @@ public class PaymentMethod {
                 case paymentInstrument, tokenType, paymentFlow, customerId
             }
             
+            init(
+                paymentInstrument: PaymentMethodTokenizationInstrumentParameters,
+                tokenType: PaymentMethod.TokenType,
+                paymentFlow: PaymentMethod.Flow?,
+                customerId: String?
+            ) {
+                self.paymentInstrument = paymentInstrument
+                self.tokenType = tokenType
+                self.paymentFlow = paymentFlow
+                self.customerId = customerId
+            }
+            
             init(from decoder: Decoder) throws {
                 let container = try decoder.container(keyedBy: CodingKeys.self)
                 
-                if let paymentCardTokenizationInstrumentParameters = (try? container.decode(PaymentCardTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                if let paymentCardTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.PaymentCard.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = paymentCardTokenizationInstrumentParameters
-                } else if let payPalTokenizationInstrumentParameters = (try? container.decode(PayPalTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let payPalTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.PayPal.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = payPalTokenizationInstrumentParameters
-                } else if let applePayTokenizationInstrumentParameters = (try? container.decode(ApplePayTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let applePayTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.ApplePay.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = applePayTokenizationInstrumentParameters
-                } else if let goCardlessTokenizationInstrumentParameters = (try? container.decode(GoCardlessTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let goCardlessTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.GoCardless.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = goCardlessTokenizationInstrumentParameters
-                } else if let klarnaAuthTokenizationInstrumentParameters = (try? container.decode(KlarnaAuthTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let klarnaAuthTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.KlarnaAuth.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = klarnaAuthTokenizationInstrumentParameters
-                } else if let klarnaCustomerTokenizationInstrumentParameters = (try? container.decode(KlarnaCustomerTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let klarnaCustomerTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.KlarnaCustomer.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = klarnaCustomerTokenizationInstrumentParameters
-                } else if let apayaTokenizationInstrumentParameters = (try? container.decode(ApayaTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let apayaTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.Apaya.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = apayaTokenizationInstrumentParameters
-                } else if let dotPayTokenizationInstrumentParameters = (try? container.decode(DotPayTokenizationInstrumentParameters.self, forKey: .paymentInstrument)) {
+                } else if let dotPayTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.DotPay.self, forKey: .paymentInstrument)) {
                     self.paymentInstrument = dotPayTokenizationInstrumentParameters
+                } else if let redirectTokenizationInstrumentParameters = (try? container.decode(PaymentMethod.Tokenization.InstrumentParameters.Redirect.self, forKey: .paymentInstrument)) {
+                    self.paymentInstrument = redirectTokenizationInstrumentParameters
                 } else {
                     fatalError()
                 }
@@ -65,22 +79,24 @@ public class PaymentMethod {
             func encode(to encoder: Encoder) throws {
                 var container = encoder.container(keyedBy: CodingKeys.self)
                 
-                if let paymentCardTokenizationInstrumentParameters = paymentInstrument as? PaymentCardTokenizationInstrumentParameters {
+                if let paymentCardTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.PaymentCard {
                     try container.encode(paymentCardTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let payPalTokenizationInstrumentParameters = paymentInstrument as? PayPalTokenizationInstrumentParameters {
+                } else if let payPalTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.PayPal {
                     try container.encode(payPalTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let applePayTokenizationInstrumentParameters = paymentInstrument as? ApplePayTokenizationInstrumentParameters {
+                } else if let applePayTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.ApplePay {
                     try container.encode(applePayTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let goCardlessTokenizationInstrumentParameters = paymentInstrument as? GoCardlessTokenizationInstrumentParameters {
+                } else if let goCardlessTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.GoCardless {
                     try container.encode(goCardlessTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let klarnaAuthTokenizationInstrumentParameters = paymentInstrument as? KlarnaAuthTokenizationInstrumentParameters {
+                } else if let klarnaAuthTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.KlarnaAuth {
                     try container.encode(klarnaAuthTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let klarnaCustomerTokenizationInstrumentParameters = paymentInstrument as? KlarnaCustomerTokenizationInstrumentParameters {
+                } else if let klarnaCustomerTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.KlarnaCustomer {
                     try container.encode(klarnaCustomerTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let apayaTokenizationInstrumentParameters = paymentInstrument as? ApayaTokenizationInstrumentParameters {
+                } else if let apayaTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.Apaya {
                     try container.encode(apayaTokenizationInstrumentParameters, forKey: .paymentInstrument)
-                } else if let dotPayTokenizationInstrumentParameters = paymentInstrument as? DotPayTokenizationInstrumentParameters {
+                } else if let dotPayTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.DotPay {
                     try container.encode(dotPayTokenizationInstrumentParameters, forKey: .paymentInstrument)
+                } else if let redirectTokenizationInstrumentParameters = paymentInstrument as? PaymentMethod.Tokenization.InstrumentParameters.Redirect {
+                    try container.encode(redirectTokenizationInstrumentParameters, forKey: .paymentInstrument)
                 } else {
                     fatalError()
                 }
--- Sources/PrimerSDK/Classes/Data Models/Payment Method/PaymentMethodTokenizationInstrumentParameters.swift
+++ Sources/PrimerSDK/Classes/Data Models/Payment Method/PaymentMethodTokenizationInstrumentParameters.swift
@@ -11,54 +11,82 @@ import Foundation
 
 protocol PaymentMethodTokenizationInstrumentParameters: Codable {}
 
-struct PaymentCardTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let number: String
-    let cvv: String
-    let expirationMonth: String
-    let expirationYear: String
-    let cardholderName: String?
+extension PaymentMethod.Tokenization {
+    class InstrumentParameters {
+        struct PaymentCard: PaymentMethodTokenizationInstrumentParameters {
+            let number: String
+            let cvv: String
+            let expirationMonth: String
+            let expirationYear: String
+            let cardholderName: String?
+        }
+        
+        struct PayPal: PaymentMethodTokenizationInstrumentParameters {
+            let paypalOrderId: String?
+            let paypalBillingAgreementId: String?
+            let shippingAddress: ShippingAddress?
+            let externalPayerInfo: ExternalPayerInfo?
+        }
+        
+        struct ApplePay: PaymentMethodTokenizationInstrumentParameters {
+            let paymentMethodConfigId: String
+            let token: ApplePayPaymentResponseToken
+            let sourceConfig: ApplePaySourceConfig
+        }
+        
+        struct Redirect: PaymentMethodTokenizationInstrumentParameters {
+            let paymentMethodType: PaymentMethod.PaymentMethodType
+            let paymentMethodConfigId: String
+            lazy var type: String = {
+                "OFF_SESSION_PAYMENT"
+            }()
+            let sessionInfo: SessionInfo
+            
+            struct SessionInfo: Codable {
+                var locale: String
+                var platform: String = "IOS"
+                var redirectionUrl: String? = PrimerSettings.current.urlScheme
+            }
+            
+        }
+
+        struct GoCardless: PaymentMethodTokenizationInstrumentParameters {
+            let gocardlessMandateId: String
+        }
+
+        struct KlarnaAuth: PaymentMethodTokenizationInstrumentParameters {
+            let klarnaAuthorizationToken: String
+        }
+
+        struct KlarnaCustomer: PaymentMethodTokenizationInstrumentParameters {
+            let klarnaCustomerToken: String
+            let sessionData: KlarnaSessionData
+        }
+
+        struct Apaya: PaymentMethodTokenizationInstrumentParameters {
+            let currencyCode: String
+            let hashedIdentifier: String
+            let mcc: String
+            let mnc: String
+            let mx: String
+            let productId: String
+        }
+
+        struct DotPay: PaymentMethodTokenizationInstrumentParameters {
+            let sessionInfo: BankSelectorSessionInfo
+            lazy var type: String = {
+                "OFF_SESSION_PAYMENT"
+            }()
+            let paymentMethodType: String
+        }
+    }
 }
 
-struct PayPalTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let paypalOrderId: String?
-    let paypalBillingAgreementId: String?
-    let shippingAddress: ShippingAddress?
-    let externalPayerInfo: ExternalPayerInfo?
-}
 
-struct ApplePayTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let paymentMethodConfigId: String
-    let token: ApplePayPaymentResponseToken
-    let sourceConfig: ApplePaySourceConfig
-}
 
-struct GoCardlessTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let gocardlessMandateId: String
-}
 
-struct KlarnaAuthTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let klarnaAuthorizationToken: String
-}
 
-struct KlarnaCustomerTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let klarnaCustomerToken: String
-    let sessionData: KlarnaSessionData
-}
 
-struct ApayaTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let currencyCode: String
-    let hashedIdentifier: String
-    let mcc: String
-    let mnc: String
-    let mx: String
-    let productId: String
-}
-
-struct DotPayTokenizationInstrumentParameters: PaymentMethodTokenizationInstrumentParameters {
-    let sessionInfo: BankSelectorSessionInfo
-    let type: String = "OFF_SESSION_PAYMENT"
-    let paymentMethodType: String
-}
 
 #endif
 
