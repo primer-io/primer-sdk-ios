beeade565d0443942722818d2d2f9474b785c584
Dario Carlomagno <dario@primer.io>
2022-04-14 22:50:03 +0200

    Client Session Action BE call works

--- Sources/PrimerSDK/Classes/Core/Payment Services/ClientSessionService.swift
+++ Sources/PrimerSDK/Classes/Core/Payment Services/ClientSessionService.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 internal protocol ClientSessionServiceProtocol {
-    func requestClientSessionWithActions(actionsRequest: ClientSessionActionsRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void)
-    func requestClientSessionWithActions(actionsRequest: ClientSessionActionsRequest) -> Promise<PrimerConfiguration>
+    func requestClientSessionWithActions(actionsRequest: ClientSessionUpdateRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void)
+    func requestClientSessionWithActions(actionsRequest: ClientSessionUpdateRequest) -> Promise<PrimerConfiguration>
 }
 
 internal class ClientSessionService: ClientSessionServiceProtocol {
     
-    func requestClientSessionWithActions(actionsRequest: ClientSessionActionsRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void) {
+    func requestClientSessionWithActions(actionsRequest: ClientSessionUpdateRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void) {
         self.requestClientSessionWithActionsRequest(actionsRequest, completion: completion)
     }
     
-    func requestClientSessionWithActions(actionsRequest: ClientSessionActionsRequest) -> Promise<PrimerConfiguration> {
+    func requestClientSessionWithActions(actionsRequest: ClientSessionUpdateRequest) -> Promise<PrimerConfiguration> {
         return Promise { seal in
             self.requestClientSessionWithActionsRequest(actionsRequest, completion: { configuration, err in
                 if let err = err {
@@ -28,10 +28,15 @@ extension ClientSessionService {
     
     // MARK: - API Request
     
-    private func requestClientSessionWithActionsRequest(_ request: ClientSessionActionsRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void) {
+    private func requestClientSessionWithActionsRequest(_ request: ClientSessionUpdateRequest, completion: @escaping (PrimerConfiguration?, Error?) -> Void) {
+        
+        guard let decodedClientToken = ClientTokenService.decodedClientToken else {
+            completion(nil, nil)
+            return
+        }
         
         let api: PrimerAPIClientProtocol = DependencyContainer.resolve()
-        api.requestClientSessionWithActions(request: request) { result in
+        api.requestClientSessionWithActions(clientToken: decodedClientToken, request: request) { result in
             switch result {
             case .success(let configuration):
                 completion(configuration, nil)
