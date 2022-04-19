618873088851fe09007983a29603b8188c1c1603
Dario Carlomagno <dario@primer.io>
2022-04-19 18:15:11 +0200

    ApplePayTokenizationVM with new SessionActions

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
@@ -164,7 +164,7 @@ class ApplePayTokenizationViewModel: PaymentMethodTokenizationViewModel, Externa
         
         if PrimerDelegateProxy.isClientSessionActionsImplemented {
             let params: [String: Any] = ["paymentMethodType": config.type.rawValue]
-            ClientSession.Action.selectPaymentMethodWithParameters(params)
+            self.selectPaymentMethodWithParameters(params)
         } else {
             continueTokenizationFlow()
         }
@@ -191,11 +191,11 @@ class ApplePayTokenizationViewModel: PaymentMethodTokenizationViewModel, Externa
         .ensure {
             
         }
-        .catch { err in
+        .catch { error in
             DispatchQueue.main.async {
-                ClientSession.Action.unselectPaymentMethod()
-                PrimerDelegateProxy.checkoutFailed(with: err)
-                self.handleFailedTokenizationFlow(error: err)
+                self.unselectPaymentMethodWithError(error)
+                PrimerDelegateProxy.checkoutFailed(with: error)
+                self.handleFailedTokenizationFlow(error: error)
             }
         }
     }
@@ -265,11 +265,11 @@ class ApplePayTokenizationViewModel: PaymentMethodTokenizationViewModel, Externa
             request.paymentSummaryItems = applePayItems
             
             guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request) else {
-                let err = PrimerError.unableToPresentPaymentMethod(paymentMethodType: .applePay, userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"])
-                ErrorHandler.handle(error: err)
-                ClientSession.Action.unselectPaymentMethod()
-                PrimerDelegateProxy.checkoutFailed(with: err)
-                return completion(nil, err)
+                let error = PrimerError.unableToPresentPaymentMethod(paymentMethodType: .applePay, userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"])
+                ErrorHandler.handle(error: error)
+                self.unselectPaymentMethodWithError(error)
+                PrimerDelegateProxy.checkoutFailed(with: error)
+                return completion(nil, error)
             }
             
             paymentVC.delegate = self
@@ -400,14 +400,42 @@ extension ApplePayTokenizationViewModel: PKPaymentAuthorizationViewControllerDel
 
 extension ApplePayTokenizationViewModel {
     
+    private func executeCompletionAndNullifyAfter(error: Error? = nil) {
+        self.completion?(nil, error)
+        self.completion = nil
+    }
+    
+    private func selectPaymentMethodWithParameters(_ parameters: [String: Any]) {
+        
+        firstly {
+            ClientSession.Action.selectPaymentMethodWithParameters(parameters)
+        }
+        .done {}
+        .catch { error in
+            self.handle(error: error)
+        }
+    }
+        
+    private func unselectPaymentMethodWithError(_ error: Error) {
+        firstly {
+            ClientSession.Action.unselectPaymentMethod()
+        }
+        .done {
+            self.applePayControllerCompletion = nil
+        }
+        .catch { error in
+            PrimerDelegateProxy.checkoutFailed(with: error)
+        }
+    }
+}
+
+extension ApplePayTokenizationViewModel {
+    
     override func handle(error: Error) {
         if #available(iOS 11.0, *) {
             self.applePayControllerCompletion?(PKPaymentAuthorizationResult(status: .failure, errors: [error]))
         }
-        ClientSession.Action.unselectPaymentMethod()
-        self.applePayControllerCompletion = nil
-        self.completion?(nil, error)
-        self.completion = nil
+        self.unselectPaymentMethodWithError(error)
     }
     
     override func handle(newClientToken clientToken: String) {
