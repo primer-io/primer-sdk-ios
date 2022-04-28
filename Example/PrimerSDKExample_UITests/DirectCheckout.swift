f3c33b0ddc48b14a8e23687a92bd12fc603976e4
Dario Carlomagno <dario@primer.io>
2022-04-28 18:54:29 +0200

    Fixed 3DS UI Tests

--- Example/PrimerSDKExample_UITests/DirectCheckout.swift
+++ Example/PrimerSDKExample_UITests/DirectCheckout.swift
@@ -41,9 +41,9 @@ class DirectCheckout: XCTestCase {
         let applePay = XCUIApplication(bundleIdentifier: "com.apple.PassbookUIService")
         let applePayExists = expectation(for: Expectation.exists, evaluatedWith: applePay, handler: nil)
         wait(for: [applePayExists], timeout: 15.0)
-        _ = applePay.wait(for: .runningForeground, timeout: 5)
+        _ = applePay.wait(for: .runningForeground, timeout: 10)
 
-        applePay.buttons["Pay Total, €1.19"].tap()
+        applePay.buttons.matching(NSPredicate(format: "label == 'Pay Total, €1.19'")).firstMatch.tap()
     }
 
 
@@ -69,6 +69,28 @@ class DirectCheckout: XCTestCase {
         
         let alertContinueButton = alert.buttons["Continue"]
         alertContinueButton.tap()
+        
+        let email = "sb-358o431493298@personal.example.com"
+        let password = ".H^5MkHX"
+        
+        if !app.staticTexts.matching(NSPredicate(format: "label == '\(email)'")).firstMatch.exists {
+            let emailTextField = app.textFields.matching(NSPredicate(format: "placeholderValue == 'Email address or mobile number'")).firstMatch
+            emailTextField.tap()
+            emailTextField.clearText()
+            emailTextField.typeText(email)
+        }
+        
+        if !app.staticTexts.matching(NSPredicate(format: "label == '\(password)'")).firstMatch.exists {
+            let passwordTextField = app.secureTextFields.matching(NSPredicate(format: "placeholderValue == 'Password'")).firstMatch
+            passwordTextField.tap()
+            passwordTextField.clearText()
+            passwordTextField.typeText(password)
+        }
+
+        let loginButton = app.buttons.matching(NSPredicate(format: "label == 'Log In'")).firstMatch
+        let loginButtonExists = expectation(for: Expectation.exists, evaluatedWith: loginButton, handler: nil)
+        wait(for: [loginButtonExists], timeout: 30)
+        loginButton.tap()
         
         let payNowButton = app.webViews.buttons["Pay Now"].firstMatch
         let payNowButtonExists = expectation(for: Expectation.exists, evaluatedWith: payNowButton, handler: nil)
