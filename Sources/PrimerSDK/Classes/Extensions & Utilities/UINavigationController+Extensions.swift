db76e6b576a722d7dc3bcbdcf2a63f60657593ca
Evangelos Pittas <evangelos@primer.io>
2022-05-03 09:54:55 +0300

    Add navigation controller helpers to get notified on completion of push/pop. Remove old helper for push.

---
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UINavigationController+Extensions.swift
@@ -0,0 +1,50 @@
+//
+//  UINavigationController+Extensions.swift
+//  PrimerSDK
+//
+//  Created by Evangelos on 29/4/22.
+//
+
+#if canImport(UIKit)
+
+import Foundation
+
+extension UINavigationController {
+    func pushViewController(viewController: UIViewController, animated: Bool, completion: @escaping () -> Void) {
+        pushViewController(viewController, animated: animated)
+
+        if animated, let coordinator = transitionCoordinator {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+
+    func popViewController(animated: Bool, completion: @escaping () -> Void) {
+        popViewController(animated: animated)
+
+        if animated, let coordinator = transitionCoordinator {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+    
+    func popToViewController(_ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void) {
+        popToViewController(viewController, animated: animated)
+
+        if animated, let coordinator = transitionCoordinator {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+}
+
+#endif
