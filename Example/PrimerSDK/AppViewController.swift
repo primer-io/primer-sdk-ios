f65ec6c8a50b53f9da0854a2cff6f2bb7a2347d5
Evangelos Pittas <evangelos@primer.io>
2022-05-10 15:46:05 +0300

    Add apple pay cancel logic

--- Example/PrimerSDK/AppViewController.swift
+++ Example/PrimerSDK/AppViewController.swift
@@ -29,7 +29,7 @@ class AppViewController: UIViewController, UIPickerViewDataSource, UIPickerViewD
         phoneNumberTextField.accessibilityIdentifier = "phone_number_txt_field"
         phoneNumberTextField.text = nil
         phoneNumberTextField.accessibilityIdentifier = "phone_number_txt_field"
-        countryCodeTextField.text = CountryCode.de.rawValue
+        countryCodeTextField.text = CountryCode.fr.rawValue
         countryCodeTextField.accessibilityIdentifier = "country_code_txt_field"
         currencyTextField.text = Currency.EUR.rawValue
         currencyTextField.accessibilityIdentifier = "currency_txt_field"
