d7a6741adbbaba2cee2318c0adfe6bef2ab8277e
Dario Carlomagno <dario@primer.io>
2022-05-25 14:55:12 +0300

    Fix Apple Pay and removed commented out code
    - Add business name validation
    - Set order items from settings/client session order items
    - Convert order items to apple pay items and add a total item at the end of the array. Set the item's name to the business name.
    
    # Conflicts:
    #	Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
@@ -77,6 +77,12 @@ class ApplePayTokenizationViewModel: PaymentMethodTokenizationViewModel {
             ErrorHandler.handle(error: err)
             throw err
         }
+        
+        guard settings.businessDetails?.name != nil else {
+            let err = PrimerError.invalidValue(key: "settings.businessDetails.name", value: nil, userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"])
+            ErrorHandler.handle(error: err)
+            throw err
+        }
     }
     
     override func start() {
