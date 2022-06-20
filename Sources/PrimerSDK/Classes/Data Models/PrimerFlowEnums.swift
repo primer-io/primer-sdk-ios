2ad525a4e48fde96f9cd747769b203aa19904c59
Evangelos Pittas <evangelos@primer.io>
2022-06-20 13:50:21 +0300

    Utilize PrimerSessionIntent and remove PaymentFlow

--- Sources/PrimerSDK/Classes/Data Models/PrimerFlowEnums.swift
+++ Sources/PrimerSDK/Classes/Data Models/PrimerFlowEnums.swift
@@ -7,9 +7,9 @@
 
 #if canImport(UIKit)
 
-public enum PrimerSessionIntent: String {
-    case checkout
-    case vault
+public enum PrimerSessionIntent: String, Encodable {
+    case checkout = "CHECKOUT"
+    case vault = "VAULT"
 }
 
 #endif
