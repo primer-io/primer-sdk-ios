6be076672b937c28604c2df2afce1089e6daeb4c
Dario Carlomagno <dario@primer.io>
2022-07-04 00:11:12 +0200

    Minor fixes to comments

--- Sources/PrimerSDK/Classes/Extensions & Utilities/UIDeviceExtension.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UIDeviceExtension.swift
@@ -1,8 +1,8 @@
 //
-//  UIDeviceExtension.swift
+//  Strings.swift
 //  PrimerSDK
 //
-//  Created by Evangelos on 12/11/21.
+//  Copyright © 2022 Primer API ltd. All rights reserved.
 //
 
 #if canImport(UIKit)
