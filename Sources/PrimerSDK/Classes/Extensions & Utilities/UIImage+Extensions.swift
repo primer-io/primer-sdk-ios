9177cd3725774f328bcbb8c89680d30ed6eaabf4
Dario Carlomagno <dario@primer.io>
2022-07-25 23:46:20 +0200

    Added UIImage extension

---
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UIImage+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Extensions.swift
+//  PrimerSDK
+//
+//  Copyright © 2022 Primer API ltd. All rights reserved.
+//
+
+
+extension UIImage {
+    
+    func withColor(_ color: UIColor) -> UIImage? {
+        
+        if #available(iOS 13.0, *) {
+            return self.withTintColor(color)
+        } else {
+            UIGraphicsBeginImageContextWithOptions(size, false, scale)
+            let drawRect = CGRect(x: 0,y: 0,width: size.width,height: size.height)
+            color.setFill()
+            UIRectFill(drawRect)
+            draw(in: drawRect, blendMode: .destinationIn, alpha: 1)
+            let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return tintedImage!
+        }
+    }
+}
