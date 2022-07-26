c015ee2f7fd2e98842e1ebfd53e9210458f0bad0
Dario Carlomagno <dario@primer.io>
2022-07-26 11:15:29 +0200

    Moved UIImageView download to separate extension

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewControllers/QRCodeViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewControllers/QRCodeViewController.swift
@@ -161,28 +161,5 @@ internal class QRCodeViewController: PrimerFormViewController {
     }
 }
 
-extension UIImageView {
-    
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    
-    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
-}
-
 #endif
 
