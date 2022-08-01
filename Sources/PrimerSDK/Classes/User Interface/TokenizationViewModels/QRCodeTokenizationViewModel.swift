2e9fdda49b381548acec967c454a83a6f51b2436
Evangelos Pittas <evangelos@primer.io>
2022-08-01 15:06:39 +0300

    Refactor PayPal, PrimerTest and QR code tokenization view models

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/QRCodeTokenizationViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/QRCodeTokenizationViewModel.swift
@@ -29,7 +29,7 @@ class QRCodeTokenizationViewModel: ExternalPaymentMethodTokenizationViewModel {
         }
     }
     
-    override func startTokenizationFlow() -> Promise<PrimerPaymentMethodTokenData> {
+    override func performPreTokenizationSteps() -> Promise<Void> {
         let event = Analytics.Event(
             eventType: .ui,
             properties: UIEventProperties(
@@ -52,16 +52,35 @@ class QRCodeTokenizationViewModel: ExternalPaymentMethodTokenizationViewModel {
             .then { () -> Promise<Void> in
                 return self.handlePrimerWillCreatePaymentEvent(PrimerPaymentMethodData(type: self.config.type))
             }
-            .then { () -> Promise<PrimerPaymentMethodTokenData> in
-                PrimerDelegateProxy.primerHeadlessUniversalCheckoutTokenizationDidStart(for: self.config.type)
-                return self.tokenize()
-            }
-            .done { tmpPaymentMethodTokenData in
-                seal.fulfill(tmpPaymentMethodTokenData)
+            .done {
+                seal.fulfill()
             }
             .catch { err in
                 seal.reject(err)
             }
+        }
+    }
+    
+    override func performTokenizationStep() -> Promise<Void> {
+        return Promise { seal in
+            PrimerDelegateProxy.primerHeadlessUniversalCheckoutTokenizationDidStart(for: self.config.type)
+
+            firstly {
+                self.tokenize()
+            }
+            .done { paymentMethodTokenData in
+                self.paymentMethodTokenData = paymentMethodTokenData
+                seal.fulfill()
+            }
+            .catch { err in
+                seal.reject(err)
+            }
+        }
+    }
+    
+    override func performPostTokenizationSteps() -> Promise<Void> {
+        return Promise { seal in
+            seal.fulfill()
         }
     }
     
