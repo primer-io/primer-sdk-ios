8a8cf4f85278124f0c66868ca09685414df931eb
Evangelos Pittas <evangelospittas@gmail.com>
2022-08-15 05:09:44 +0300

    HUC raw card data and minor UI Test fixes
    Co-authored-by: Evangelos Pittas <evangelos@primer.io>
    Co-authored-by: Dario Carlomagno <dario@primer.io>

---
+++ Sources/PrimerSDK/Classes/Core/Checkout Components/PrimerRawData.swift
@@ -0,0 +1,82 @@
+//
+//  PrimerRawData.swift
+//  PrimerSDK
+//
+//  Created by Evangelos on 12/7/22.
+//
+
+#if canImport(UIKit)
+
+import Foundation
+
+internal protocol PrimerRawDataProtocol: Encodable {
+    var onDataDidChange: (() -> Void)? { get set }
+}
+
+public class PrimerRawData: NSObject, PrimerRawDataProtocol {
+    
+    var onDataDidChange: (() -> Void)?
+    
+    public func encode(to encoder: Encoder) throws {
+        fatalError()
+    }
+}
+
+public class PrimerCardData: PrimerRawData {
+    
+    public var cardNumber: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var expiryMonth: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var expiryYear: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var cvv: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var cardholderName: String? {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case cardNumber, expiryMonth, expiryYear, cvv, cardholderName
+    }
+        
+    public required init(
+        cardNumber: String,
+        expiryMonth: String,
+        expiryYear: String,
+        cvv: String,
+        cardholderName: String?
+    ) {
+        self.cardNumber = cardNumber
+        self.expiryMonth = expiryMonth
+        self.expiryYear = expiryYear
+        self.cvv = cvv
+        self.cardholderName = cardholderName
+        super.init()
+    }
+    
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(cardNumber, forKey: .cardNumber)
+        try container.encode(expiryMonth, forKey: .expiryMonth)
+        try container.encode(expiryYear, forKey: .expiryYear)
+        try container.encode(cvv, forKey: .cvv)
+        try container.encode(cardholderName, forKey: .cardholderName)
+    }
+}
+
+#endif
