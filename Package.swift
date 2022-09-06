f06de1353b51787659ad3970c6376406c7dc0b40
Evangelos Pittas <evangelos@primer.io>
2022-09-06 10:07:05 +0300

    Add explicit dependency

--- Package.swift
+++ Package.swift
@@ -15,18 +15,23 @@ let package = Package(
             name: "PrimerSDK",
             targets: ["PrimerSDK"]
         )
+//        .product(name: "PrimerKlarnaSDK", package: "primer-klarna-sdk-ios")
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "PrimerKlarnaSDK", path: "https://github.com/primer-io/primer-klarna-sdk-ios.git")
+//        .package(url: "https://github.com/primer-io/primer-klarna-sdk-ios.git", from: "0.5.0")
+        .package(url: "https://github.com/primer-io/primer-klarna-sdk-ios.git", .branch("feature/DEX-2448-Klarna1-SPM-local"))
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PrimerSDK",
-            dependencies: ["PrimerKlarnaSDK"],
+            dependencies: [
+                .product(name: "PrimerKlarnaSDK", package: "primer-klarna-sdk-ios")
+            ],
             path: "Sources/PrimerSDK",
             resources: [
                 .process("Resources"),
