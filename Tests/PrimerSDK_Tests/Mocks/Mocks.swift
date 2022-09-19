4cbdd6312f815f977d553dabe286b6588d8ce148
Evangelos Pittas <evangelos@primer.io>
2022-09-19 10:57:19 +0300

    Remove `TokenizationService` from the dependency container

--- Tests/PrimerSDK_Tests/Mocks/Mocks.swift
+++ Tests/PrimerSDK_Tests/Mocks/Mocks.swift
@@ -346,7 +346,6 @@ class MockLocator {
         // register dependencies
         DependencyContainer.register(mockSettings as PrimerSettingsProtocol)
         DependencyContainer.register(state as AppStateProtocol)
-        DependencyContainer.register(MockTokenizationService(paymentInstrumentType: "PAYMENT_CARD", tokenType: TokenType.singleUse.rawValue) as TokenizationServiceProtocol)
         DependencyContainer.register(MockVaultPaymentMethodViewModel() as VaultPaymentMethodViewModelProtocol)
         DependencyContainer.register(PrimerTheme() as PrimerThemeProtocol)
     }
