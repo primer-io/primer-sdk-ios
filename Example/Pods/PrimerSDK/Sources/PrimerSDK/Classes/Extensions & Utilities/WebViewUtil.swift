a25752c45b6e8be8b251fa10cab9c5de25fdd14c
Dario Carlomagno <dario@primer.io>
2022-09-21 01:24:54 +0200

    Ignored Example/Pods

--- Example/Pods/PrimerSDK/Sources/PrimerSDK/Classes/Extensions & Utilities/WebViewUtil.swift
+++
@@ -1,9 +0,0 @@
-internal class WebViewUtil {
-    static func allowedHostsContain(_ host: String?) -> Bool {
-        guard let host = host else { return false }
-        if (host == "primer.io") { return true }
-        let allowedHost = ".primer.io"
-        let containsHost = host.suffix(allowedHost.count).contains(allowedHost)
-        return containsHost
-    }
-}
