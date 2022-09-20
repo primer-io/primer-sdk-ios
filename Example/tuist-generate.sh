8e44427c7294d238e42428ac0173cfcf72523e2a
Dario Carlomagno <dario@primer.io>
2022-09-21 00:52:27 +0200

    Updated script and Github workflow

--- Example/tuist-generate.sh
+++ Example/tuist-generate.sh
@@ -2,7 +2,9 @@
 
 # Generate Example App project structure
 # via Tuist and Project.swift schema
-tuist generate
+tuist generate --path "Example"
 
-# Install pod for the Example App
-pod install
+if [[ $1 != "is_ci" ]]; then
+    # Install pod for the Example App
+    (cd "Example" && pod install)
+fi
