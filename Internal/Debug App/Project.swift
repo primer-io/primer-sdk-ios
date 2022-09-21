4001066acc03a40f5c17d59c65d8291c9853612d
Dario Carlomagno <dario@primer.io>
2022-09-21 16:07:41 +0200

    Statically adding Primer3DS

--- Internal/Debug App/Project.swift
+++ Internal/Debug App/Project.swift
@@ -5,11 +5,7 @@ enum BaseSettings {
     static let appName = "ExampleApp"
     
     static let settingsDictionary: [String: SettingValue] = [
-        "DEVELOPMENT_TEAM": .string("N8UN9TR5DY"),
-        "ONLY_ACTIVE_ARCH": .string("YES"),
-        "ENABLE_BITCODE": .string("YES"),
-        "LD_RUNPATH_SEARCH_PATHS": .array(["$(inherited)",
-                                           "@executable_path/Frameworks"])
+        "DEVELOPMENT_TEAM": .string("N8UN9TR5DY")
     ]
 }
 
