086ebc819240b190d840fee66bf562c3f0a0e073
Dario Carlomagno <dario@primer.io>
2022-09-21 09:39:39 +0200

    Checking is_ci

--- Example/tuist-generate.sh
+++ Example/tuist-generate.sh
@@ -2,6 +2,11 @@
 
 # Generate Example App project structure
 # via Tuist and Project.swift schema
-tuist generate --path "Example"
+
+if [[ $1 = "is_ci" ]]; then
+  tuist generate --path "Example" --no-open
+else
+  tuist generate --path "Example"
+fi
 
 (cd "Example" && pod install)
