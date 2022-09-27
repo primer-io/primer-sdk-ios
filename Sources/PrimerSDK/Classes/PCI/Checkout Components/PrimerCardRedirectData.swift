51c85fe8cfe41564b4869f22f21feb68e3d352ab
Dario Carlomagno <dario@primer.io>
2022-09-27 19:38:27 +0200

    Implemented Adyen Bancontact in Headless Raw Card

---
+++ Sources/PrimerSDK/Classes/PCI/Checkout Components/PrimerCardRedirectData.swift
@@ -0,0 +1,62 @@
+//
+//  PrimerCardRedirectData.swift
+//  PrimerSDK
+//
+//  Created by Dario Carlomagno on 27/09/22.
+//
+
+#if canImport(UIKit)
+
+import Foundation
+
+public class PrimerCardRedirectData: PrimerRawData {
+    
+    public var cardNumber: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var expiryMonth: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var expiryYear: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    public var cardholderName: String {
+        didSet {
+            self.onDataDidChange?()
+        }
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case cardNumber, expiryMonth, expiryYear, cardholderName
+    }
+        
+    public required init(
+        cardNumber: String,
+        expiryMonth: String,
+        expiryYear: String,
+        cvv: String,
+        cardholderName: String
+    ) {
+        self.cardNumber = cardNumber
+        self.expiryMonth = expiryMonth
+        self.expiryYear = expiryYear
+        self.cardholderName = cardholderName
+        super.init()
+    }
+    
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(cardNumber, forKey: .cardNumber)
+        try container.encode(expiryMonth, forKey: .expiryMonth)
+        try container.encode(expiryYear, forKey: .expiryYear)
+        try container.encode(cardholderName, forKey: .cardholderName)
+    }
+}
+
+#endif
---
+++ Sources/PrimerSDK/Classes/PCI/Checkout Components/PrimerRawCardDataRedirectTokenizationBuilder.swift
@@ -0,0 +1,148 @@
+//
+//  PrimerRawCardDataRedirectTokenizationBuilder.swift
+//  PrimerSDK
+//
+//  Created by Dario Carlomagno on 27/09/22.
+//
+
+#if canImport(UIKit)
+
+import Foundation
+
+class PrimerRawCardDataRedirectTokenizationBuilder: PrimerRawDataTokenizationBuilderProtocol {
+    
+    var rawData: PrimerRawData? {
+        didSet {
+            if let rawCardData = self.rawData as? PrimerCardRedirectData {
+                rawCardData.onDataDidChange = {
+                    _ = self.validateRawData(rawCardData)
+                    
+                    let newCardNetwork = CardNetwork(cardNumber: rawCardData.cardNumber)
+                    if newCardNetwork != self.cardNetwork {
+                        self.cardNetwork = newCardNetwork
+                    }
+                }
+                
+                let newCardNetwork = CardNetwork(cardNumber: rawCardData.cardNumber)
+                if newCardNetwork != self.cardNetwork {
+                    self.cardNetwork = newCardNetwork
+                }
+                
+            } else {
+                if self.cardNetwork != .unknown {
+                    self.cardNetwork = .unknown
+                }
+            }
+            
+            if let rawData = self.rawData {
+                _ = self.validateRawData(rawData)
+            }
+        }
+    }
+    
+    var rawDataManager: PrimerHeadlessUniversalCheckout.RawDataManager? {
+        didSet {
+            self.delegate = rawDataManager?.delegate
+        }
+    }
+    
+    var isDataValid: Bool = false
+    var paymentMethodType: String
+    var delegate: PrimerRawDataManagerDelegate?
+    
+    public private(set) var cardNetwork: CardNetwork = .unknown {
+        didSet {
+            guard let rawDataManager = rawDataManager else {
+                return
+            }
+            
+            rawDataManager.delegate?.primerRawDataManager?(rawDataManager, metadataDidChange: ["cardNetwork": self.cardNetwork.rawValue])
+        }
+    }
+    
+    var requiredInputElementTypes: [PrimerInputElementType] {
+        return [.cardNumber, .expiryDate, .cardholderName]
+    }
+    
+    required init(paymentMethodType: String) {
+        self.paymentMethodType = paymentMethodType
+    }
+    
+    func configureRawDataManager(_ rawDataManager: PrimerHeadlessUniversalCheckout.RawDataManager) {
+        self.rawDataManager = rawDataManager
+    }
+    
+    func makeRequestBodyWithRawData(_ data: PrimerRawData) -> Promise<Request.Body.Tokenization> {
+        return Promise { seal in
+            
+            guard let paymentMethod = PrimerPaymentMethod.getPaymentMethod(withType: paymentMethodType),
+                  let configId = AppState.current.apiConfiguration?.getConfigId(for: paymentMethod.type) else {
+                let err = PrimerError.unsupportedPaymentMethod(paymentMethodType: paymentMethodType, userInfo: nil, diagnosticsId: nil)
+                ErrorHandler.handle(error: err)
+                seal.reject(err)
+                return
+            }
+                        
+            guard let rawData = data as? PrimerCardRedirectData else {
+                let err = PrimerError.invalidValue(key: "rawData", value: nil, userInfo: nil, diagnosticsId: nil)
+                ErrorHandler.handle(error: err)
+                seal.reject(err)
+                return
+            }
+            
+            let paymentInstrument = CardOffSessionPaymentInstrument(paymentMethodConfigId: configId,
+                                                                    paymentMethodType: paymentMethodType,
+                                                                    number: PrimerInputElementType.cardNumber.clearFormatting(value: rawData.cardNumber) as! String,
+                                                                    expirationMonth: rawData.expiryMonth,
+                                                                    expirationYear: rawData.expiryYear,
+                                                                    cardholderName: rawData.cardholderName)
+            
+            let requestBody = Request.Body.Tokenization(paymentInstrument: paymentInstrument)
+            seal.fulfill(requestBody)
+        }
+    }
+    
+    func validateRawData(_ data: PrimerRawData) -> Promise<Void> {
+        return Promise { seal in
+            
+            var errors: [PrimerValidationError] = []
+            
+            guard let rawData = data as? PrimerCardRedirectData, let rawDataManager = rawDataManager else {
+                let err = PrimerValidationError.invalidRawData(userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"], diagnosticsId: nil)
+                errors.append(err)
+                ErrorHandler.handle(error: err)
+                seal.reject(err)
+                return
+            }
+            
+            if !rawData.cardNumber.isValidCardNumber {
+                errors.append(PrimerValidationError.invalidCardnumber(userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"], diagnosticsId: nil))
+            }
+            
+            let expiryDate = rawData.expiryMonth + "/" + rawData.expiryYear.suffix(2)
+            
+            if !expiryDate.isValidExpiryDate {
+                errors.append(PrimerValidationError.invalidExpiryDate(userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"], diagnosticsId: nil))
+            }
+            
+            if self.requiredInputElementTypes.contains(PrimerInputElementType.cardholderName) {
+                if !(rawData.cardholderName).isValidCardholderName {
+                    errors.append(PrimerValidationError.invalidCardholderName(userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"], diagnosticsId: nil))
+                }
+            }
+            
+            if !errors.isEmpty {
+                let err = PrimerError.underlyingErrors(errors: errors, userInfo: ["file": #file, "class": "\(Self.self)", "function": #function, "line": "\(#line)"], diagnosticsId: nil)
+                self.isDataValid = false
+                delegate?.primerRawDataManager?(rawDataManager, dataIsValid: false, errors: errors)
+                seal.reject(err)
+            } else {
+                self.isDataValid = true
+                delegate?.primerRawDataManager?(rawDataManager, dataIsValid: true, errors: nil)
+                seal.fulfill()
+            }
+        }
+    }
+}
+
+#endif
