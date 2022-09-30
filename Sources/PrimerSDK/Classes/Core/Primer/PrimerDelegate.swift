3ef09266932e685402c6fe25b57cca9017b42e88
Evangelos Pittas <evangelospittas@gmail.com>
2022-09-30 13:51:48 +0300

    DEVX-279 (#515)
    Fix HUC error callback
    
    Co-authored-by: Evangelos Pittas <evangelos@primer.io>

--- Sources/PrimerSDK/Classes/Core/Primer/PrimerDelegate.swift
+++ Sources/PrimerSDK/Classes/Core/Primer/PrimerDelegate.swift
@@ -124,8 +124,14 @@ internal class PrimerDelegateProxy {
                 decisionHandler(.fail(withErrorMessage: nil))
                 return
             }
-
-            if Primer.shared.delegate?.primerDidFailWithError != nil {
+            
+            if PrimerHeadlessUniversalCheckout.current.delegate?.primerHeadlessUniversalCheckoutDidFail != nil {
+                DispatchQueue.main.async {
+                    PrimerHeadlessUniversalCheckout.current.delegate?.primerHeadlessUniversalCheckoutDidFail!(withError: exposedError)
+                    decisionHandler(.fail(withErrorMessage: nil))
+                }
+                
+            } else if Primer.shared.delegate?.primerDidFailWithError != nil {
                 Primer.shared.delegate?.primerDidFailWithError?(exposedError, data: data, decisionHandler: { errorDecision in
                     switch errorDecision.type {
                     case .fail(let message):
@@ -135,10 +141,6 @@ internal class PrimerDelegateProxy {
                     }
                 })
                 
-            }
-            
-            if PrimerHeadlessUniversalCheckout.current.delegate?.primerHeadlessUniversalCheckoutDidFail != nil {
-                PrimerHeadlessUniversalCheckout.current.delegate?.primerHeadlessUniversalCheckoutDidFail!(withError: exposedError)
             }
         }
     }
