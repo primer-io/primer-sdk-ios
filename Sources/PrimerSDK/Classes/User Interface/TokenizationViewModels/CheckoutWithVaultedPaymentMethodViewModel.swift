24ecca2f65b915e98b9c45cad6a6e54caf81fd01
Evangelos Pittas <evangelos@primer.io>
2022-10-05 17:17:34 +0300

    Bump

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/CheckoutWithVaultedPaymentMethodViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/CheckoutWithVaultedPaymentMethodViewModel.swift
@@ -338,7 +338,7 @@ class CheckoutWithVaultedPaymentMethodViewModel {
                                     throw err
                                 }
                             
-=                               seal.fulfill(decodedJWTToken)
+                                seal.fulfill(decodedJWTToken)
                             }
                             .catch { err in
                                 seal.reject(err)
