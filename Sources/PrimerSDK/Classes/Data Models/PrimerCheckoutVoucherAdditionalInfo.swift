d2db6e8503dacf3d8f967226f66c136c8875e4b2
Dario Carlomagno <dario@primer.io>
2022-10-19 16:06:41 +0200

    Removed optional from retailerName

--- Sources/PrimerSDK/Classes/Data Models/PrimerCheckoutVoucherAdditionalInfo.swift
+++ Sources/PrimerSDK/Classes/Data Models/PrimerCheckoutVoucherAdditionalInfo.swift
@@ -15,7 +15,7 @@ import Foundation
     
     let expiresAt: String
     let couponCode: String
-    let retailerName: String?
+    let retailerName: String
     
     private enum CodingKeys : String, CodingKey {
         case expiresAt
@@ -23,7 +23,7 @@ import Foundation
         case retailerName
     }
     
-    public init(expiresAt: String, couponCode: String, retailerName: String?) {
+    public init(expiresAt: String, couponCode: String, retailerName: String) {
         self.expiresAt = expiresAt
         self.couponCode = couponCode
         self.retailerName = retailerName
@@ -34,7 +34,7 @@ import Foundation
         let container = try decoder.container(keyedBy: CodingKeys.self)
         expiresAt = try container.decode(String.self, forKey: .expiresAt)
         couponCode = try container.decode(String.self, forKey: .couponCode)
-        retailerName = try? container.decode(String.self, forKey: .retailerName)
+        retailerName = try container.decode(String.self, forKey: .retailerName)
         super.init()
     }
     
@@ -42,7 +42,7 @@ import Foundation
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(expiresAt, forKey: .expiresAt)
         try container.encode(couponCode, forKey: .couponCode)
-        try? container.encode(retailerName, forKey: .retailerName)
+        try container.encode(retailerName, forKey: .retailerName)
     }
 }
 
