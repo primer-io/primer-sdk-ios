56fdade02187478b3c262d1078c9066b7fc4c716
Evangelos Pittas <evangelos@primer.io>
2022-10-26 17:08:24 +0300

    Move function to super class

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/Tokenization Modules/CardTokenizationModule.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/Tokenization Modules/CardTokenizationModule.swift
@@ -117,29 +117,6 @@ class CardTokenizationModule: TokenizationModule {
         }
     }
     
-    override func performTokenizationStep() -> Promise<Void> {
-        return Promise { seal in
-            PrimerDelegateProxy.primerHeadlessUniversalCheckoutTokenizationDidStart(for: self.paymentMethodModule.paymentMethodConfiguration.type)
-
-            firstly {
-                self.paymentMethodModule.checkouEventsNotifierModule.fireDidStartTokenizationEvent()
-            }
-            .then { () -> Promise<PrimerPaymentMethodTokenData> in
-                return self.tokenize()
-            }
-            .then { paymentMethodTokenData -> Promise<Void> in
-                self.paymentMethodTokenData = paymentMethodTokenData
-                return self.paymentMethodModule.checkouEventsNotifierModule.fireDidFinishTokenizationEvent()
-            }
-            .done {
-                seal.fulfill()
-            }
-            .catch { err in
-                seal.reject(err)
-            }
-        }
-    }
-    
     override func tokenize() -> Promise<PrimerPaymentMethodTokenData> {
         return Promise { seal in
             self.cardComponentsManagerTokenizationCompletion = { (paymentMethodTokenData, err) in
