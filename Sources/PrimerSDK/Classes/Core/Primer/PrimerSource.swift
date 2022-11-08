9e6356599d9a6fab4e1a1a443c0ea3cd4b9b183a
Dario Carlomagno <dario@primer.io>
2022-11-08 10:55:37 +0100

    Bump

--- Sources/PrimerSDK/Classes/Core/Primer/PrimerSource.swift
+++ Sources/PrimerSDK/Classes/Core/Primer/PrimerSource.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-@objc public class PrimerSource: NSObject {
+class PrimerSource {
     
-    public static var defaultSourceType: String = Primer.shared.integrationOptions?.reactNativeVersion == nil ? "IOS_NATIVE" : "RN_IOS"
+    static var defaultSourceType: String = Primer.shared.integrationOptions?.reactNativeVersion == nil ? "IOS_NATIVE" : "RN_IOS"
 
     private(set) var sourceType: String
     
-    public init(sourceType: String) {
+    init(sourceType: String) {
         self.sourceType = sourceType
     }
 }
 
 extension PrimerSource {
     
-    public static var iOSNative: PrimerSource {
+    static var sdkSourceType: PrimerSource {
         PrimerSource(sourceType: PrimerSource.defaultSourceType)
     }
 }
