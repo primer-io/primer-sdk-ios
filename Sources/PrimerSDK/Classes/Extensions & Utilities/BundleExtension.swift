3dbd908f12e62a9884d0bc9e18ecdb884ee6f6ab
Evangelos Pittas <evangelospittas@gmail.com>
2023-04-10 15:11:32 +0300

    Bump events sdk version for rc.8

--- Sources/PrimerSDK/Classes/Extensions & Utilities/BundleExtension.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/BundleExtension.swift
@@ -34,8 +34,8 @@ internal extension Bundle {
         }
         let version = Bundle.primerFramework.infoDictionary?["CFBundleShortVersionString"] as? String
         
-        if version != "2.17.0-rc.6" {
-            return "2.17.0-rc.6"
+        if version != "2.17.0-rc.8" {
+            return "2.17.0-rc.8"
         } else {
             return version
         }
