306bb4d240c57eb55433894b9a2face36360768b
Evangelos Pittas <evangelospittas@gmail.com>
2023-06-01 18:39:40 +0300

    Fix tests

--- Internal/Debug App/Tests/Unit Tests/Primer/PrimerRawRetailerDataTests.swift
+++ Internal/Debug App/Tests/Unit Tests/Primer/PrimerRawRetailerDataTests.swift
@@ -14,35 +14,45 @@ import XCTest
 class PrimerRawRetailerDataTests: XCTestCase {
     
     func test_invalid_raw_retail_data() throws {
+        let exp = expectation(description: "Await validation")
         
         let rawRetailData = PrimerRetailerData(id: "")
-        
+
         let tokenizationBuilder = PrimerRawRetailerDataTokenizationBuilder(paymentMethodType: "XENDIT_RETAIL_OUTLETS")
-        
+
         firstly {
             return tokenizationBuilder.validateRawData(rawRetailData)
         }
         .done {
             XCTAssert(false, "Card data should not pass validation")
+            exp.fulfill()
         }
-        .catch { _ in }
+        .catch { _ in
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 0.1)
     }
     
     func test_valid_raw_retail_data() throws {
+        let exp = expectation(description: "Await validation")
         
         let rawRetailData = PrimerRetailerData(id: "test")
-        
+
         let tokenizationBuilder = PrimerRawRetailerDataTokenizationBuilder(paymentMethodType: "XENDIT_RETAIL_OUTLETS")
-        
+
         firstly {
             return tokenizationBuilder.validateRawData(rawRetailData)
         }
         .done { _ in
-            // Continue
+            exp.fulfill()
         }
         .catch { _ in
             XCTAssert(false, "Card data should pass validation")
+            exp.fulfill()
         }
+        
+        wait(for: [exp], timeout: 0.1)
     }
 
 }
