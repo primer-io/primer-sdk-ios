772d0f3ff8c613b571e8dd937ae2a487ae11bf12
Evangelos Pittas <evangelospittas@gmail.com>
2023-07-12 16:13:36 +0300

    Perform a cleanup at the end of the 3DS flow

--- Sources/PrimerSDK/Classes/Core/3DS/3DSService.swift
+++ Sources/PrimerSDK/Classes/Core/3DS/3DSService.swift
@@ -99,6 +99,7 @@ class ThreeDSService: ThreeDSServiceProtocol {
             
             self.threeDSSDKWindow?.isHidden = true
             self.threeDSSDKWindow = nil
+            self.primer3DS.cleanup()
         }
         .catch { err in
             var continueInfo: ThreeDS.ContinueInfo?
