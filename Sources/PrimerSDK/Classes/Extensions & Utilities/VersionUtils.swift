02f3dd8826e5d673aff735daeb8856d95b621dd2
Niall Quinn <niall@primer.io>
2023-09-26 11:49:57 +0200

    PR cleanup

--- Sources/PrimerSDK/Classes/Extensions & Utilities/VersionUtils.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/VersionUtils.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 struct VersionUtils {
+    
+    /**
+     Returns the version string in the format `"major.minor.patch"`
+     
+     If `PrimerIntegrationOptions.reactNativeVersion` is set, it will be returned.
+     If not, the version specified as `PrimerSDKVersion` in the file `"sources/version.swift"` will be returned.
+     */
     static var releaseVersionNumber: String? {
         if let reactNativeVersion = Primer.shared.integrationOptions?.reactNativeVersion {
             return reactNativeVersion
