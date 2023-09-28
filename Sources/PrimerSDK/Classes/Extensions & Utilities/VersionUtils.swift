cfbc8a6c70a95c5b2c5fe3785e73968ec7aa06f3
Niall Quinn <niall@primer.io>
2023-09-28 09:56:33 +0200

    chore: use version.swift file (#681)
    * Added VersionUtils.swift
    
    * Added VersionUtils and Tests
    
    * Add VersionUtilsTests to project
    
    * Update callsites

---
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/VersionUtils.swift
@@ -0,0 +1,25 @@
+//
+//  VersionUtils.swift
+//  PrimerSDK
+//
+//  Created by Niall Quinn on 26/09/23.
+//
+
+import Foundation
+
+struct VersionUtils {
+    
+    /**
+     Returns the version string in the format `"major.minor.patch"`
+     
+     If `PrimerIntegrationOptions.reactNativeVersion` is set, it will be returned.
+     If not, the version specified as `PrimerSDKVersion` in the file `"sources/version.swift"` will be returned.
+     */
+    static var releaseVersionNumber: String? {
+        if let reactNativeVersion = Primer.shared.integrationOptions?.reactNativeVersion {
+            return reactNativeVersion
+        }
+    
+        return PrimerSDKVersion
+    }
+}
