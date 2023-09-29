83bce0ad11f60d2d9d152a28bb8f495689c8432d
Jack Newcombe <jack.newcombe@primer.io>
2023-09-29 17:27:23 +0100

    Remove Klarna and iPay 88 to avoid upgrade issues

--- Debug App/Pods/Target Support Files/Pods-Debug App/Pods-Debug App-frameworks.sh
+++ Debug App/Pods/Target Support Files/Pods-Debug App/Pods-Debug App-frameworks.sh
@@ -177,18 +177,12 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Primer3DS/Primer3DS.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PrimerIPay88MYSDK/PrimerIPay88MYSDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PrimerKlarnaSDK/PrimerKlarnaSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PrimerSDK/PrimerSDK.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KlarnaMobileSDK/full/KlarnaMobileSDK.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Primer3DS/ThreeDS_SDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Primer3DS/Primer3DS.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PrimerIPay88MYSDK/PrimerIPay88MYSDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PrimerKlarnaSDK/PrimerKlarnaSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PrimerSDK/PrimerSDK.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KlarnaMobileSDK/full/KlarnaMobileSDK.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Primer3DS/ThreeDS_SDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
