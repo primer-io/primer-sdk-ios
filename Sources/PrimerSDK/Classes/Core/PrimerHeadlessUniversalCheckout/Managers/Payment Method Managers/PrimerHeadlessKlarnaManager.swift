5ed9e777047bbf0cd45e10b9ab57f68fc715ee76
Illia Khrypunov <ikhrypunov@n-ix.com>
2023-11-06 10:22:49 +0100

    Basic implementation of headless Klarna manager

---
+++ Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Managers/Payment Method Managers/PrimerHeadlessKlarnaManager.swift
@@ -0,0 +1,28 @@
+//
+//  PrimerHeadlessKlarnaManager.swift
+//  PrimerSDK
+//
+//  Created by Illia Khrypunov on 06.11.2023.
+//
+
+#if canImport(PrimerKlarnaSDK)
+import Foundation
+import PrimerKlarnaSDK
+
+extension PrimerHeadlessUniversalCheckout {
+    
+    public class PrimerHeadlessKlarnaManager: NSObject {
+        // MARK: - Provider
+        
+        
+        // MARK: - Components
+        
+        
+        // MARK: - Init
+        override init() {
+            super.init()
+        }
+    }
+    
+}
+#endif
