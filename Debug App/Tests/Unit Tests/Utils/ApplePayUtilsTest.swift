620f0f1ceb53cfd75652cd6f57971b4512a0eeb0
Jack Newcombe <jack.newcombe@primer.io>
2023-11-09 09:21:51 +0000

    Fix apple pay utils tests

--- Debug App/Tests/Unit Tests/Utils/ApplePayUtilsTest.swift
+++ Debug App/Tests/Unit Tests/Utils/ApplePayUtilsTest.swift
@@ -13,28 +13,28 @@ final class ApplePayUtilsTest: XCTestCase {
 
     func testApplePaySupportedPaymentNetworks() {
         // w/ CB
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: [.cartesBancaires]), [.cartesBancaires])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: [.cartesBancaires])), Set([.cartesBancaires]))
         
         // w/ mixed
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: [.visa, .masterCard, .amex, .cartesBancaires]), [.visa, .masterCard, .amex, .cartesBancaires])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: [.visa, .masterCard, .amex, .cartesBancaires])), Set([.visa, .masterCard, .amex, .cartesBancaires]))
         
         // w/ none
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: []), [])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks(cardNetworks: [])), Set())
     }
     
     func testApplePaySupportedPaymentNetworksViaPrimerSettings() {
         
         // w/ CB
         PrimerSettings.current.paymentMethodOptions.cardPaymentOptions = .init(supportedCardNetworks: [.cartesBancaires])
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(), [.cartesBancaires])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks()), Set([.cartesBancaires]))
         
         // w/ mixed
         PrimerSettings.current.paymentMethodOptions.cardPaymentOptions = .init(supportedCardNetworks: [.visa, .masterCard, .amex, .cartesBancaires])
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(), [.visa, .masterCard, .amex, .cartesBancaires])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks()), Set([.visa, .masterCard, .amex, .cartesBancaires]))
         
         // w/ none
         PrimerSettings.current.paymentMethodOptions.cardPaymentOptions = .init(supportedCardNetworks: [])
-        XCTAssertEqual(ApplePayUtils.supportedPKPaymentNetworks(), [])
+        XCTAssertEqual(Set(ApplePayUtils.supportedPKPaymentNetworks()), Set())
     }
 
 }
