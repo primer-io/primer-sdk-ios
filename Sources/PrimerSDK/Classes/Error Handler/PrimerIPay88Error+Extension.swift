b0237129944ef1e221600748d1f2a0cd7dc364da
Niall Quinn <niall@primer.io>
2023-12-06 15:55:38 +0100

    Extend Klarna and iPay88 errors with PrimerErrorProtocol

--- Sources/PrimerSDK/Classes/Error Handler/PrimerIPay88Error+Extension.swift
+++ Sources/PrimerSDK/Classes/Error Handler/PrimerIPay88Error+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  PrimerKlarnaError+Extension.swift
+//  PrimerIPay88Error+Extension.swift
 //  PrimerSDK
 //
 //  Created by Niall Quinn on 06/12/23.
@@ -7,40 +7,44 @@
 
 import Foundation
 
-#if canImport(PrimerKlarnaSDK)
-import PrimerKlarnaSDK
-extension PrimerKlarnaError: PrimerErrorProtocol {
+#if canImport(PrimerIPay88MYSDK)
+import PrimerIPay88MYSDK
+extension PrimerIPay88Error: PrimerErrorProtocol {
     var exposedError: Error {
         self
+    }
+ 
+    var errorId: String {
+        switch self {
+        case .iPay88Error:
+            return "ipay88"
+        }
     }
     
     var info: [String : Any]? {
         var tmpUserInfo: [String: String] = ["createdAt": Date().toString()]
         
         switch self {
-        case .userNotApproved(let userInfo),
-                .klarnaSdkError(_, let userInfo):
+        case .iPay88Error(let description, let userInfo):
             tmpUserInfo = tmpUserInfo.merging(userInfo ?? [:]) { (_, new) in new }
+            tmpUserInfo["description"] = description
             tmpUserInfo["diagnosticsId"] = self.diagnosticsId
         }
         
         return tmpUserInfo
     }
     
- 
-    var errorId: String {
+    public var errorDescription: String? {
         switch self {
-        case .userNotApproved:
-            return "klarna-user-not-approved"
-        case .klarnaSdkError:
-            return "klarna-sdk-error"
+        case .iPay88Error(let description, _):
+            return "[\(errorId)] iPay88 failed with error \(description) (diagnosticsId: \(self.diagnosticsId))"
         }
     }
     
     var analyticsContext: [String : Any] {
         [
             AnalyticsContextKeys.errorId: errorId,
-            AnalyticsContextKeys.paymentMethodType: PrimerPaymentMethodType.klarna.rawValue
+            AnalyticsContextKeys.paymentMethodType: PrimerPaymentMethodType.iPay88Card.rawValue
         ]
     }
     
