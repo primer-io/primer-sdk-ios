c116fe40e20c5e4570dc872f7ac99cdc4dad9a88
Jack Newcombe <jack.newcombe@primer.io>
2024-01-09 16:35:33 +0000

    Test fix

--- Debug App/Tests/Unit Tests/Primer/NativeUIManagerTests.swift
+++ Debug App/Tests/Unit Tests/Primer/NativeUIManagerTests.swift
@@ -12,14 +12,6 @@ import XCTest
 
 final class NativeUIManagerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testNativeUIManagerWithUninitializedSDK() throws {
         do {
             _ = try PrimerHeadlessUniversalCheckout.NativeUIManager(paymentMethodType: "PAYMENT_CARD")
@@ -34,8 +26,9 @@ final class NativeUIManagerTests: XCTestCase {
         SDKSessionHelper.setUp()
         do {
             _ = try PrimerHeadlessUniversalCheckout.NativeUIManager(paymentMethodType: "PAYMENT_CARD")
-        } catch PrimerError.unsupportedPaymentMethod(let type, _, _) {
+        } catch PrimerError.unsupportedPaymentMethodForManager(let type, let category, _, _) {
             XCTAssertEqual(type, "PAYMENT_CARD")
+            XCTAssertEqual(category, "NATIVE_UI")
         } catch {
             XCTFail("Unexpected error type. Should be `uninitializedSDKSession`")
         }
