9b8d07832ffeaa6d91dbe115976d26e1e0319e63
Niall Quinn <niall@primer.io>
2024-02-01 10:25:22 +0100

    fix typo

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -82,7 +82,7 @@ if pr.assignees?.count == 0 {
 // MARK: - Conventional Commit Title
 let validPrefixes = ["fix", "feat", "chore", "ci", "refactor", "docs", 
                     "perf", "test", "build", "revert", "style", "BREAKING CHANGE"]
-let isConventionalCommitTitle = prefixes.contains { pr.title.hasPrefix($0) }
+let isConventionalCommitTitle = validPrefixes.contains { pr.title.hasPrefix($0) }
 
 if !pr.head.ref.hasPrefix("release") && !isConventionalCommitTitle {
     fail("Please use a conventional commit title for this PR. See [Conventional Commits and SemVer](https://www.notion.so/primerio/Automating-Version-Bumping-and-Changelog-Creation-c13e32fea11447069dea76f966f4b0fb?pvs=4#c55764aa2f2748eb988d581a456e61e7)")
