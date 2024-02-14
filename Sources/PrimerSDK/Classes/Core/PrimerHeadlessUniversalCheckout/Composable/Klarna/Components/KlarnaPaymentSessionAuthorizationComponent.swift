c218e21b6f9db5de94b20054e86cac8ed97f2bce
StefanV-PRIMERIO <stefan@ext.primer.io>
2024-02-14 16:44:26 +0200

    chore: added nits and comments to Authorization and Creation components

--- Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Composable/Klarna/Components/KlarnaPaymentSessionAuthorizationComponent.swift
+++ Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Composable/Klarna/Components/KlarnaPaymentSessionAuthorizationComponent.swift
@@ -9,6 +9,19 @@
 import Foundation
 import PrimerKlarnaSDK
 
+/**
+ * Enumerates the possible states and outcomes related to the authorization of a Klarna payment session.
+ * This enum is designed to communicate the various stages of authorization (and reauthorization, if necessary) for a Klarna payment session.
+ * It conforms to `PrimerHeadlessStep`.
+ *
+ * - Cases:
+ *  - paymentSessionAuthorized: Indicates that the payment session has been successfully authorized. It carries an `authToken` string for subsequent operations that require authorization.
+ *  - paymentSessionAuthorizationFailed: Represents a failure in the authorization process.
+ *  - paymentSessionFinalizationRequired: Signals that the payment session requires finalization steps to be completed by the user or the system.
+ *
+ *  - paymentSessionReauthorized: Similar to `paymentSessionAuthorized`.
+ *  - paymentSessionReauthorizationFailed: Indicates a failure in the reauthorization process of an existing payment session.
+ */
 public enum KlarnaPaymentSessionAuthorization: PrimerHeadlessStep {
     case paymentSessionAuthorized(authToken: String)
     case paymentSessionAuthorizationFailed
@@ -33,28 +46,41 @@ public class KlarnaPaymentSessionAuthorizationComponent: PrimerHeadlessAnalytics
         self.tokenizationComponent = tokenizationComponent
     }
     
-    // MARK: - Set
+    // MARK: - Set Klarna provider
     func setProvider(provider: PrimerKlarnaProviding?) {
-        self.klarnaProvider = provider
-        self.klarnaProvider?.authorizationDelegate = self
+        klarnaProvider = provider
+        klarnaProvider?.authorizationDelegate = self
     }
 }
 
-// MARK: - Authorization
+// MARK: - Authorization / Reauthorize session
 public extension KlarnaPaymentSessionAuthorizationComponent {
     func authorizeSession(autoFinalize: Bool, jsonData: String? = nil) {
         recordAuthorizeEvent(name: KlarnaAnalyticsEvents.AUTHORIZE_SESSION_METHOD, autoFinalize: autoFinalize, jsonData: jsonData)
-        self.klarnaProvider?.authorize(autoFinalize: autoFinalize, jsonData: jsonData)
+        klarnaProvider?.authorize(autoFinalize: autoFinalize, jsonData: jsonData)
     }
     
     func reauthorizeSession(jsonData: String? = nil) {
         recordAuthorizeEvent(name: KlarnaAnalyticsEvents.REAUTHORIZE_SESSION_METHOD, jsonData: jsonData)
-        self.klarnaProvider?.reauthorize(jsonData: jsonData)
+        klarnaProvider?.reauthorize(jsonData: jsonData)
     }
 }
 
-// MARK: - Private
+// MARK: - Finalize session
 private extension KlarnaPaymentSessionAuthorizationComponent {
+    
+    /**
+     * Finalizes the payment session with specific authorization and tokenization processes.
+     *
+     * - Parameters:
+     *   - token: A `String` representing the authorization token used for payment session authorization.
+     *            This token is necessary for both the initial authorization request and the tokenization process that follows.
+     *   - reauthorization: A `Bool` indicating whether the current operation is a reauthorization.
+     *
+     * This method first attempts to authorize the payment session using the provided `token`.
+     * Upon successful authorization, it proceeds to tokenize the customer token received in response.
+     * Based on the `reauthorization` flag, it then determines the correct step to proceed with.
+     */
     func finalizeSession(token: String, reauthorization: Bool) {
         firstly {
             tokenizationComponent.authorizePaymentSession(authorizationToken: token)
@@ -78,35 +104,50 @@ private extension KlarnaPaymentSessionAuthorizationComponent {
 
 // MARK: - PrimerKlarnaProviderAuthorizationDelegate
 extension KlarnaPaymentSessionAuthorizationComponent: PrimerKlarnaProviderAuthorizationDelegate {
+    
+    /**
+     * Handles the authorization response from the Primer Klarna Wrapper.
+     * This function is called in response to the authorization attempt via the Primer Klarna Wrapper.
+     * It processes the result of the authorization attempt, which can lead to various outcomes based on the combination of:
+     *  - `approved` - A `Bool` indicating whether the authorization was approved or not.
+     *  -  `authToken` - An optional `String` containing the authorization token.  Returned only if `approved` is `true`.
+     *  - `finalizeRequired`. - A `Bool` indicating whether additional steps are required to finalize the payment session.
+     */
     public func primerKlarnaWrapperAuthorized(approved: Bool, authToken: String?, finalizeRequired: Bool) {
         if approved == false {
             if finalizeRequired == true {
                 let step = KlarnaPaymentSessionAuthorization.paymentSessionFinalizationRequired
-                self.stepDelegate?.didReceiveStep(step: step)
+                stepDelegate?.didReceiveStep(step: step)
             } else {
                 let step = KlarnaPaymentSessionAuthorization.paymentSessionAuthorizationFailed
-                self.stepDelegate?.didReceiveStep(step: step)
+                stepDelegate?.didReceiveStep(step: step)
             }
         }
         
         if let authToken = authToken, approved == true {
-            self.finalizeSession(token: authToken, reauthorization: false)
+            finalizeSession(token: authToken, reauthorization: false)
         }
         
         if finalizeRequired == true {
             let step = KlarnaPaymentSessionAuthorization.paymentSessionFinalizationRequired
-            self.stepDelegate?.didReceiveStep(step: step)
+            stepDelegate?.didReceiveStep(step: step)
         }
     }
     
+    /**
+     * Handles the re-authorization response from the Primer Klarna Wrapper.
+     * It processes the result of the re-authorization attempt, which can lead to various outcomes based on the combination of:
+     *  - `approved` - A `Bool` indicating whether the authorization was approved or not.
+     *  -  `authToken` - An optional `String` containing the authorization token.  Returned only if `approved` is `true`.
+     */
     public func primerKlarnaWrapperReauthorized(approved: Bool, authToken: String?) {
         if approved == false {
             let step = KlarnaPaymentSessionAuthorization.paymentSessionReauthorizationFailed
-            self.stepDelegate?.didReceiveStep(step: step)
+            stepDelegate?.didReceiveStep(step: step)
         }
         
         if let authToken = authToken, approved == true {
-            self.finalizeSession(token: authToken, reauthorization: true)
+            finalizeSession(token: authToken, reauthorization: true)
         }
     }
 }
--- Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Composable/Klarna/Components/KlarnaPaymentSessionCreationComponent.swift
+++ Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Composable/Klarna/Components/KlarnaPaymentSessionCreationComponent.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 
+/**
+ * Represents the possible outcomes of a Klarna payment session creation process.
+ *
+ * This enum is used to communicate the result of attempting to create a payment session with Klarna.
+ * It conforms to `PrimerHeadlessStep`.
+ *
+ * Cases:
+ * - paymentSessionCreated: Indicates a successful creation of a payment session. It caries:
+ *     - `clientToken` string, which is used for further API interactions.
+ *     - `paymentCategories` of type `KlarnaPaymentCategory`, representing the available payment options for the user.
+ */
 public enum KlarnaPaymentSessionCreation: PrimerHeadlessStep {
     case paymentSessionCreated(clientToken: String, paymentCategories: [KlarnaPaymentCategory])
 }
 
+/**
+ * Defines the specific errors that can be encountered during the Klarna payment session creation process.
+ * This enum categorizes errors specific to the Klarna payment session creation component.
+ *
+ * - Cases:
+ *  - missingConfiguration: Indicates that essential configuration details are missing, which are required to initiate the payment session creation process.
+ *  - invalidClientToken: Signifies that the client token provided for the session creation is invalid or malformed, preventing further API interactions.
+ *  - createPaymentSessionFailed: Represents a failure in the payment session creation process, encapsulating the underlying `Error` that led to the failure.
+ */
 enum KlarnaPaymentSessionCreationComponentError {
     case missingConfiguration
     case invalidClientToken
@@ -38,6 +58,13 @@ public class KlarnaPaymentSessionCreationComponent: PrimerHeadlessAnalyticsRecor
 
 // MARK: - Start
 public extension KlarnaPaymentSessionCreationComponent {
+    
+    /**
+     * Initiates the process of creating a payment session.
+     * This method kicks off the payment session creation process by first recording the creation event for tracking or analytical purposes.
+        - Success: it handles the creation of a payment session step
+        - Failure: It handles the creation of a payment session error
+     */
     func start() {
         recordCreationEvent()
         
@@ -45,25 +72,41 @@ public extension KlarnaPaymentSessionCreationComponent {
             tokenizationComponent.createPaymentSession()
         }
         .done { paymentSession in
-            self.handleSuccess(success: paymentSession)
+            self.createSessionStep(paymentSession)
         }
         .catch { error in
-            self.handleError(error: .createPaymentSessionFailed(error: error))
+            self.createSessionError(.createPaymentSessionFailed(error: error))
         }
     }
 }
 
 // MARK: - Private
 private extension KlarnaPaymentSessionCreationComponent {
-    func handleSuccess(success: Response.Body.Klarna.PaymentSession) {
+    
+    /**
+     * Processes and communicates the successful creation of a payment session.
+     * This method takes the response from a successful payment session creation and extracts necessary details to form a `KlarnaPaymentSessionCreation` step.
+     * It encapsulates the client token and payment categories from the response into this step.
+     * Then notifies the `stepDelegate` of this successful step
+     */
+    func createSessionStep(_ response: Response.Body.Klarna.PaymentSession) {
         let step = KlarnaPaymentSessionCreation.paymentSessionCreated(
-            clientToken: success.clientToken,
-            paymentCategories: success.categories.map { KlarnaPaymentCategory(response: $0) }
+            clientToken: response.clientToken,
+            paymentCategories: response.categories.map { KlarnaPaymentCategory(response: $0) }
         )
         stepDelegate?.didReceiveStep(step: step)
     }
     
-    func handleError(error: KlarnaPaymentSessionCreationComponentError) {
+    /**
+     * Handles errors encountered during the payment session creation process.
+     * This method processes a specific `KlarnaPaymentSessionCreationComponentError` and converts it into a more generalized `PrimerError`.
+     *
+     * - Parameter error: A `KlarnaPaymentSessionCreationComponentError` representing the specific error encountered during the payment session creation process.
+     *
+     * This method utilizes a switch statement to differentiate between various types of `KlarnaPaymentSessionCreationComponentError`, and constructs a specific `PrimerError`.
+     * Then notifies the `errorDelegate` with the specific `PrimerError`.
+     */
+    func createSessionError(_ error: KlarnaPaymentSessionCreationComponentError) {
         var primerError: PrimerError
         
         let userInfo: [String: String] = [
