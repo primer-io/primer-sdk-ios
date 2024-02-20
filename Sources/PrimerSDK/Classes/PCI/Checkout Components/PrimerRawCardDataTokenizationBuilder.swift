92c719a9667a68c9fec5ffdf8e90289e8ff5c7e7
Jack Newcombe <jack.newcombe@primer.io>
2024-02-20 15:24:34 +0000

    Align headless card validation logic with android

--- Sources/PrimerSDK/Classes/PCI/Checkout Components/PrimerRawCardDataTokenizationBuilder.swift
+++ Sources/PrimerSDK/Classes/PCI/Checkout Components/PrimerRawCardDataTokenizationBuilder.swift
@@ -42,6 +42,8 @@ class PrimerRawCardDataTokenizationBuilder: PrimerRawDataTokenizationBuilderProt
 
     var cardValidationService: CardValidationService?
 
+    private var lastValidationErrors: [PrimerValidationError] = []
+
     var isDataValid: Bool = false
     var paymentMethodType: String
 
@@ -243,6 +245,12 @@ class PrimerRawCardDataTokenizationBuilder: PrimerRawDataTokenizationBuilderProt
                             diagnosticsId: UUID().uuidString))
                     }
                 }
+
+                let newValidationErrorsPresent = errors.map { $0.errorDescription } != lastValidationErrors.map { $0.errorDescription }
+                guard newValidationErrorsPresent else {
+                    return
+                }
+                self.lastValidationErrors = errors
 
                 if !errors.isEmpty {
                     let err = PrimerError.underlyingErrors(
