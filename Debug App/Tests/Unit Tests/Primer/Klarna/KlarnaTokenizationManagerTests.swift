eb8b672f8f5ad9ddacc94e25d2653efc391b300b
Niall Quinn <niall@primer.io>
2024-03-12 12:08:36 +0100

    disable test extension

--- Debug App/Tests/Unit Tests/Primer/Klarna/KlarnaTokenizationManagerTests.swift
+++ Debug App/Tests/Unit Tests/Primer/Klarna/KlarnaTokenizationManagerTests.swift
@@ -63,53 +63,53 @@ final class KlarnaTokenizationManagerTests: XCTestCase {
     
 }
 
-extension KlarnaTokenizationManagerTests {
-    private func setupPrimerConfiguration(paymentMethod: PrimerPaymentMethod, apiConfiguration: PrimerAPIConfiguration) {
-        let mockApiClient = MockPrimerAPIClient()
-        mockApiClient.fetchConfigurationWithActionsResult = (apiConfiguration, nil)
-        mockApiClient.mockSuccessfulResponses()
-        
-        AppState.current.clientToken = KlarnaTestsMocks.clientToken
-        PrimerAPIConfigurationModule.apiClient = mockApiClient
-        PrimerAPIConfigurationModule.clientToken = KlarnaTestsMocks.clientToken
-        PrimerAPIConfigurationModule.apiConfiguration = apiConfiguration
-        
-        tokenizationComponent = KlarnaTokenizationComponent(paymentMethod: paymentMethod)
-    }
-    
-    private func prepareConfigurations() {
-        PrimerInternal.shared.intent = .checkout
-        let clientSession = KlarnaTestsMocks.getClientSession()
-        let successApiConfiguration = KlarnaTestsMocks.getMockPrimerApiConfiguration(clientSession: clientSession)
-        successApiConfiguration.paymentMethods?[0].baseLogoImage = PrimerTheme.BaseImage(colored: UIImage(), light: nil, dark: nil)
-        setupPrimerConfiguration(paymentMethod: Mocks.PaymentMethods.klarnaPaymentMethod, apiConfiguration: successApiConfiguration)
-    }
-    
-    private func restartPrimerConfiguration() {
-        AppState.current.clientToken = nil
-        PrimerAPIConfigurationModule.clientToken = nil
-        PrimerAPIConfigurationModule.apiConfiguration = nil
-        PrimerAPIConfigurationModule.apiClient = nil
-        tokenizationComponent = nil
-    }
-    
-    private func getInvalidTokenError() -> PrimerError {
-        let error = PrimerError.invalidClientToken(
-            userInfo: self.getErrorUserInfo(),
-            diagnosticsId: UUID().uuidString
-        )
-        ErrorHandler.handle(error: error)
-        return error
-    }
-    
-    private func getErrorUserInfo() -> [String: String] {
-        return [
-            "file": #file,
-            "class": "\(Self.self)",
-            "function": #function,
-            "line": "\(#line)"
-        ]
-    }
-}
+//extension KlarnaTokenizationManagerTests {
+//    private func setupPrimerConfiguration(paymentMethod: PrimerPaymentMethod, apiConfiguration: PrimerAPIConfiguration) {
+//        let mockApiClient = MockPrimerAPIClient()
+//        mockApiClient.fetchConfigurationWithActionsResult = (apiConfiguration, nil)
+//        mockApiClient.mockSuccessfulResponses()
+//        
+//        AppState.current.clientToken = KlarnaTestsMocks.clientToken
+//        PrimerAPIConfigurationModule.apiClient = mockApiClient
+//        PrimerAPIConfigurationModule.clientToken = KlarnaTestsMocks.clientToken
+//        PrimerAPIConfigurationModule.apiConfiguration = apiConfiguration
+//        
+//        tokenizationComponent = KlarnaTokenizationComponent(paymentMethod: paymentMethod)
+//    }
+//    
+//    private func prepareConfigurations() {
+//        PrimerInternal.shared.intent = .checkout
+//        let clientSession = KlarnaTestsMocks.getClientSession()
+//        let successApiConfiguration = KlarnaTestsMocks.getMockPrimerApiConfiguration(clientSession: clientSession)
+//        successApiConfiguration.paymentMethods?[0].baseLogoImage = PrimerTheme.BaseImage(colored: UIImage(), light: nil, dark: nil)
+//        setupPrimerConfiguration(paymentMethod: Mocks.PaymentMethods.klarnaPaymentMethod, apiConfiguration: successApiConfiguration)
+//    }
+//    
+//    private func restartPrimerConfiguration() {
+//        AppState.current.clientToken = nil
+//        PrimerAPIConfigurationModule.clientToken = nil
+//        PrimerAPIConfigurationModule.apiConfiguration = nil
+//        PrimerAPIConfigurationModule.apiClient = nil
+//        tokenizationComponent = nil
+//    }
+//    
+//    private func getInvalidTokenError() -> PrimerError {
+//        let error = PrimerError.invalidClientToken(
+//            userInfo: self.getErrorUserInfo(),
+//            diagnosticsId: UUID().uuidString
+//        )
+//        ErrorHandler.handle(error: error)
+//        return error
+//    }
+//    
+//    private func getErrorUserInfo() -> [String: String] {
+//        return [
+//            "file": #file,
+//            "class": "\(Self.self)",
+//            "function": #function,
+//            "line": "\(#line)"
+//        ]
+//    }
+//}
 
 #endif
