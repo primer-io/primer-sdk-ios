3c3474f05fec5428cf8c5a9e5019bbd9fea34ae8
Boris Nikolic <boris.nikolic.dev@gmail.com>
2024-03-20 11:51:26 +0100

    Edit Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -71,7 +71,7 @@ if pr.assignees?.count == 0 {
 // Use a different path for SwiftLint
 
 let filesToLint = sdkEditedFiles.filter { $0.fileType == .swift }
-SwiftLint.lint(.files(filesToLint), inline: true)
+SwiftLint.lint(.files(filesToLint), inline: true, configFile: "Debug App/.swiftlint.yml")
 
 // MARK: Check Coverage
 
