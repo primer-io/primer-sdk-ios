8e025d1ba7d157dea464142027e3eb288c408449
Jack Newcombe <jack.newcombe@primer.io>
2024-04-15 15:04:35 +0100

    Change error user info dict extension to be explicitly on optional dict to make CI happy

--- Sources/PrimerSDK/Classes/Extensions & Utilities/Dictionary+Extensions.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/Dictionary+Extensions.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Dictionary<String, String> {
+extension Optional<Dictionary<String, String>> {
     static func errorUserInfoDictionary<T>(
         file: StaticString = #file,
         function: StaticString = #function,
