4d806bc927e51be9bdc58c1b9862b1fdfeb069c6
Jack Newcombe <jack.newcombe@primer.io>
2024-04-29 10:22:44 +0100

    remove custom string decoding

--- Sources/PrimerSDK/Classes/PCI/Services/Network/Factories/NetworkResponseFactory.swift
+++ Sources/PrimerSDK/Classes/PCI/Services/Network/Factories/NetworkResponseFactory.swift
@@ -42,26 +42,11 @@ class JSONNetworkResponseFactory: NetworkResponseFactory, LogReporter {
 
     func model<T>(for response: Data, forMetadata metadata: ResponseMetadata) throws -> T where T: Decodable {
         log(data: response, metadata: metadata)
-        return try decodeData(response, metadata: metadata)
-    }
 
-    func responseAsString(for response: Data, forMetadata metadata: ResponseMetadata) throws -> String {
-        log(data: response, metadata: metadata)
-        guard let responseString = String(data: response, encoding: .utf8) else {
-            throw InternalError.failedToDecode(
-                message: "Failed to convert data to String from URL: \(metadata.responseUrl ?? "Unknown")",
-                userInfo: .errorUserInfoDictionary(),
-                diagnosticsId: UUID().uuidString
-            )
-        }
-        return responseString
-    }
-
-    private func decodeData<T: Decodable>(_ data: Data, metadata: ResponseMetadata) throws -> T {
         switch metadata.statusCode {
         case 200:
             do {
-                return try decoder.decode(T.self, from: data)
+                return try decoder.decode(T.self, from: response)
             } catch {
                 throw InternalError.failedToDecode(
                     message: "Failed to decode response of type '\(T.self)' from URL: \(metadata.responseUrl ?? "Unknown")",
@@ -70,7 +55,7 @@ class JSONNetworkResponseFactory: NetworkResponseFactory, LogReporter {
                 )
             }
         case 400...599:
-            let serverError = try? decoder.decode(PrimerServerErrorResponse.self, from: data)
+            let serverError = try? decoder.decode(PrimerServerErrorResponse.self, from: response)
             throw InternalError.serverError(
                 status: metadata.statusCode,
                 response: serverError?.error,
