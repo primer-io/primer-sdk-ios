c64559fbb35ab533cbc9a0f2dd11be1c6cb0373a
Jack Newcombe <jack.newcombe@primer.io>
2024-05-13 21:25:34 +0100

    Add user agent tests

---
+++ Tests/Unit Tests/Data Models/UserAgentTests.swift
@@ -0,0 +1,19 @@
+//
+//  UserAgentTests.swift
+//  
+//
+//  Created by Jack Newcombe on 13/05/2024.
+//
+
+import XCTest
+@testable import PrimerSDK
+
+final class UserAgentTests: XCTestCase {
+
+    func testUserAgent() throws {
+        let regex = try NSRegularExpression(pattern: "^xctest/\\d+\\.\\d+ arm64 iOS/\\d+\\.\\d+ CFNetwork/1\\.0 Darwin/\\d+\\.\\d+\\.0$")
+        let range = NSRange(location: 0, length: UserAgent.userAgentAsString.count)
+        XCTAssertEqual(regex.numberOfMatches(in: UserAgent.userAgentAsString, range: range), 1)
+    }
+
+}
