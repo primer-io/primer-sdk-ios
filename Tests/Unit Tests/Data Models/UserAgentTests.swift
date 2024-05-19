8a86d367bf8f9ae5de4a3753f2c76f927af6868a
Jack Newcombe <jack.newcombe@primer.io>
2024-05-19 19:25:09 +0100

    Attemp another user agent fix

--- Tests/Unit Tests/Data Models/UserAgentTests.swift
+++ Tests/Unit Tests/Data Models/UserAgentTests.swift
@@ -11,9 +11,8 @@ import XCTest
 final class UserAgentTests: XCTestCase {
 
     func testUserAgent() throws {
-        let regex = try NSRegularExpression(pattern: "^xctest/\\d+\\.\\d+ arm64 iOS/\\d+\\.\\d+ CFNetwork/1\\.0 Darwin/\\d+\\.\\d+\\.\\d+$")
+        let regex = try NSRegularExpression(pattern: "^xctest/\\d+\\.\\d+ arm64 iOS/\\d+\\.\\d+ CFNetwork/\\d+\\.\\d+ Darwin/\\d+\\.\\d+\\.\\d+$")
         let range = NSRange(location: 0, length: UserAgent.userAgentAsString.count)
-        print("USER AGENT: \(UserAgent.userAgentAsString)")
         XCTAssertEqual(regex.numberOfMatches(in: UserAgent.userAgentAsString, range: range), 1)
     }
 
