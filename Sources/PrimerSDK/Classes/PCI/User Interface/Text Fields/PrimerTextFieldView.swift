e1d83fb5e53ab67a3a0db86442b720006112e230
Jack Newcombe <jack.newcombe@primer.io>
2024-05-21 12:19:15 +0100

    Move text field delegate to separate file

--- Sources/PrimerSDK/Classes/PCI/User Interface/Text Fields/PrimerTextFieldView.swift
+++ Sources/PrimerSDK/Classes/PCI/User Interface/Text Fields/PrimerTextFieldView.swift
@@ -8,40 +8,6 @@
 // swiftlint:disable function_body_length
 import UIKit
 
-/// The PrimerTextFieldViewDelegate protocol can be used to retrieve information about the text input.
-/// PrimerCardNumberFieldView, PrimerExpiryDateFieldView, PrimerCVVFieldView & PrimerCardholderNameFieldView
-/// all have a delegate of PrimerTextFieldViewDelegate type.
-public protocol PrimerTextFieldViewDelegate: AnyObject {
-    /// Will return true if valid, false if invalid, nil if it cannot be detected yet.
-    /// It is applied on all PrimerTextFieldViews.
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, isValid: Bool?)
-    /// Will return the card network (e.g. Visa) detected, unknown if the network cannot be detected.
-    /// Only applies on PrimerCardNumberFieldView
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, didDetectCardNetwork cardNetwork: CardNetwork?)
-    /// Will return a the validation error on the text input.
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, validationDidFailWithError error: Error)
-
-    func primerTextFieldViewDidBeginEditing(_ primerTextFieldView: PrimerTextFieldView)
-
-    func primerTextFieldViewShouldBeginEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool
-
-    func primerTextFieldViewShouldEndEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool
-
-    func primerTextFieldViewDidEndEditing(_ primerTextFieldView: PrimerTextFieldView)
-}
-
-public extension PrimerTextFieldViewDelegate {
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, isValid: Bool?) {}
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView,
-                             didDetectCardNetwork cardNetwork: CardNetwork?) {}
-    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView,
-                             validationDidFailWithError error: Error) {}
-    func primerTextFieldViewDidBeginEditing(_ primerTextFieldView: PrimerTextFieldView) {}
-    func primerTextFieldViewShouldBeginEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool { return true }
-    func primerTextFieldViewShouldEndEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool { return true }
-    func primerTextFieldViewDidEndEditing(_ primerTextFieldView: PrimerTextFieldView) {}
-}
-
 public class PrimerTextFieldView: PrimerNibView, UITextFieldDelegate {
 
     @IBOutlet internal weak var textField: PrimerTextField!
---
+++ Sources/PrimerSDK/Classes/PCI/User Interface/Text Fields/PrimerTextFieldViewDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  PrimerTextFieldViewDelegate.swift
+//  PrimerSDK
+//
+//  Created by Jack Newcombe on 21/05/2024.
+//
+
+import Foundation
+
+
+/// The PrimerTextFieldViewDelegate protocol can be used to retrieve information about the text input.
+/// PrimerCardNumberFieldView, PrimerExpiryDateFieldView, PrimerCVVFieldView & PrimerCardholderNameFieldView
+/// all have a delegate of PrimerTextFieldViewDelegate type.
+public protocol PrimerTextFieldViewDelegate: AnyObject {
+    /// Will return true if valid, false if invalid, nil if it cannot be detected yet.
+    /// It is applied on all PrimerTextFieldViews.
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, isValid: Bool?)
+    /// Will return the card network (e.g. Visa) detected, unknown if the network cannot be detected.
+    /// Only applies on PrimerCardNumberFieldView
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, didDetectCardNetwork cardNetwork: CardNetwork?)
+    /// Will return a the validation error on the text input.
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, validationDidFailWithError error: Error)
+
+    func primerTextFieldViewDidBeginEditing(_ primerTextFieldView: PrimerTextFieldView)
+
+    func primerTextFieldViewShouldBeginEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool
+
+    func primerTextFieldViewShouldEndEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool
+
+    func primerTextFieldViewDidEndEditing(_ primerTextFieldView: PrimerTextFieldView)
+}
+
+public extension PrimerTextFieldViewDelegate {
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView, isValid: Bool?) {}
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView,
+                             didDetectCardNetwork cardNetwork: CardNetwork?) {}
+    func primerTextFieldView(_ primerTextFieldView: PrimerTextFieldView,
+                             validationDidFailWithError error: Error) {}
+    func primerTextFieldViewDidBeginEditing(_ primerTextFieldView: PrimerTextFieldView) {}
+    func primerTextFieldViewShouldBeginEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool { return true }
+    func primerTextFieldViewShouldEndEditing(_ primerTextFieldView: PrimerTextFieldView) -> Bool { return true }
+    func primerTextFieldViewDidEndEditing(_ primerTextFieldView: PrimerTextFieldView) {}
+}
