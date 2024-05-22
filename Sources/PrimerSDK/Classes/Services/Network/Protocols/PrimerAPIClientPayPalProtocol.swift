835001892285d7d2d27f86c9b5b4711ee1c7f8c3
Jack Newcombe <jack.newcombe@primer.io>
2024-05-22 15:44:00 +0100

    whtspc!

--- Sources/PrimerSDK/Classes/Services/Network/Protocols/PrimerAPIClientPayPalProtocol.swift
+++ Sources/PrimerSDK/Classes/Services/Network/Protocols/PrimerAPIClientPayPalProtocol.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol PrimerAPIClientPayPalProtocol {
-    
+
     func createPayPalOrderSession(
         clientToken: DecodedJWTToken,
         payPalCreateOrderRequest: Request.Body.PayPal.CreateOrder,
