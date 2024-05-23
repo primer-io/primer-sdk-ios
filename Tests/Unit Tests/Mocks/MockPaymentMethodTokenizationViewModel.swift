b15c511b3559e7efc07db971b73adba90b880dfb
Jack Newcombe <jack.newcombe@primer.io>
2024-05-23 13:12:32 +0100

    correct in tests

--- Tests/Unit Tests/Mocks/MockPaymentMethodTokenizationViewModel.swift
+++ Tests/Unit Tests/Mocks/MockPaymentMethodTokenizationViewModel.swift
@@ -16,7 +16,7 @@ class MockPaymentMethodTokenizationViewModel: NSObject, PaymentMethodTokenizatio
     var config: PrimerPaymentMethod!
     var uiModule: UserInterfaceModule!
     var position: Int = 0
-    var checkouEventsNotifierModule: CheckoutEventsNotifierModule
+    var checkoutEventsNotifierModule: CheckoutEventsNotifierModule
     var didStartPayment: (() -> Void)?
     var didFinishPayment: ((Error?) -> Void)?
     var willPresentPaymentMethodUI: (() -> Void)?
@@ -35,7 +35,7 @@ class MockPaymentMethodTokenizationViewModel: NSObject, PaymentMethodTokenizatio
 
     required init(config: PrimerPaymentMethod) {
         self.config = config
-        self.checkouEventsNotifierModule = CheckoutEventsNotifierModule()
+        self.checkoutEventsNotifierModule = CheckoutEventsNotifierModule()
     }
 
     convenience init(
@@ -140,14 +140,14 @@ class MockPaymentMethodTokenizationViewModel: NSObject, PaymentMethodTokenizatio
             PrimerDelegateProxy.primerHeadlessUniversalCheckoutDidStartTokenization(for: self.config.type)
 
             firstly {
-                self.checkouEventsNotifierModule.fireDidStartTokenizationEvent()
+                self.checkoutEventsNotifierModule.fireDidStartTokenizationEvent()
             }
             .then { () -> Promise<PrimerPaymentMethodTokenData> in
                 return self.tokenize()
             }
             .then { paymentMethodTokenData -> Promise<Void> in
                 self.paymentMethodTokenData = paymentMethodTokenData
-                return self.checkouEventsNotifierModule.fireDidFinishTokenizationEvent()
+                return self.checkoutEventsNotifierModule.fireDidFinishTokenizationEvent()
             }
             .done {
                 seal.fulfill()
