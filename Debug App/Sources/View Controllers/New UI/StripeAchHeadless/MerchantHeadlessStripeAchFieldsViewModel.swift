1e3212fbda697601df14ce41288544005436c523
StefanV-PRIMERIO <stefan.vrancianu@primer.io>
2024-05-23 23:32:04 +0300

    chore: cleanup merchant vm

--- Debug App/Sources/View Controllers/New UI/StripeAchHeadless/MerchantHeadlessStripeAchFieldsViewModel.swift
+++ Debug App/Sources/View Controllers/New UI/StripeAchHeadless/MerchantHeadlessStripeAchFieldsViewModel.swift
@@ -25,21 +25,3 @@ class StripeAchFieldsViewModel: ObservableObject {
         isFirstNameValid && isLastNameValid && isEmailAddressValid
     }
 }
-
-//let publishableKey = "pk_test_51MoI84BvedTI81V7r8h9B1XSl7lqU5rEdZHx8N6qoNeXDyyhUq8ziBgWXyMlO3bPADYRdAiYFQJg51fWN784pIBH00Y5YTas27"
-//
-//func getParams() -> Params {
-//    return Params(publishableKey: publishableKey,
-//                  clientSecret: "pi_3P86AgBvedTI81V719xwKkhz_secret_izeuhjFqW6ibJkjvPVBcvsGNL",
-//                  returnUrl: "primer://merchant",
-//                  fullName: fullName,
-//                  emailAddress: emailAddress)
-//}
-
-struct Params {
-    var publishableKey: String
-    var clientSecret: String
-    var returnUrl: String
-    var fullName: String
-    var emailAddress: String
-}
