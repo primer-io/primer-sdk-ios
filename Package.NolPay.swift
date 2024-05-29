d61f2d8f86a8eae42ea532c9b391f9e800e3ecf7
Jack Newcombe <jack.newcombe@primer.io>
2024-05-29 19:49:49 +0100

    Setup nol pay package.swift

--- Package.NolPay.swift
+++ Package.NolPay.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "PrimerSDK",
-            targets: ["PrimerSDK"]
+            targets: ["PrimerSDK", "PrimerNolPaySDK"]
         )
     ],
     dependencies: [
@@ -20,9 +20,6 @@ let package = Package(
     targets: [
         .target(
             name: "PrimerSDK",
-            dependencies: [
-                .product(name: "PrimerNolPaySDK", package: "primer-nol-pay-sdk-ios")
-            ],
             path: "Sources/PrimerSDK",
             resources: [
                 .process("Resources"),
