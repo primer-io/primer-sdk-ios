59073a2b536622ba4c2de0a64c703d5ece9fffcc
Jack Newcombe <jack.newcombe@primer.io>
2024-05-31 13:14:01 +0100

    Add 3DS package swift + tests

---
+++ Package.3DS.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "PrimerSDK",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("13.1")
+    ],
+    products: [
+        .library(
+            name: "PrimerSDK",
+            targets: ["PrimerSDK"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.3.1")
+    ],
+    targets: [
+        .target(
+            name: "PrimerSDK",
+            dependencies: [
+                .product(name: "Primer3DS", package: "primer-sdk-3ds-ios")
+            ],
+            path: "Sources/PrimerSDK",
+            resources: [
+                .process("Resources"),
+                .copy("Classes/Third Party/PromiseKit/LICENSE")
+            ]
+        ),
+        .testTarget(
+            name: "Tests",
+            dependencies: [
+                .product(name: "Primer3DS", package: "primer-sdk-3ds-ios"),
+                .byName(name: "PrimerSDK")
+            ],
+            path: "Tests/",
+            sources: [
+                "3DS/",
+                "Utilities/"
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
