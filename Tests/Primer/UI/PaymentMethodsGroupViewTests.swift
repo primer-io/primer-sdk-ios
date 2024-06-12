481a75c4a1f1dccd4f6ec486149036cff3123f3f
Jack Newcombe <jack@newcombe.io>
2024-06-12 16:44:53 +0100

    test: view tests (#898)
    * Add header footer view test
    
    * Add test for primer form view
    
    * Remove unused view
    
    * Add test for payment methods group view
    
    * lint

---
+++ Tests/Primer/UI/PaymentMethodsGroupViewTests.swift
@@ -0,0 +1,143 @@
+//
+//  PaymentMethodsGroupViewTests.swift
+//  
+//
+//  Created by Jack Newcombe on 11/06/2024.
+//
+
+import XCTest
+@testable import PrimerSDK
+
+final class PaymentMethodsGroupViewTests: XCTestCase {
+
+    var sut: PaymentMethodsGroupView!
+
+    func testViewInit() throws {
+        let viewModel = MockTokenizationViewModel()
+        sut = PaymentMethodsGroupView(title: "title", paymentMethodTokenizationViewModels: [viewModel])
+
+        let stackView = sut.subviews.first as? UIStackView
+        XCTAssertNotNil(stackView)
+
+        let paymentMethodButton = stackView?.arrangedSubviews.last as? PrimerButton
+        XCTAssertNotNil(paymentMethodButton)
+        XCTAssertEqual(paymentMethodButton?.title(for: .normal), "Pay with card")
+
+        let expectStart = self.expectation(description: "Tokenization is started")
+        viewModel.onStart = {
+            expectStart.fulfill()
+        }
+
+        paymentMethodButton?.simulateEvent(.touchUpInside)
+
+        waitForExpectations(timeout: 2.0)
+    }
+}
+
+private class MockTokenizationViewModel: NSObject, PaymentMethodTokenizationViewModelProtocol {
+
+    func submitButtonTapped() {
+    }
+
+    static var apiClient: (any PrimerAPIClientProtocol)?
+
+    var checkoutEventsNotifierModule: CheckoutEventsNotifierModule = .init()
+
+    var didStartPayment: (() -> Void)?
+
+    var didFinishPayment: (((any Error)?) -> Void)?
+
+    var paymentMethodTokenData: PrimerPaymentMethodTokenData?
+
+    var paymentCheckoutData: PrimerCheckoutData?
+
+    var successMessage: String?
+
+    func validate() throws {
+    }
+
+    var onStart: (() -> Void)?
+
+    func start() {
+        onStart?()
+    }
+
+    func performPreTokenizationSteps() -> Promise<Void> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func performTokenizationStep() -> Promise<Void> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func performPostTokenizationSteps() -> Promise<Void> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func tokenize() -> PrimerSDK.Promise<PrimerPaymentMethodTokenData> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func startTokenizationFlow() -> Promise<PrimerPaymentMethodTokenData> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func startPaymentFlow(withPaymentMethodTokenData paymentMethodTokenData: PrimerPaymentMethodTokenData) -> Promise<PrimerCheckoutData?> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func presentPaymentMethodUserInterface() -> Promise<Void> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func awaitUserInput() -> Promise<Void> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func handleDecodedClientTokenIfNeeded(_ decodedJWTToken: DecodedJWTToken, 
+                                          paymentMethodTokenData: PrimerPaymentMethodTokenData) -> Promise<String?> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func handleResumeStepsBasedOnSDKSettings(resumeToken: String) -> PrimerSDK.Promise<PrimerSDK.PrimerCheckoutData?> {
+        Promise.rejected(PrimerError.unknown(userInfo: nil, diagnosticsId: ""))
+    }
+
+    func handleSuccessfulFlow() {
+    }
+
+    func handleFailureFlow(errorMessage: String?) {
+    }
+
+    func cancel() {
+    }
+
+    var willPresentPaymentMethodUI: (() -> Void)?
+
+    var didPresentPaymentMethodUI: (() -> Void)?
+    
+    var willDismissPaymentMethodUI: (() -> Void)?
+    
+    var didDismissPaymentMethodUI: (() -> Void)?
+    
+    var config: PrimerSDK.PrimerPaymentMethod = Mocks.PaymentMethods.paymentCardPaymentMethod
+
+    lazy var uiModule: PrimerSDK.UserInterfaceModule! = {
+        UserInterfaceModule(paymentMethodTokenizationViewModel: self)
+    }()
+    
+    var position: Int = 0
+}
+
+extension UIControl {
+
+    func simulateEvent(_ event: UIControl.Event) {
+        for target in allTargets {
+            let target = target as NSObjectProtocol
+            for actionName in actions(forTarget: target, forControlEvent: event) ?? [] {
+                let selector = Selector(actionName)
+                target.perform(selector, with: self)
+            }
+        }
+    }
+}
