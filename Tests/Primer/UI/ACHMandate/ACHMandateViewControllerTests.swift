4ceb45511aa8aa652fa5b7fb3d125c137f3925a5
StefanV-PRIMERIO <stefan.vrancianu@primer.io>
2024-08-07 00:27:02 +0300

    chore: fix unit test

--- Tests/Primer/UI/ACHMandate/ACHMandateViewControllerTests.swift
+++ Tests/Primer/UI/ACHMandate/ACHMandateViewControllerTests.swift
@@ -46,11 +46,7 @@ final class ACHMandateViewControllerTests: XCTestCase {
         tearDown()
         
         let merchantName = "Primer Inc"
-        let templateMandateText = """
-                    By clicking [accept], you authorise [\(merchantName)] to debit the bank account specified above for any amount owed for charges arising from your use of [\(merchantName)]'s services and/or purchase of products from [\(merchantName)], pursuant to [\(merchantName)]'s website and terms, until this authorisation is revoked. You may amend or cancel this authorisation at any time by providing notice to [\(merchantName)] with 30 (thirty) days notice.
-
-                    If you use [\(merchantName)]'s services or purchase additional products periodically pursuant to [\(merchantName)]'s terms, you authorise [\(merchantName)] to debit your bank account periodically. Payments that fall outside the regular debits authorised above will only be debited after your authorisation is obtained.
-                    """
+        let templateMandateText = "By clicking Accept, you authorize Primer Inc to debit the bank account specified above for any amount owed for charges arising from your use of Primer Inc's services and/or purchase of products from Primer Inc, pursuant to Primer Inc's website and terms, until this authorization is revoked. You may amend or cancel this authorization at any time by providing notice to Primer Inc with 30 (thirty) days notice.\n\nIf you use Primer Inc's services or purchase additional products periodically pursuant to Primer Inc's terms, you authorize Primer Inc to debit your bank account periodically. Payments that fall outside the regular debits authorized above will only be debited after your authorization is obtained."
         
         mockDelegate = MockACHMandateViewController()
         mandateData = PrimerStripeOptions.MandateData.templateMandate(merchantName: merchantName)
@@ -82,14 +78,14 @@ final class ACHMandateViewControllerTests: XCTestCase {
 }
 
 class MockACHMandateViewController: ACHMandateDelegate {
-    var didAcceptMandate: (() -> Void)?
-    var didDeclineMandate: (() -> Void)?
-    
-    func mandateAccepted() {
+    func acceptMandate() {
         didAcceptMandate?()
     }
     
-    func mandateDeclined() {
+    func declineMandate() {
         didDeclineMandate?()
     }
+    
+    var didAcceptMandate: (() -> Void)?
+    var didDeclineMandate: (() -> Void)?
 }
