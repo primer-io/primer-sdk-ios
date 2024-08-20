076b2b2039eb057ea929af095460722e7dfdd2a9
Niall Quinn <niall@primer.io>
2024-08-20 09:44:22 +0100

    fix: apply uiDelegate when set in PrimerHeadlessUniversalCheckout.current.start (#975)
    * Set uiDelegate in headless checkout start
    
    * Add test for delegates

--- Tests/Primer/Utils/CheckoutSessionTests.swift
+++ Tests/Primer/Utils/CheckoutSessionTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import PrimerSDK
 
 final class CheckoutSessionTests: XCTestCase {
-    func test_headless_cleanup() {
+    func test_headless_cleanup() throws {
         XCTAssertFalse(Primer.shared.checkoutSessionIsActive())
 
         let expectation = self.expectation(description: "Wait for headless load")
@@ -23,4 +23,26 @@ final class CheckoutSessionTests: XCTestCase {
         PrimerHeadlessUniversalCheckout.current.cleanUp()
         XCTAssertFalse(Primer.shared.checkoutSessionIsActive())
     }
+
+    func test_headless_delegates() throws {
+        let mockDelegate = MockDelegate()
+        let expectation = self.expectation(description: "Wait for headless load")
+        PrimerHeadlessUniversalCheckout.current.start(withClientToken: "", delegate: mockDelegate, uiDelegate: mockDelegate) { paymentMethods, err in
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 30)
+        guard let mockDelegate = PrimerHeadlessUniversalCheckout.current.delegate as? MockDelegate, let mockUIDelegate = PrimerHeadlessUniversalCheckout.current.uiDelegate as? MockDelegate else {
+            XCTFail("Delegates not set")
+            return
+        }
+        
+        XCTAssertEqual(mockDelegate.id, "mock-id")
+        XCTAssertEqual(mockUIDelegate.id, "mock-id")
+    }
+}
+
+private class MockDelegate: PrimerHeadlessUniversalCheckoutDelegate, PrimerHeadlessUniversalCheckoutUIDelegate {
+    let id = "mock-id"
+    func primerHeadlessUniversalCheckoutDidCompleteCheckoutWithData(_ data: PrimerSDK.PrimerCheckoutData) {}
 }
