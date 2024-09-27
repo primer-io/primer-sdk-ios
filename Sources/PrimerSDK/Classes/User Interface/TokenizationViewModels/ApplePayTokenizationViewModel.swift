ab27c5d2c7876d7b252729698f22110a12648a13
Niall Quinn <niall@primer.io>
2024-09-27 12:09:40 +0200

    Remove name from shipping lineItem

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/ApplePayTokenizationViewModel.swift
@@ -379,7 +379,7 @@ class ApplePayTokenizationViewModel: PaymentMethodTokenizationViewModel {
             $0.id == options.selectedShippingMethod
         }) {
             shippingItem = try? ApplePayOrderItem(
-                name: "Shipping: \(selectedShippingMethod.name)",
+                name: "Shipping:",
                 unitAmount: selectedShippingMethod.amount,
                 quantity: 1,
                 discountAmount: nil,
