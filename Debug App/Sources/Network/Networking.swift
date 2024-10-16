7ef14bfbf8bfb80465ad64c33f18a25f6b325c23
Jack Newcombe <jack@newcombe.io>
2024-10-16 12:29:31 +0100

    Clean up client session config UI by removing controls for unused and deprecated fields

--- Debug App/Sources/Network/Networking.swift
+++ Debug App/Sources/Network/Networking.swift
@@ -53,7 +53,7 @@ class Networking {
         apiVersion: APIVersion?,
         url: URL,
         method: HTTPMethod,
-        headers: [String: String]?,
+        headers: [String: String]? = nil,
         queryParameters: [String: String]?,
         body: Data?,
         completion: @escaping (_ result: Result<Data, Error>) -> Void) {
@@ -276,7 +276,6 @@ class Networking {
             apiVersion: .v2_2,
             url: url,
             method: .post,
-            headers: URL.requestSessionHTTPHeaders(useNewWorkflows: useNewWorkflows),
             queryParameters: nil,
             body: bodyData
         ) { result in
