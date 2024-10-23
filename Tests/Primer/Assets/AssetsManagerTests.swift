b081bd33af4d212cb5be241e64223661d0170417
Niall Quinn <niall@primer.io>
2024-10-23 17:05:33 +0200

    Add convenience method for getting asset based on token

--- Tests/Primer/Assets/AssetsManagerTests.swift
+++ Tests/Primer/Assets/AssetsManagerTests.swift
@@ -22,6 +22,7 @@ final class AssetsManagerTests: XCTestCase {
     }
 
     func testCardAssets() throws {
+
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .cartesBancaires)?.cardImage)
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .discover)?.cardImage)
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .masterCard)?.cardImage)
@@ -33,6 +34,140 @@ final class AssetsManagerTests: XCTestCase {
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .maestro)?.cardImage)
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .mir)?.cardImage)
         XCTAssertNotNil(AssetsManager.getCardNetworkAsset(for: .unknown)?.cardImage)
+
     }
 
+    func testGetNetworkAssetForTokenData() throws {
+        XCTAssertEqual(AssetsManager.getCardNetworkAsset(tokenData: .visaNetworkToken)?.cardNetwork, .visa)
+        XCTAssertNotNil(AssetsManager.getCardNetworkAsset(tokenData: .visaNetworkToken)?.cardImage)
+
+        XCTAssertEqual(AssetsManager.getCardNetworkAsset(tokenData: .mcNetworkToken)?.cardNetwork, .masterCard)
+        XCTAssertNotNil(AssetsManager.getCardNetworkAsset(tokenData: .mcNetworkToken)?.cardImage)
+
+        XCTAssertEqual(AssetsManager.getCardNetworkAsset(tokenData: .cbNetworkToken)?.cardNetwork, .cartesBancaires)
+        XCTAssertNotNil(AssetsManager.getCardNetworkAsset(tokenData: .cbNetworkToken)?.cardImage)
+
+        XCTAssertEqual(AssetsManager.getCardNetworkAsset(tokenData: .visaBinNetworkToken)?.cardNetwork, .visa)
+        XCTAssertNotNil(AssetsManager.getCardNetworkAsset(tokenData: .visaBinNetworkToken)?.cardImage)
+
+        XCTAssertEqual(AssetsManager.getCardNetworkAsset(tokenData: .visafirstSixToken)?.cardNetwork, .visa)
+        XCTAssertNotNil(AssetsManager.getCardNetworkAsset(tokenData: .visafirstSixToken)?.cardImage)
+    }
+}
+
+private extension Response.Body.Tokenization.PaymentInstrumentData {
+    static let visaNetworkToken = Self.init(paypalBillingAgreementId: nil,
+                                            first6Digits: nil,
+                                            last4Digits: nil,
+                                            expirationMonth: nil,
+                                            expirationYear: nil,
+                                            cardholderName: nil,
+                                            network: "VISA",
+                                            isNetworkTokenized: nil,
+                                            klarnaCustomerToken: nil,
+                                            sessionData: nil,
+                                            externalPayerInfo: nil,
+                                            shippingAddress: nil,
+                                            binData: nil,
+                                            threeDSecureAuthentication: nil,
+                                            gocardlessMandateId: nil,
+                                            authorizationToken: nil,
+                                            mx: nil,
+                                            currencyCode: nil,
+                                            productId: nil,
+                                            paymentMethodConfigId: nil,
+                                            paymentMethodType: "PAYMENT_CARD",
+                                            sessionInfo: nil)
+
+    static let mcNetworkToken = Self.init(paypalBillingAgreementId: nil,
+                                          first6Digits: nil,
+                                          last4Digits: nil,
+                                          expirationMonth: nil,
+                                          expirationYear: nil,
+                                          cardholderName: nil,
+                                          network: "MASTERCARD",
+                                          isNetworkTokenized: nil,
+                                          klarnaCustomerToken: nil,
+                                          sessionData: nil,
+                                          externalPayerInfo: nil,
+                                          shippingAddress: nil,
+                                          binData: nil,
+                                          threeDSecureAuthentication: nil,
+                                          gocardlessMandateId: nil,
+                                          authorizationToken: nil,
+                                          mx: nil,
+                                          currencyCode: nil,
+                                          productId: nil,
+                                          paymentMethodConfigId: nil,
+                                          paymentMethodType: "PAYMENT_CARD",
+                                          sessionInfo: nil)
+
+    static let cbNetworkToken = Self.init(paypalBillingAgreementId: nil,
+                                          first6Digits: nil,
+                                          last4Digits: nil,
+                                          expirationMonth: nil,
+                                          expirationYear: nil,
+                                          cardholderName: nil,
+                                          network: "CARTES_BANCAIRES",
+                                          isNetworkTokenized: nil,
+                                          klarnaCustomerToken: nil,
+                                          sessionData: nil,
+                                          externalPayerInfo: nil,
+                                          shippingAddress: nil,
+                                          binData: nil,
+                                          threeDSecureAuthentication: nil,
+                                          gocardlessMandateId: nil,
+                                          authorizationToken: nil,
+                                          mx: nil,
+                                          currencyCode: nil,
+                                          productId: nil,
+                                          paymentMethodConfigId: nil,
+                                          paymentMethodType: "PAYMENT_CARD",
+                                          sessionInfo: nil)
+
+    static let visaBinNetworkToken = Self.init(paypalBillingAgreementId: nil,
+                                               first6Digits: nil,
+                                               last4Digits: nil,
+                                               expirationMonth: nil,
+                                               expirationYear: nil,
+                                               cardholderName: nil,
+                                               network: nil,
+                                               isNetworkTokenized: nil,
+                                               klarnaCustomerToken: nil,
+                                               sessionData: nil,
+                                               externalPayerInfo: nil,
+                                               shippingAddress: nil,
+                                               binData: BinData(network: "VISA"),
+                                               threeDSecureAuthentication: nil,
+                                               gocardlessMandateId: nil,
+                                               authorizationToken: nil,
+                                               mx: nil,
+                                               currencyCode: nil,
+                                               productId: nil,
+                                               paymentMethodConfigId: nil,
+                                               paymentMethodType: "PAYMENT_CARD",
+                                               sessionInfo: nil)
+
+    static let visafirstSixToken = Self.init(paypalBillingAgreementId: nil,
+                                               first6Digits: "401288",
+                                               last4Digits: nil,
+                                               expirationMonth: nil,
+                                               expirationYear: nil,
+                                               cardholderName: nil,
+                                               network: nil,
+                                               isNetworkTokenized: nil,
+                                               klarnaCustomerToken: nil,
+                                               sessionData: nil,
+                                               externalPayerInfo: nil,
+                                               shippingAddress: nil,
+                                               binData: nil,
+                                               threeDSecureAuthentication: nil,
+                                               gocardlessMandateId: nil,
+                                               authorizationToken: nil,
+                                               mx: nil,
+                                               currencyCode: nil,
+                                               productId: nil,
+                                               paymentMethodConfigId: nil,
+                                               paymentMethodType: "PAYMENT_CARD",
+                                               sessionInfo: nil)
 }
