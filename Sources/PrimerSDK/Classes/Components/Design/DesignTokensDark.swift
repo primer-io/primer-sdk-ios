6b2a93e727b7aba3b1c775226e3ddd4c6b9e55a2
Boris Nikolic <boris.nikolic.dev@gmail.com>
2025-02-25 18:32:25 +0100

    Update token gen for repo migration

---
+++ Sources/PrimerSDK/Classes/Components/Design/DesignTokensDark.swift
@@ -0,0 +1,27 @@
+
+//
+// DesignTokensDark.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+internal class DesignTokensDark {
+    internal static let primerColorGray100 = UIColor(red: 0.161, green: 0.161, blue: 0.161, alpha: 1)
+    internal static let primerColorGray200 = UIColor(red: 0.259, green: 0.259, blue: 0.259, alpha: 1)
+    internal static let primerColorGray300 = UIColor(red: 0.341, green: 0.341, blue: 0.341, alpha: 1)
+    internal static let primerColorGray400 = UIColor(red: 0.522, green: 0.522, blue: 0.522, alpha: 1)
+    internal static let primerColorGray500 = UIColor(red: 0.463, green: 0.459, blue: 0.467, alpha: 1)
+    internal static let primerColorGray600 = UIColor(red: 0.780, green: 0.780, blue: 0.780, alpha: 1)
+    internal static let primerColorGray900 = UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
+    internal static let primerColorGray000 = UIColor(red: 0.090, green: 0.086, blue: 0.098, alpha: 1)
+    internal static let primerColorGreen500 = UIColor(red: 0.153, green: 0.694, blue: 0.490, alpha: 1)
+    internal static let primerColorBrand = UIColor(red: 0.184, green: 0.596, blue: 1.000, alpha: 1)
+    internal static let primerColorRed100 = UIColor(red: 0.196, green: 0.110, blue: 0.125, alpha: 1)
+    internal static let primerColorRed500 = UIColor(red: 0.894, green: 0.427, blue: 0.439, alpha: 1)
+    internal static let primerColorRed900 = UIColor(red: 0.965, green: 0.749, blue: 0.749, alpha: 1)
+    internal static let primerColorBlue500 = UIColor(red: 0.247, green: 0.576, blue: 0.894, alpha: 1)
+    internal static let primerColorBlue900 = UIColor(red: 0.290, green: 0.682, blue: 1.000, alpha: 1)
+}
---
+++ Sources/PrimerSDK/Classes/Components/Design/DesignTokensLight.swift
@@ -0,0 +1,99 @@
+
+//
+// DesignTokensLight.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+internal class DesignTokensLight {
+    internal static let primerColorBorderTransparentDefault = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0)
+    internal static let primerColorGray100 = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1)
+    internal static let primerColorGray200 = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1)
+    internal static let primerColorGray300 = UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1)
+    internal static let primerColorGray400 = UIColor(red: 0.741, green: 0.741, blue: 0.741, alpha: 1)
+    internal static let primerColorGray500 = UIColor(red: 0.620, green: 0.620, blue: 0.620, alpha: 1)
+    internal static let primerColorGray600 = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+    internal static let primerColorGray900 = UIColor(red: 0.129, green: 0.129, blue: 0.129, alpha: 1)
+    internal static let primerColorGray000 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    internal static let primerColorGreen500 = UIColor(red: 0.243, green: 0.714, blue: 0.561, alpha: 1)
+    internal static let primerColorBrand = UIColor(red: 0.184, green: 0.596, blue: 1.000, alpha: 1)
+    internal static let primerColorRed100 = UIColor(red: 1.000, green: 0.925, blue: 0.925, alpha: 1)
+    internal static let primerColorRed500 = UIColor(red: 1.000, green: 0.447, blue: 0.475, alpha: 1)
+    internal static let primerColorRed900 = UIColor(red: 0.706, green: 0.196, blue: 0.294, alpha: 1)
+    internal static let primerColorBlue500 = UIColor(red: 0.224, green: 0.616, blue: 1.000, alpha: 1)
+    internal static let primerColorBlue900 = UIColor(red: 0.133, green: 0.439, blue: 0.957, alpha: 1)
+    internal static let primerRadiusBase = CGFloat(64.00)
+    internal static let primerTypographyBrand = Inter
+    internal static let primerTypographyTitleXlargeLetterSpacing = CGFloat(-9.60)
+    internal static let primerTypographyTitleXlargeWeight = CGFloat(8800.00)
+    internal static let primerTypographyTitleXlargeSize = CGFloat(384.00)
+    internal static let primerTypographyTitleXlargeLineHeight = CGFloat(512.00)
+    internal static let primerTypographyTitleLargeLetterSpacing = CGFloat(-3.20)
+    internal static let primerTypographyTitleLargeWeight = CGFloat(8800.00)
+    internal static let primerTypographyTitleLargeSize = CGFloat(256.00)
+    internal static let primerTypographyTitleLargeLineHeight = CGFloat(320.00)
+    internal static let primerTypographyBodyLargeLetterSpacing = CGFloat(-3.20)
+    internal static let primerTypographyBodyLargeWeight = CGFloat(6400.00)
+    internal static let primerTypographyBodyLargeSize = CGFloat(256.00)
+    internal static let primerTypographyBodyLargeLineHeight = CGFloat(320.00)
+    internal static let primerTypographyBodyMediumLetterSpacing = CGFloat(0.00)
+    internal static let primerTypographyBodyMediumWeight = CGFloat(6400.00)
+    internal static let primerTypographyBodyMediumSize = CGFloat(224.00)
+    internal static let primerTypographyBodyMediumLineHeight = CGFloat(320.00)
+    internal static let primerTypographyBodySmallLetterSpacing = CGFloat(0.00)
+    internal static let primerTypographyBodySmallWeight = CGFloat(6400.00)
+    internal static let primerTypographyBodySmallSize = CGFloat(192.00)
+    internal static let primerTypographyBodySmallLineHeight = CGFloat(256.00)
+    internal static let primerSpaceBase = CGFloat(64.00)
+    internal static let primerSizeBase = CGFloat(64.00)
+    internal static let primerColorBackground = primerColorGray000
+    internal static let primerColorTextPrimary = primerColorGray900
+    internal static let primerColorTextPlaceholder = primerColorGray500
+    internal static let primerColorTextDisabled = primerColorGray400
+    internal static let primerColorTextNegative = primerColorRed900
+    internal static let primerColorTextLink = primerColorBlue900
+    internal static let primerColorTextSecondary = primerColorGray600
+    internal static let primerColorBorderOutlinedDefault = primerColorGray300
+    internal static let primerColorBorderOutlinedHover = primerColorGray400
+    internal static let primerColorBorderOutlinedActive = primerColorGray500
+    internal static let primerColorBorderOutlinedDisabled = primerColorGray200
+    internal static let primerColorBorderOutlinedLoading = primerColorGray200
+    internal static let primerColorBorderOutlinedSelected = primerColorBrand
+    internal static let primerColorBorderOutlinedError = primerColorRed500
+    internal static let primerColorBorderTransparentHover = primerColorBorderTransparentDefault
+    internal static let primerColorBorderTransparentActive = primerColorBorderTransparentDefault
+    internal static let primerColorBorderTransparentDisabled = primerColorBorderTransparentDefault
+    internal static let primerColorBorderTransparentSelected = primerColorBorderTransparentDefault
+    internal static let primerColorIconPrimary = primerColorGray900
+    internal static let primerColorIconDisabled = primerColorGray400
+    internal static let primerColorIconNegative = primerColorRed500
+    internal static let primerColorIconPositive = primerColorGreen500
+    internal static let primerColorFocus = primerColorBrand
+    internal static let primerColorLoader = primerColorBrand
+    internal static let primerRadiusMedium = primerRadiusBase * 2.00
+    internal static let primerRadiusSmall = primerRadiusBase * 1.00
+    internal static let primerRadiusLarge = primerRadiusBase * 3.00
+    internal static let primerRadiusXsmall = primerRadiusBase * 0.50
+    internal static let primerTypographyTitleXlargeFont = primerTypographyBrand
+    internal static let primerTypographyTitleLargeFont = primerTypographyBrand
+    internal static let primerTypographyBodyLargeFont = primerTypographyBrand
+    internal static let primerTypographyBodyMediumFont = primerTypographyBrand
+    internal static let primerTypographyBodySmallFont = primerTypographyBrand
+    internal static let primerSpaceXxsmall = primerSpaceBase * 0.50
+    internal static let primerSpaceXsmall = primerSpaceBase * 1.00
+    internal static let primerSpaceSmall = primerSpaceBase * 2.00
+    internal static let primerSpaceMedium = primerSpaceBase * 3.00
+    internal static let primerSpaceLarge = primerSpaceBase * 4.00
+    internal static let primerSpaceXlarge = primerSpaceBase * 5.00
+    internal static let primerSizeSmall = primerSizeBase * 4.00
+    internal static let primerSizeMedium = primerSizeBase * 5.00
+    internal static let primerSizeLarge = primerSizeBase * 6.00
+    internal static let primerSizeXlarge = primerSizeBase * 8.00
+    internal static let primerSizeXxlarge = primerSizeBase * 11.00
+    internal static let primerSizeXxxlarge = primerSizeBase * 14.00
+    internal static let primerColorBorderOutlinedFocus = primerColorFocus
+    internal static let primerColorBorderTransparentFocus = primerColorFocus
+}
