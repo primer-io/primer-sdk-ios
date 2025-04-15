ffe723501699df2647c912952da2ae6190f265eb
Niall Quinn <niall@primer.io>
2025-04-15 08:32:08 +0100

    Add AppLinkConfigProviderTests

--- Debug App/Tests/AppLinkConfigProviderTests.swift
+++ Debug App/Tests/AppLinkConfigProviderTests.swift
@@ -6,31 +6,72 @@
 //  Copyright © 2025 Primer API Ltd. All rights reserved.
 //
 
+//
+//  AppLinkConfigProviderTests.swift
+//  Debug App Tests
+//
+//  Created by Niall Quinn on 15/04/2025.
+//  Copyright © 2025 Primer API Ltd. All rights reserved.
+//
+
 import XCTest
+@testable import Debug_App
 
 final class AppLinkConfigProviderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    class MockPayloadProvider: AppLinkPayloadProviding {
+        var clientToken: String?
+        var settingsJwt: String?
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        init(clientToken: String? = nil, settingsJwt: String? = nil) {
+            self.clientToken = clientToken
+            self.settingsJwt = settingsJwt
         }
     }
 
+    func testFetchClientToken_WhenTokenExists_ReturnsToken() {
+        let mockToken = "mock-client-token"
+        let provider = AppLinkConfigProvider(payloadProvider: MockPayloadProvider(clientToken: mockToken))
+
+        let result = provider.fetchClientToken()
+
+        XCTAssertEqual(result, mockToken)
+    }
+
+    func testFetchClientToken_WhenTokenIsNil_ReturnsNil() {
+        let provider = AppLinkConfigProvider(payloadProvider: MockPayloadProvider(clientToken: nil))
+
+        let result = provider.fetchClientToken()
+
+        XCTAssertNil(result)
+    }
+
+    func testFetchConfig_WhenSettingsJwtIsNil_ReturnsNil() {
+        let provider = AppLinkConfigProvider(payloadProvider: MockPayloadProvider(settingsJwt: nil))
+
+        let result = provider.fetchConfig()
+
+        XCTAssertNil(result)
+    }
+
+    func testFetchConfig_WhenJwtIsInvalid_ReturnsNil() {
+        let invalidJwt = "invalid.jwt.string"
+        let provider = AppLinkConfigProvider(payloadProvider: MockPayloadProvider(settingsJwt: invalidJwt))
+
+        let result = provider.fetchConfig()
+
+        XCTAssertNil(result)
+    }
+
+    func testFetchConfig_WhenJwtIsValid_ReturnsSettings() {
+        let rnSettings = RNPrimerSettings() // Fill with actual required values for valid settings
+        let data = try! JSONEncoder().encode(rnSettings)
+        let jwt = data.base64EncodedString()
+        let provider = AppLinkConfigProvider(payloadProvider: MockPayloadProvider(settingsJwt: jwt))
+
+        let result = provider.fetchConfig()
+
+        XCTAssertNotNil(result)
+    }
 }
+
