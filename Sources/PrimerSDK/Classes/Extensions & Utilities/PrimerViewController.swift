c305e19e393c6c28155df069d2482a4dca6bec16
Henry Cooper <henry.cooper@primer.io>
2025-05-06 12:57:46 +0100

    refactor: Remove Unused Code from Extensions
    Along my exploration of the code, I've been using [https://github.com/peripheryapp/periphery](Periphery) to help identify redundant protocol conformances and general unused code. This PR concerns Extensions & Utilities.

--- Sources/PrimerSDK/Classes/Extensions & Utilities/PrimerViewController.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/PrimerViewController.swift
@@ -5,23 +5,4 @@ internal class PrimerViewController: UIViewController {
     var titleImage: UIImage?
     var titleImageTintColor: UIColor?
 
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(PrimerViewController.dismissKeyboard))
-        //        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-
-    func removeLoadingView(_ indicator: UIActivityIndicatorView) {
-        indicator.removeFromSuperview()
-    }
-
-    func addLoadingView(_ indicator: UIActivityIndicatorView) {
-        view.addSubview(indicator)
-
-    }
-
 }
