cb3ad1e8c06db95b103fa43c3dc67c8028e8df13
Onur Var <onur.var@primer.io>
2025-06-15 03:25:58 +0300

    refactor(async-await): migration on VaultService

--- Sources/PrimerSDK/Classes/Core/Payment Services/VaultService.swift
+++ Sources/PrimerSDK/Classes/Core/Payment Services/VaultService.swift
@@ -2,9 +2,12 @@ import Foundation
 
 internal protocol VaultServiceProtocol {
     func fetchVaultedPaymentMethods() -> Promise<Void>
+    func fetchVaultedPaymentMethods() async throws
     func deleteVaultedPaymentMethod(with id: String) -> Promise<Void>
+    func deleteVaultedPaymentMethod(with id: String) async throws
 }
 
+// MARK: MISSING_TESTS
 final class VaultService: VaultServiceProtocol {
 
     let apiClient: PrimerAPIClientVaultProtocol
@@ -37,6 +40,20 @@ final class VaultService: VaultServiceProtocol {
                 seal.reject(err)
             }
         }
+    }
+
+    func fetchVaultedPaymentMethods() async throws {
+        guard let clientToken = PrimerAPIConfigurationModule.decodedJWTToken else {
+            let err = PrimerError.invalidClientToken(userInfo: .errorUserInfoDictionary(),
+                                                     diagnosticsId: UUID().uuidString)
+            ErrorHandler.handle(error: err)
+            throw err
+        }
+
+        let state: AppStateProtocol = AppState.current
+        let paymentMethods = try await apiClient.fetchVaultedPaymentMethods(clientToken: clientToken)
+        state.paymentMethods = paymentMethods.data
+        state.selectedPaymentMethodId = paymentMethods.data.first?.id
     }
 
     func fetchVaultedPaymentMethods(clientToken: DecodedJWTToken) -> Promise<Response.Body.VaultedPaymentMethods> {
@@ -74,6 +91,27 @@ final class VaultService: VaultServiceProtocol {
                     seal.fulfill()
                 }
             }
+        }
+    }
+
+    func deleteVaultedPaymentMethod(with id: String) async throws {
+        guard let clientToken = PrimerAPIConfigurationModule.decodedJWTToken else {
+            let err = PrimerError.invalidClientToken(userInfo: .errorUserInfoDictionary(),
+                                                     diagnosticsId: UUID().uuidString)
+            ErrorHandler.handle(error: err)
+            throw err
+        }
+
+        do {
+            try await apiClient.deleteVaultedPaymentMethod(clientToken: clientToken, id: id)
+        } catch {
+            let containerErr = PrimerError.failedToCreateSession(
+                error: error,
+                userInfo: .errorUserInfoDictionary(),
+                diagnosticsId: UUID().uuidString
+            )
+            ErrorHandler.handle(error: containerErr)
+            throw containerErr
         }
     }
 }
