eb6dc72c1a2d9dba69591f7c756ae9c44ed00790
Onur Var <onur.var@primer.io>
2025-06-26 17:27:18 +0300

    chore: Fix for review comments

--- Sources/PrimerSDK/Classes/Extensions & Utilities/ImageFileProcessor.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/ImageFileProcessor.swift
@@ -146,23 +146,13 @@ final class ImageFileProcessor {
                 $0.fileName.contains(paymentMethod.type.lowercased().replacingOccurrences(of: "_",
                                                                                           with: "-"))
             }
-            if paymentMethodImageFiles.isEmpty {
-                continue
-            }
+            if paymentMethodImageFiles.isEmpty { continue }
 
-            let coloredImageFile = paymentMethodImageFiles
-                .filter { $0.fileName.contains("dark") == false && $0.fileName.contains("light") == false }.first
-            let darkImageFile = paymentMethodImageFiles
-                .filter { $0.fileName.contains("dark") == true }.first
-            let lightImageFile = paymentMethodImageFiles
-                .filter { $0.fileName.contains("light") == true }.first
-
-            let baseImage = PrimerTheme.BaseImage(
-                colored: coloredImageFile?.image,
-                light: lightImageFile?.image,
-                dark: darkImageFile?.image
+            apiConfiguration.paymentMethods?[index].baseLogoImage = .init(
+                colored: paymentMethodImageFiles.first { !$0.fileName.contains("dark") && !$0.fileName.contains("light") }?.image,
+                light: paymentMethodImageFiles.first { $0.fileName.contains("light") }?.image,
+                dark: paymentMethodImageFiles.first { $0.fileName.contains("dark") }?.image
             )
-            apiConfiguration.paymentMethods?[index].baseLogoImage = baseImage
         }
     }
     
