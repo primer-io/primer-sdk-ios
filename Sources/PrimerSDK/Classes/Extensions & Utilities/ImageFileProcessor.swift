3e4ab5c3d9137523907a291521f70e306682bdd6
Onur Var <onur.var@primer.io>
2025-06-26 18:56:24 +0300

    refactor: Migrate ImageManager and related files (#1198)
    * refactor: Migrate ImageManager
    * chore: Fix for review comments
    * chore: Fix for review comments

--- Sources/PrimerSDK/Classes/Extensions & Utilities/ImageFileProcessor.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/ImageFileProcessor.swift
@@ -9,6 +9,7 @@ import Foundation
 
 // swiftlint:disable cyclomatic_complexity
 // swiftlint:disable function_body_length
+// MARK: MISSING_TESTS
 final class ImageFileProcessor {
 
     init() {}
@@ -126,6 +127,46 @@ final class ImageFileProcessor {
                 apiConfiguration.paymentMethods?[index].baseLogoImage = baseImage
             }
         }
+    }
+
+    func process(configuration apiConfiguration: PrimerAPIConfiguration) async throws {
+        var imageFiles: [ImageFile] = []
+
+        for paymentMethod in apiConfiguration.paymentMethods ?? [] {
+            imageFiles.append(makeImageFile(for: paymentMethod, variant: "colored", value: paymentMethod.displayMetadata?.button.iconUrl?.coloredUrlStr))
+            imageFiles.append(makeImageFile(for: paymentMethod, variant: "light", value: paymentMethod.displayMetadata?.button.iconUrl?.lightUrlStr))
+            imageFiles.append(makeImageFile(for: paymentMethod, variant: "dark", value: paymentMethod.displayMetadata?.button.iconUrl?.darkUrlStr))
+        }
+
+        let imageManager = ImageManager()
+        let images = try await imageManager.getImages(for: imageFiles)
+
+        for (index, paymentMethod) in (apiConfiguration.paymentMethods ?? []).enumerated() {
+            let paymentMethodImageFiles = images.filter {
+                $0.fileName.contains(paymentMethod.type.lowercased().replacingOccurrences(of: "_",
+                                                                                          with: "-"))
+            }
+            if paymentMethodImageFiles.isEmpty { continue }
+
+            apiConfiguration.paymentMethods?[index].baseLogoImage = .init(
+                colored: paymentMethodImageFiles.first { !$0.fileName.contains("dark") && !$0.fileName.contains("light") }?.image,
+                light: paymentMethodImageFiles.first { $0.fileName.contains("light") }?.image,
+                dark: paymentMethodImageFiles.first { $0.fileName.contains("dark") }?.image
+            )
+        }
+    }
+    
+    private func makeImageFile(
+        for paymentMethod: PrimerPaymentMethod,
+        variant: String,
+        value: String?
+    ) -> ImageFile {
+        ImageFile(
+            fileName: "\(paymentMethod.type.lowercased().replacingOccurrences(of: "_", with: "-"))-logo-\(variant)",
+            fileExtension: "png",
+            remoteUrl: value.flatMap { URL(string: $0) },
+            base64Data: value.flatMap { Data(base64Encoded: $0) }
+        )
     }
 }
 // swiftlint:enable cyclomatic_complexity
