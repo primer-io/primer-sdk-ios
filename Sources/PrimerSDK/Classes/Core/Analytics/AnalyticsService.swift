dc6b9769f5b05ca567c7b221e9a6b10902967894
Onur Var <onur.var@primer.io>
2025-06-28 15:48:22 +0300

    chore: Fix for errors

--- Sources/PrimerSDK/Classes/Core/Analytics/AnalyticsService.swift
+++ Sources/PrimerSDK/Classes/Core/Analytics/AnalyticsService.swift
@@ -105,10 +105,10 @@ extension Analytics {
         }
 
         func record(events: [Analytics.Event]) async throws {
-            try await withCheckedThrowingContinuation { continuation in
+            return try await withCheckedThrowingContinuation { continuation in
                 Analytics.queue.async(flags: .barrier) {
                     let storedEvents: [Analytics.Event] = self.storage.loadEvents()
-                    let storedEventsIds = storedEvents.compactMap(\.localId)
+                    let storedEventsIds = storedEvents.compactMap { $0.localId }
                     var eventsToAppend: [Analytics.Event] = []
 
                     for event in events {
