cec6443b921305fb67d00e04cc489e1bb83d74b4
Onur Var <onur.var@primer.io>
2025-06-30 16:50:21 +0300

    refactor: Fix for review comments

--- Tests/Utilities/Mocks/Services/MockWebAuthenticationService.swift
+++ Tests/Utilities/Mocks/Services/MockWebAuthenticationService.swift
@@ -1,21 +1,19 @@
 import AuthenticationServices
 @testable import PrimerSDK
 
-class MockWebAuthenticationService: WebAuthenticationService {
+final class MockWebAuthenticationService: WebAuthenticationService {
     var session: ASWebAuthenticationSession?
     var onConnect: ((URL, String) -> URL)?
 
     func connect(paymentMethodType: String, url: URL, scheme: String, _ completion: @escaping (Result<URL, any Error>) -> Void) {
-        guard let onConnect = onConnect else {
-            completion(.failure(PrimerError.unknown(userInfo: nil, diagnosticsId: "")))
-            return
+        guard let onConnect else {
+            return completion(.failure(PrimerError.unknown(userInfo: nil, diagnosticsId: "")))
         }
-
         completion(.success(onConnect(url, scheme)))
     }
 
     func connect(paymentMethodType: String, url: URL, scheme: String) async throws -> URL {
-        guard let onConnect = onConnect else {
+        guard let onConnect else {
             throw PrimerError.unknown(userInfo: nil, diagnosticsId: "")
         }
         return onConnect(url, scheme)
