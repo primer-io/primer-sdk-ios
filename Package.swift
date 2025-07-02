fbd1046966a7be82ee74c63df2cdf3128c64f465
Henry Cooper <henry.cooper@primer.io>
2025-07-02 17:41:30 +0100

    Upgrade 3ds version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.4.1")
+        .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.4.4")
     ],
     targets: [
         .target(
