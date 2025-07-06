12e07e967f3a4c799d671bbb8d16bd6bf9aac117
Onur Var <onur.var@primer.io>
2025-07-06 23:48:20 +0300

    refactor: Migrate RawDataManager and related files

--- Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Managers/PrimerRawPhoneNumberDataTokenizationBuilder.swift
+++ Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Managers/PrimerRawPhoneNumberDataTokenizationBuilder.swift
@@ -10,6 +10,7 @@
 
 import Foundation
 
+// MARK: MISSING_TESTS
 final class PrimerRawPhoneNumberDataTokenizationBuilder: PrimerRawDataTokenizationBuilderProtocol {
 
     var rawData: PrimerRawData? {
@@ -77,6 +78,37 @@ final class PrimerRawPhoneNumberDataTokenizationBuilder: PrimerRawDataTokenizati
         }
     }
 
+    func makeRequestBodyWithRawData(_ data: PrimerRawData) async throws -> Request.Body.Tokenization {
+        guard let paymentMethod = PrimerPaymentMethod.getPaymentMethod(withType: paymentMethodType), let paymentMethodId = paymentMethod.id else {
+            let err = PrimerError.unsupportedPaymentMethod(
+                paymentMethodType: paymentMethodType,
+                userInfo: .errorUserInfoDictionary(),
+                diagnosticsId: UUID().uuidString
+            )
+            ErrorHandler.handle(error: err)
+            throw err
+        }
+
+        guard let rawData = data as? PrimerPhoneNumberData else {
+            let err = PrimerError.invalidValue(
+                key: "rawData",
+                value: nil,
+                userInfo: .errorUserInfoDictionary(),
+                diagnosticsId: UUID().uuidString
+            )
+            ErrorHandler.handle(error: err)
+            throw err
+        }
+
+        return Request.Body.Tokenization(
+            paymentInstrument: OffSessionPaymentInstrument(
+                paymentMethodConfigId: paymentMethodId,
+                paymentMethodType: paymentMethodType,
+                sessionInfo: InputPhonenumberSessionInfo(phoneNumber: rawData.phoneNumber)
+            )
+        )
+    }
+
     func validateRawData(_ data: PrimerRawData) -> Promise<Void> {
         return Promise { seal in
             DispatchQueue.global(qos: .userInteractive).async {
@@ -89,9 +121,8 @@ final class PrimerRawPhoneNumberDataTokenizationBuilder: PrimerRawDataTokenizati
                     errors.append(err)
                     ErrorHandler.handle(error: err)
 
-                    self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
-
-                    DispatchQueue.main.async {
+                    Task { @MainActor in
+                        self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
                         seal.reject(err)
                     }
 
@@ -113,15 +144,13 @@ final class PrimerRawPhoneNumberDataTokenizationBuilder: PrimerRawDataTokenizati
                         diagnosticsId: UUID().uuidString)
                     ErrorHandler.handle(error: err)
 
-                    self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
-
-                    DispatchQueue.main.async {
+                    Task { @MainActor in
+                        self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
                         seal.reject(err)
                     }
                 } else {
-                    self.notifyDelegateOfValidationResult(isValid: true, errors: nil)
-
-                    DispatchQueue.main.async {
+                    Task { @MainActor in
+                        self.notifyDelegateOfValidationResult(isValid: true, errors: nil)
                         seal.fulfill()
                     }
                 }
@@ -129,18 +158,57 @@ final class PrimerRawPhoneNumberDataTokenizationBuilder: PrimerRawDataTokenizati
         }
     }
 
+    func validateRawData(_ data: PrimerRawData) async throws {
+        try await Task(priority: .userInitiated) {
+            var errors: [PrimerValidationError] = []
+
+            guard let rawData = data as? PrimerPhoneNumberData else {
+                let err = PrimerValidationError.invalidRawData(
+                    userInfo: .errorUserInfoDictionary(),
+                    diagnosticsId: UUID().uuidString
+                )
+                errors.append(err)
+                ErrorHandler.handle(error: err)
+
+                await self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
+                throw err
+            }
+
+            if let paymentMethodType = PrimerPaymentMethodType(rawValue: self.paymentMethodType),
+               !rawData.phoneNumber.isValidPhoneNumberForPaymentMethodType(paymentMethodType) {
+                errors.append(PrimerValidationError.invalidPhoneNumber(
+                    message: "Phone number is not valid.",
+                    userInfo: .errorUserInfoDictionary(),
+                    diagnosticsId: UUID().uuidString
+                ))
+            }
+
+            guard errors.isEmpty else {
+                let err = PrimerError.underlyingErrors(
+                    errors: errors,
+                    userInfo: .errorUserInfoDictionary(),
+                    diagnosticsId: UUID().uuidString
+                )
+                ErrorHandler.handle(error: err)
+
+                await self.notifyDelegateOfValidationResult(isValid: false, errors: errors)
+                throw err
+            }
+
+            await self.notifyDelegateOfValidationResult(isValid: true, errors: nil)
+        }.value
+    }
+
+    @MainActor
     private func notifyDelegateOfValidationResult(isValid: Bool, errors: [Error]?) {
         self.isDataValid = isValid
 
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self, let rawDataManager = self.rawDataManager else { return }
-
-            rawDataManager.delegate?.primerRawDataManager?(
-                rawDataManager,
-                dataIsValid: isValid,
-                errors: errors
-            )
-        }
+        guard let rawDataManager else { return }
+        rawDataManager.delegate?.primerRawDataManager?(
+            rawDataManager,
+            dataIsValid: isValid,
+            errors: errors
+        )
     }
 }
 // swiftlint:enable type_name
