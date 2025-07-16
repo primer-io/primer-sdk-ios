dd6af0e0d3b51fe7af262c5f37424f9358ff6728
Niall Quinn <niall@primer.io>
2025-07-16 13:29:51 +0200

    Add klarna dependency

--- Package.swift
+++ Package.swift
@@ -6,42 +6,46 @@ let package = Package(
     name: "PrimerSDK",
     defaultLocalization: "en",
     platforms: [
-        .iOS("13.1")
+        .iOS("13.1"),
     ],
     products: [
         .library(
             name: "PrimerSDK",
             targets: ["PrimerSDK"]
-        )
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.4.4")
+        .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.4.1"),
+        .package(url: "https://github.com/primer-io/primer-klarna-sdk-ios", from: "1.1.2"),
     ],
     targets: [
         .target(
             name: "PrimerSDK",
             dependencies: [
-                .product(name: "Primer3DS", package: "primer-sdk-3ds-ios")
+                .product(name: "Primer3DS", package: "primer-sdk-3ds-ios"),
+                .product(name: "PrimerKlarnaSDK", package: "primer-klarna-sdk-ios"),
             ],
             path: "Sources/PrimerSDK",
             resources: [
                 .process("Resources"),
-                .copy("Classes/Third Party/PromiseKit/LICENSE")
+                .copy("Classes/Third Party/PromiseKit/LICENSE"),
             ]
         ),
         .testTarget(
             name: "Tests",
             dependencies: [
                 .product(name: "Primer3DS", package: "primer-sdk-3ds-ios"),
-                .byName(name: "PrimerSDK")
+                .product(name: "PrimerKlarnaSDK", package: "primer-klarna-sdk-ios"),
+                .byName(name: "PrimerSDK"),
             ],
             path: "Tests/",
             sources: [
                 "3DS/",
                 "Utilities/",
-                "Primer/"
+                "Primer/",
+                "Klarna/",
             ]
-        )
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
