4a8b00845889362dc0a2cb8b18566fc997acc3fe
Boris Nikolic <boris.nikolic.dev@gmail.com>
2025-08-21 16:17:40 +0200

    Implement a mechanism for manual dark/light mode setting

--- Sources/PrimerSDK/Classes/Extensions & Utilities/UIScreenExtension.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UIScreenExtension.swift
@@ -9,6 +9,19 @@ import UIKit
 extension UIScreen {
 
     static var isDarkModeEnabled: Bool {
+        // Check if there's an appearance mode override in PrimerSettings
+        let uiOptions = PrimerSettings.current.uiOptions
+        switch uiOptions.appearanceMode {
+        case .light:
+            return false
+        case .dark:
+            return true
+        case .system:
+            // Fall through to system check
+            break
+        }
+        
+        // Default to system appearance
         return Self.main.traitCollection.userInterfaceStyle == .dark
     }
 }
