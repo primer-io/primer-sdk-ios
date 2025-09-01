485960075456eddad3313dcf27603215369388e6
Onur Var <onur.var@primer.io>
2025-09-01 16:09:32 +0300

    refactor: Remove PromiseKit from ClientSessionActionsModule and related files

--- Tests/Primer/Modules/PrimerAPIConfigurationModuleTests.swift
+++ Tests/Primer/Modules/PrimerAPIConfigurationModuleTests.swift
@@ -1,5 +1,5 @@
 //
-//  PrimerAPIConfigurationModuleAsyncTests.swift
+//  PrimerAPIConfigurationModuleTests.swift
 //
 //  Copyright © 2025 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
@@ -7,7 +7,7 @@
 @testable import PrimerSDK
 import XCTest
 
-class PrimerAPIConfigurationModuleAsyncTests: XCTestCase {
+class PrimerAPIConfigurationModuleTests: XCTestCase {
     override func tearDown() {
         ConfigurationCache.shared.clearCache()
     }
