1f81e8bd95eb2cdad71390ed0551978d61008fbe
Henry Cooper <henry.cooper@primer.io>
2025-10-10 12:53:57 +0100

    chore: Reduce cyclomatic complexity

--- Sources/PrimerSDK/Classes/PCI/User Interface/Text Fields/PrimerExpiryDateFieldView.swift
+++ Sources/PrimerSDK/Classes/PCI/User Interface/Text Fields/PrimerExpiryDateFieldView.swift
@@ -4,15 +4,14 @@
 //  Copyright © 2025 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-// swiftlint:disable cyclomatic_complexity
 // swiftlint:disable function_body_length
 
 import UIKit
 
 public final class PrimerExpiryDateFieldView: PrimerTextFieldView {
 
-    internal(set) public var expiryMonth: String?
-    internal(set) public var expiryYear: String?
+    public internal(set) var expiryMonth: String?
+    public internal(set) var expiryYear: String?
 
     override func xibSetup() {
         super.xibSetup()
@@ -28,7 +27,7 @@ public final class PrimerExpiryDateFieldView: PrimerTextFieldView {
         }
     }
 
-    public override func textField(_ textField: UITextField,
+    override public func textField(_ textField: UITextField,
                                    shouldChangeCharactersIn range: NSRange,
                                    replacementString string: String) -> Bool {
         guard let primerTextField = textField as? PrimerTextField else { return true }
@@ -41,8 +40,9 @@ public final class PrimerExpiryDateFieldView: PrimerTextFieldView {
 
         var newText = (currentText as NSString).replacingCharacters(in: range, with: string) as String
         newText = newText.replacingOccurrences(of: "/", with: "")
-        if !(newText.isNumeric || newText.isEmpty) { return false }
-        if string != "" && newText.withoutWhiteSpace.count >= 5 { return false }
+		if !(newText.isNumeric || newText.isEmpty) || (string != "" && newText.withoutWhiteSpace.count >= 5) {
+			return false
+		}
 
         if self.isValid?(newText) ?? false {
             validation = .valid
@@ -72,13 +72,8 @@ expiry month and 4 characters for expiry year separated by '/'.
         primerTextField.internalText = newText
         primerTextField.text = newText
 
-        if newText.isValidExpiryDate {
-            expiryMonth = String(newText.prefix(2))
-            expiryYear = String(newText.suffix(2))
-        } else {
-            expiryMonth = nil
-            expiryYear = nil
-        }
+		expiryMonth = newText.isValidExpiryDate ? String(newText.prefix(2)) : nil
+		expiryYear =  newText.isValidExpiryDate ? String(newText.suffix(2)) : nil
 
         if newText.count == 5, !newText.isValidExpiryDate {
             delegate?.primerTextFieldView(self, isValid: false)
@@ -93,5 +88,4 @@ expiry month and 4 characters for expiry year separated by '/'.
         return false
     }
 }
-// swiftlint:enable cyclomatic_complexity
 // swiftlint:enable function_body_length
