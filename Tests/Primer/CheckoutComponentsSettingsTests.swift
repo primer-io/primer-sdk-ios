78883bead58b5fd8477b30c15602d190da871957
Boris Nikolic <boris.nikolic.dev@gmail.com>
2025-10-28 13:26:15 +0100

    remove obsolete tests

--- Tests/Primer/CheckoutComponentsSettingsTests.swift
+++ Tests/Primer/CheckoutComponentsSettingsTests.swift
@@ -13,24 +13,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
 
     // MARK: - Klarna Options Tests
 
-    func testKlarnaOptionsAreAccessible() {
-        // Given: Settings with klarnaOptions configured
-        let settings = PrimerSettings(
-            paymentMethodOptions: PrimerPaymentMethodOptions(
-                klarnaOptions: PrimerKlarnaOptions(
-                    recurringPaymentDescription: "Monthly subscription"
-                )
-            )
-        )
-
-        // Then: klarnaOptions should be accessible directly from settings
-        XCTAssertNotNil(settings.paymentMethodOptions.klarnaOptions)
-        XCTAssertEqual(
-            settings.paymentMethodOptions.klarnaOptions?.recurringPaymentDescription,
-            "Monthly subscription"
-        )
-    }
-
     func testKlarnaOptionsNilHandling() {
         // Given: Settings without klarnaOptions
         let settings = PrimerSettings(
@@ -53,14 +35,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
         XCTAssertEqual(settings.paymentHandling, .auto)
     }
 
-    func testPaymentHandlingReturnsManualWhenConfigured() {
-        // Given: Settings with manual paymentHandling
-        let settings = PrimerSettings(paymentHandling: .manual)
-
-        // Then: paymentHandling should be .manual
-        XCTAssertEqual(settings.paymentHandling, .manual)
-    }
-
     // MARK: - API Version Tests
 
     func testApiVersionDefaultsToLatest() {
@@ -71,27 +45,7 @@ class CheckoutComponentsSettingsTests: XCTestCase {
         XCTAssertEqual(settings.apiVersion, PrimerApiVersion.latest)
     }
 
-    func testApiVersionCanBeSet() {
-        // Given: Settings with specific apiVersion
-        let settings = PrimerSettings(apiVersion: .V2_4)
-
-        // Then: apiVersion should reflect the provided value
-        XCTAssertEqual(settings.apiVersion, .V2_4)
-    }
-
     // MARK: - Card Form UI Options Tests
-
-    func testCardFormUIOptionsReturnsTrueWhenConfigured() {
-        // Given: Settings with cardFormUIOptions configured to show "Add new card"
-        let settings = PrimerSettings(
-            uiOptions: PrimerUIOptions(
-                cardFormUIOptions: PrimerCardFormUIOptions(payButtonAddNewCard: true)
-            )
-        )
-
-        // Then: Property should return the configured value
-        XCTAssertEqual(settings.uiOptions.cardFormUIOptions?.payButtonAddNewCard, true)
-    }
 
     func testCardFormUIOptionsNilWhenUnset() {
         // Given: Settings without cardFormUIOptions
@@ -99,18 +53,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
 
         // Then: cardFormUIOptions should be nil and not crash
         XCTAssertNil(settings.uiOptions.cardFormUIOptions)
-    }
-
-    func testCardFormUIOptionsPayButtonAddNewCardFalse() {
-        // Given: Settings with payButtonAddNewCard = false (for payment mode)
-        let settings = PrimerSettings(
-            uiOptions: PrimerUIOptions(
-                cardFormUIOptions: PrimerCardFormUIOptions(payButtonAddNewCard: false)
-            )
-        )
-
-        // Then: payButtonAddNewCard should be false
-        XCTAssertEqual(settings.uiOptions.cardFormUIOptions?.payButtonAddNewCard, false)
     }
 
     // MARK: - Client Session Caching Tests
@@ -123,14 +65,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
         XCTAssertFalse(settings.clientSessionCachingEnabled)
     }
 
-    func testClientSessionCachingCanBeEnabled() {
-        // Given: Settings with caching explicitly enabled
-        let settings = PrimerSettings(clientSessionCachingEnabled: true)
-
-        // Then: clientSessionCachingEnabled should be true
-        XCTAssertTrue(settings.clientSessionCachingEnabled)
-    }
-
     // MARK: - Theme Tests
 
     func testThemeDefaultsToDefaultPrimerTheme() {
@@ -139,20 +73,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
 
         // Then: theme should default to PrimerTheme
         XCTAssertNotNil(settings.uiOptions.theme)
-    }
-
-    func testCustomThemeIsAccessible() {
-        // Given: Settings with custom theme
-        let customTheme = PrimerTheme()
-        let settings = PrimerSettings(
-            uiOptions: PrimerUIOptions(
-                theme: customTheme
-            )
-        )
-
-        // Then: Custom theme should be accessible from settings
-        XCTAssertNotNil(settings.uiOptions.theme)
-        XCTAssertTrue(settings.uiOptions.theme === customTheme)
     }
 
     // MARK: - Locale Data Tests
@@ -165,18 +85,6 @@ class CheckoutComponentsSettingsTests: XCTestCase {
         let defaultLocale = PrimerLocaleData()
         XCTAssertEqual(settings.localeData.languageCode, defaultLocale.languageCode)
         XCTAssertEqual(settings.localeData.localeCode, defaultLocale.localeCode)
-    }
-
-    func testLocaleDataCustomLanguageCode() {
-        // Given: Settings with custom language code only
-        let settings = PrimerSettings(
-            localeData: PrimerLocaleData(languageCode: "es", regionCode: nil)
-        )
-
-        // Then: Language code should be "es" and localeCode should match
-        XCTAssertEqual(settings.localeData.languageCode, "es")
-        XCTAssertEqual(settings.localeData.localeCode, "es")
-        XCTAssertNil(settings.localeData.regionCode)
     }
 
     func testLocaleDataCustomLanguageAndRegion() {
