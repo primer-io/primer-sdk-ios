ff819df0b0922a6394fc7024fb2a58b343c9dc53
henry-cooper-primer <henry.cooper@primer.io>
2025-10-31 13:32:18 +0000

    chore: Add standard header as SwiftFormat rule (#1396)

--- Debug App/Sources/Extension/Collection+Extensions.swift
+++ Debug App/Sources/Extension/Collection+Extensions.swift
@@ -1,3 +1,9 @@
+//
+//  Collection+Extensions.swift
+//
+//  Copyright © 2025 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 extension Collection {
 	subscript(safe index: Index) -> Element? {
 		indices.contains(index) ? self[index] : nil
