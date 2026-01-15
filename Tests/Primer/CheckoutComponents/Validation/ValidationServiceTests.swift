fbb6f093cc7f0e357985688f9ba6408c98d1c457
Onur Var <onur.var@primer.io>
2026-01-16 01:44:50 +0300

    refactor: Remove unused mocks and improve test quality
    - Delete empty TestData+Network.swift extension
    - Remove MockRulesFactory (call-counting without stub capability)
    - Remove unused mocks: MockHeadlessRepository, MockClientSessionActionsModule,
      MockValidateInputInteractor (~450 lines of dead code)
    - Refactor ValidationServiceTests to use DefaultRulesFactory with
      behavior-focused assertions instead of call counting

--- Tests/Primer/CheckoutComponents/Validation/ValidationServiceTests.swift
+++ Tests/Primer/CheckoutComponents/Validation/ValidationServiceTests.swift
@@ -11,290 +11,199 @@ import XCTest
 final class ValidationServiceTests: XCTestCase {
 
     private var sut: DefaultValidationService!
-    private var mockRulesFactory: MockRulesFactory!
 
     override func setUp() {
         super.setUp()
-        mockRulesFactory = MockRulesFactory()
-        sut = DefaultValidationService(rulesFactory: mockRulesFactory)
+        sut = DefaultValidationService(rulesFactory: DefaultRulesFactory())
     }
 
     override func tearDown() {
         sut = nil
-        mockRulesFactory = nil
         super.tearDown()
     }
 
     // MARK: - Card Number Validation Tests
 
-    func test_validateCardNumber_withValidNumber_returnsValid() {
-        // Given
-        let cardNumber = TestData.CardNumbers.validVisa
+    func test_validateCardNumber_withValidVisa_returnsValid() {
+        let result = sut.validateCardNumber(TestData.CardNumbers.validVisa)
+        XCTAssertTrue(result.isValid)
+    }
 
-        // When
-        let result = sut.validateCardNumber(cardNumber)
+    func test_validateCardNumber_withInvalidNumber_returnsInvalid() {
+        let result = sut.validateCardNumber("1234567890")
+        XCTAssertFalse(result.isValid)
+    }
 
-        // Then - Since MockRulesFactory returns real rules, the result depends on the rule
-        XCTAssertNotNil(result)
+    func test_validateCardNumber_withEmptyString_returnsInvalid() {
+        let result = sut.validateCardNumber("")
+        XCTAssertFalse(result.isValid)
     }
 
     // MARK: - Expiry Validation Tests
 
     func test_validateExpiry_withValidDate_returnsValid() {
-        // Given
         let expiry = TestData.ExpiryDates.validFuture
-
-        // When
         let result = sut.validateExpiry(month: expiry.month, year: expiry.year)
-
-        // Then
         XCTAssertTrue(result.isValid)
     }
 
     func test_validateExpiry_withExpiredDate_returnsInvalid() {
-        // Given
         let expiry = TestData.ExpiryDates.expired
-
-        // When
         let result = sut.validateExpiry(month: expiry.month, year: expiry.year)
+        XCTAssertFalse(result.isValid)
+    }
 
-        // Then
+    func test_validateExpiry_withInvalidMonth_returnsInvalid() {
+        let result = sut.validateExpiry(month: "13", year: "30")
         XCTAssertFalse(result.isValid)
     }
 
     // MARK: - CVV Validation Tests
 
     func test_validateCVV_with3DigitsForVisa_returnsValid() {
-        // Given
-        let cvv = TestData.CVV.valid3Digit
-
-        // When
-        let result = sut.validateCVV(cvv, cardNetwork: .visa)
-
-        // Then
+        let result = sut.validateCVV(TestData.CVV.valid3Digit, cardNetwork: .visa)
         XCTAssertTrue(result.isValid)
     }
 
     func test_validateCVV_with4DigitsForAmex_returnsValid() {
-        // Given
-        let cvv = TestData.CVV.valid4Digit
-
-        // When
-        let result = sut.validateCVV(cvv, cardNetwork: .amex)
-
-        // Then
+        let result = sut.validateCVV(TestData.CVV.valid4Digit, cardNetwork: .amex)
         XCTAssertTrue(result.isValid)
+    }
+
+    func test_validateCVV_with3DigitsForAmex_returnsInvalid() {
+        let result = sut.validateCVV(TestData.CVV.valid3Digit, cardNetwork: .amex)
+        XCTAssertFalse(result.isValid)
+    }
+
+    func test_validateCVV_withEmptyString_returnsInvalid() {
+        let result = sut.validateCVV("", cardNetwork: .visa)
+        XCTAssertFalse(result.isValid)
     }
 
     // MARK: - Cardholder Name Validation Tests
 
     func test_validateCardholderName_withValidName_returnsValid() {
-        // Given
-        let name = TestData.CardholderNames.valid
-
-        // When
-        let result = sut.validateCardholderName(name)
-
-        // Then
+        let result = sut.validateCardholderName(TestData.CardholderNames.valid)
         XCTAssertTrue(result.isValid)
+    }
+
+    func test_validateCardholderName_withEmptyString_returnsInvalid() {
+        let result = sut.validateCardholderName("")
+        XCTAssertFalse(result.isValid)
     }
 
     // MARK: - validateField Tests
 
     func test_validateField_cardNumber_withNilValue_returnsInvalid() {
-        // Given
-        let fieldType = PrimerInputElementType.cardNumber
-
-        // When
-        let result = sut.validateField(type: fieldType, value: nil)
-
-        // Then
+        let result = sut.validateField(type: .cardNumber, value: nil)
         XCTAssertFalse(result.isValid)
+    }
+
+    func test_validateField_cardNumber_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .cardNumber, value: TestData.CardNumbers.validVisa)
+        XCTAssertTrue(result.isValid)
     }
 
     func test_validateField_cvv_withNilValue_returnsInvalid() {
-        // Given
-        let fieldType = PrimerInputElementType.cvv
-
-        // When
-        let result = sut.validateField(type: fieldType, value: nil)
-
-        // Then
+        let result = sut.validateField(type: .cvv, value: nil)
         XCTAssertFalse(result.isValid)
     }
 
-    func test_validateField_expiryDate_parsesCorrectly() {
-        // Given
-        let fieldType = PrimerInputElementType.expiryDate
+    func test_validateField_expiryDate_withValidFormat_returnsValid() {
         let expiry = TestData.ExpiryDates.validFuture
         let value = "\(expiry.month)/\(expiry.year)"
-
-        // When
-        let result = sut.validateField(type: fieldType, value: value)
-
-        // Then
+        let result = sut.validateField(type: .expiryDate, value: value)
         XCTAssertTrue(result.isValid)
-        // Note: Call count may be 0 due to internal caching optimization
     }
 
-    func test_validateField_firstName_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.firstName
-        let value = TestData.FirstNames.valid
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createFirstNameRuleCallCount, 1)
+    func test_validateField_expiryDate_withInvalidFormat_returnsInvalid() {
+        let result = sut.validateField(type: .expiryDate, value: "invalid")
+        XCTAssertFalse(result.isValid)
     }
 
-    func test_validateField_lastName_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.lastName
-        let value = TestData.LastNames.valid
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createLastNameRuleCallCount, 1)
+    func test_validateField_firstName_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .firstName, value: TestData.FirstNames.valid)
+        XCTAssertTrue(result.isValid)
     }
 
-    func test_validateField_email_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.email
-        let value = TestData.EmailAddresses.valid
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createEmailRuleCallCount, 1)
+    func test_validateField_firstName_withEmptyValue_returnsInvalid() {
+        let result = sut.validateField(type: .firstName, value: "")
+        XCTAssertFalse(result.isValid)
     }
 
-    func test_validateField_phoneNumber_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.phoneNumber
-        let value = TestData.PhoneNumbers.validUS
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createPhoneNumberRuleCallCount, 1)
+    func test_validateField_lastName_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .lastName, value: TestData.LastNames.valid)
+        XCTAssertTrue(result.isValid)
     }
 
-    func test_validateField_postalCode_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.postalCode
-        let value = TestData.PostalCodes.validUS
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createPostalCodeRuleCallCount, 1)
+    func test_validateField_email_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .email, value: TestData.EmailAddresses.valid)
+        XCTAssertTrue(result.isValid)
     }
 
-    func test_validateField_countryCode_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.countryCode
-        let value = TestData.CountryCodes.us
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createCountryCodeRuleCallCount, 1)
+    func test_validateField_email_withInvalidValue_returnsInvalid() {
+        let result = sut.validateField(type: .email, value: "invalid-email")
+        XCTAssertFalse(result.isValid)
     }
 
-    func test_validateField_addressLine1_callsRulesFactory() {
-        // Given
-        let fieldType = PrimerInputElementType.addressLine1
-        let value = TestData.Addresses.valid
-
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
-
-        // Then
-        XCTAssertEqual(mockRulesFactory.createAddressFieldRuleCallCount, 1)
-        XCTAssertEqual(mockRulesFactory.lastInputType, .addressLine1)
-        XCTAssertEqual(mockRulesFactory.lastIsRequired, true)
+    func test_validateField_phoneNumber_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .phoneNumber, value: TestData.PhoneNumbers.validUS)
+        XCTAssertTrue(result.isValid)
     }
 
-    func test_validateField_addressLine2_isOptional() {
-        // Given
-        let fieldType = PrimerInputElementType.addressLine2
-        let value: String? = nil
+    func test_validateField_postalCode_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .postalCode, value: TestData.PostalCodes.validUS)
+        XCTAssertTrue(result.isValid)
+    }
 
-        // When
-        _ = sut.validateField(type: fieldType, value: value)
+    func test_validateField_countryCode_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .countryCode, value: TestData.CountryCodes.us)
+        XCTAssertTrue(result.isValid)
+    }
 
-        // Then
-        XCTAssertEqual(mockRulesFactory.createAddressFieldRuleCallCount, 1)
-        XCTAssertEqual(mockRulesFactory.lastIsRequired, false)
+    func test_validateField_addressLine1_withValidValue_returnsValid() {
+        let result = sut.validateField(type: .addressLine1, value: TestData.Addresses.valid)
+        XCTAssertTrue(result.isValid)
+    }
+
+    func test_validateField_addressLine1_withEmptyValue_returnsInvalid() {
+        let result = sut.validateField(type: .addressLine1, value: "")
+        XCTAssertFalse(result.isValid)
+    }
+
+    func test_validateField_addressLine2_withNilValue_returnsValid() {
+        let result = sut.validateField(type: .addressLine2, value: nil)
+        XCTAssertTrue(result.isValid)
     }
 
     func test_validateField_retailer_returnsValid() {
-        // Given
-        let fieldType = PrimerInputElementType.retailer
-
-        // When
-        let result = sut.validateField(type: fieldType, value: nil)
-
-        // Then
+        let result = sut.validateField(type: .retailer, value: nil)
         XCTAssertTrue(result.isValid)
     }
 
     func test_validateField_unknown_returnsInvalid() {
-        // Given
-        let fieldType = PrimerInputElementType.unknown
-
-        // When
-        let result = sut.validateField(type: fieldType, value: "any")
-
-        // Then
+        let result = sut.validateField(type: .unknown, value: "any")
         XCTAssertFalse(result.isValid)
     }
 
     // MARK: - Generic Validate Tests
 
     func test_validate_withCustomRule_usesProvidedRule() {
-        // Given
         let rule = CVVRule(cardNetwork: .visa)
-        let input = TestData.CVV.valid3Digit
-
-        // When
-        let result = sut.validate(input: input, with: rule)
-
-        // Then
+        let result = sut.validate(input: TestData.CVV.valid3Digit, with: rule)
         XCTAssertTrue(result.isValid)
     }
 
     // MARK: - Structured State Validation Tests
 
     func test_validateFieldWithStructuredResult_withInvalidValue_returnsFieldError() {
-        // Given
-        let fieldType = PrimerInputElementType.cardNumber
-
-        // When
-        let error = sut.validateFieldWithStructuredResult(type: fieldType, value: nil)
-
-        // Then
+        let error = sut.validateFieldWithStructuredResult(type: .cardNumber, value: nil)
         XCTAssertNotNil(error)
-        XCTAssertEqual(error?.fieldType, fieldType)
+        XCTAssertEqual(error?.fieldType, .cardNumber)
     }
 
     func test_validateFieldWithStructuredResult_withValidValue_returnsNil() {
-        // Given
-        let fieldType = PrimerInputElementType.cardholderName
-        let value = TestData.CardholderNames.valid
-
-        // When
-        let error = sut.validateFieldWithStructuredResult(type: fieldType, value: value)
-
-        // Then
+        let error = sut.validateFieldWithStructuredResult(type: .cardholderName, value: TestData.CardholderNames.valid)
         XCTAssertNil(error)
     }
 }
