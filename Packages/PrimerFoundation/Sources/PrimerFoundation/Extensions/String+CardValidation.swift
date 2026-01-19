7b2a0045cbd7b414e4dc7c0c08e09ee36b290fd4
Henry Cooper <sdk@primer.io>
2026-01-19 14:59:37 +0000

    chore: Complete PrimerFoundation modularisation

---
+++ Packages/PrimerFoundation/Sources/PrimerFoundation/Extensions/String+CardValidation.swift
@@ -0,0 +1,33 @@
+//
+//  String+CardValidation.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+import Foundation
+
+public extension String {
+    /// Validates a card number using the Luhn algorithm
+    var isValidLuhn: Bool {
+        var sum = 0
+        let digitStrings = self.withoutWhiteSpace.reversed().map { String($0) }
+
+        for tuple in digitStrings.enumerated() {
+            if let digit = Int(tuple.element) {
+                let odd = tuple.offset % 2 == 1
+
+                switch (odd, digit) {
+                case (true, 9):
+                    sum += 9
+                case (true, 0...8):
+                    sum += (digit * 2) % 9
+                default:
+                    sum += digit
+                }
+            } else {
+                return false
+            }
+        }
+        return sum % 10 == 0
+    }
+}
