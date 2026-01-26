630cb997d5584564d00c46f9c71536addc3dd2b6
henry-cooper-primer <henry.cooper@primer.io>
2026-01-26 13:43:56 +0000

    chore: Move some more files to PrimerFoundation (#1503)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/Data Models/ClientSession.swift
+++ Sources/PrimerSDK/Classes/Data Models/ClientSession.swift
@@ -417,18 +417,4 @@ final class ClientSession {
         }
     }
 }
-
-extension Encodable {
-    func asDictionary() throws -> [String: Any] {
-        let data = try JSONEncoder().encode(self)
-        guard let dictionary = try JSONSerialization.jsonObject(with: data,
-                                                                options: .allowFragments) as? [String: Any] else {
-            let error = NSError(domain: "EncodableError",
-                                code: 1001,
-                                userInfo: [NSLocalizedDescriptionKey: "Failed to serialize object to dictionary"])
-            throw error
-        }
-        return dictionary
-    }
-}
 // swiftlint:enable type_body_length
