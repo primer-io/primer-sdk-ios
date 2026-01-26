cd7c9c36db781cecb09709fe61a1ba06eeea7722
henry-cooper-primer <henry.cooper@primer.io>
2026-01-26 13:43:56 +0000

    chore: Move some more files to PrimerFoundation (#1503)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/User Interface/Countries/CountryTableViewCell.swift
+++ Sources/PrimerSDK/Classes/User Interface/Countries/CountryTableViewCell.swift
@@ -1,9 +1,10 @@
 //
 //  CountryTableViewCell.swift
 //
-//  Copyright © 2025 Primer API Ltd. All rights reserved. 
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+import PrimerFoundation
 import UIKit
 
 final class CountryTableViewCell: UITableViewCell {
@@ -12,7 +13,7 @@ final class CountryTableViewCell: UITableViewCell {
     var flag = UILabel()
     var nameLabel = UILabel()
 
-    internal private(set) var countryCode: CountryCode!
+    private(set) var countryCode: CountryCode!
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
