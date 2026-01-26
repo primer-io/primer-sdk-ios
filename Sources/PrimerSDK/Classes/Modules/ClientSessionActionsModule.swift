bf3060051fd220fc44b67b2bfde269ed2c242874
henry-cooper-primer <henry.cooper@primer.io>
2026-01-26 13:43:56 +0000

    chore: Move some more files to PrimerFoundation (#1503)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/Modules/ClientSessionActionsModule.swift
+++ Sources/PrimerSDK/Classes/Modules/ClientSessionActionsModule.swift
@@ -1,10 +1,11 @@
 //
 //  ClientSessionActionsModule.swift
 //
-//  Copyright © 2025 Primer API Ltd. All rights reserved. 
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 import Foundation
+import PrimerFoundation
 
 protocol ClientSessionActionsProtocol {
     func selectPaymentMethodIfNeeded(_ paymentMethodType: String, cardNetwork: String?) async throws
