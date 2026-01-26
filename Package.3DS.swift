1128d5d064b573a4cb31c60dc849e407fd8d39a3
henry-cooper-primer <henry.cooper@primer.io>
2026-01-26 13:44:45 +0000

    chore: Introduce PrimerCore (#1511)

--- Package.3DS.swift
+++ Package.3DS.swift
@@ -16,14 +16,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/primer-io/primer-sdk-3ds-ios", from: "2.4.1"),
-        .package(path: "Packages/PrimerFoundation")
+        .package(path: "Packages/PrimerFoundation"),
+        .package(path: "Packages/PrimerCore")
     ],
     targets: [
         .target(
             name: "PrimerSDK",
             dependencies: [
                 .product(name: "Primer3DS", package: "primer-sdk-3ds-ios"),
-                .product(name: "PrimerFoundation", package: "PrimerFoundation")
+                .product(name: "PrimerFoundation", package: "PrimerFoundation"),
+                .product(name: "PrimerCore", package: "PrimerCore")
             ],
             path: "Sources/PrimerSDK",
             resources: [
