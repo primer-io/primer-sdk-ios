83726a6dd7967f0d6740b513f1fb3aa5f12b7eae
henry-cooper-primer <henry.cooper@primer.io>
2026-02-04 10:15:45 +0000

    chore: Complete PrimerCore Modularisation (#1518)
    Co-authored-by: Henry Cooper <sdk@primer.io>
    
    # Conflicts:
    #	Sources/PrimerSDK/Classes/Core/Analytics/AnalyticsEvent.swift

---
+++ Packages/PrimerCore/Sources/PrimerCore/Analytics/Models/SDKEventProperties.swift
@@ -0,0 +1,35 @@
+//
+//  SDKEventProperties.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+import PrimerFoundation
+
+public struct SDKEventProperties: AnalyticsEventProperties {
+
+    public let name: String
+    public let params: [String: AnyCodable]?
+
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case params
+    }
+    
+    public init(name: String, parameters: [String: AnyCodable]? = nil) {
+        self.name = name
+        self.params = parameters
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.params = try container.decodeIfPresent([String: AnyCodable].self, forKey: .params)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encodeIfPresent(params, forKey: .params)
+    }
+}
