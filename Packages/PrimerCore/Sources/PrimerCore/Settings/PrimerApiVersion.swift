6dc0b05b1d23c4638b333a076dfd7a906977c007
henry-cooper-primer <henry.cooper@primer.io>
2026-02-04 10:15:45 +0000

    chore: Complete PrimerCore Modularisation (#1518)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Packages/PrimerCore/Sources/PrimerCore/Settings/PrimerApiVersion.swift
+++ Packages/PrimerCore/Sources/PrimerCore/Settings/PrimerApiVersion.swift
@@ -1,12 +1,11 @@
 //
-//  Analytics.swift
+//  PrimerApiVersion.swift
 //
 //  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-import Foundation
-import PrimerCore
+public enum PrimerApiVersion: String, Codable {
+    case V2_4 = "2.4"
 
-extension Analytics {
-    static var apiClient: PrimerAPIClientProtocol?
+    public static let latest = PrimerApiVersion.V2_4
 }
