80fd4ada847412f0c8cad6be353e1f8e0ec1421e
Henry Cooper <sdk@primer.io>
2026-02-18 13:12:33 +0000

    chore: Introduce PrimerBDCCore
    # Conflicts:
    #	Packages/PrimerBDCEngine/Sources/Classes/PrimerBDCEngine.swift

---
+++ Packages/PrimerBDCCore/Sources/PrimerBDCCore/Classes/URLOpen/URLOpenHandler.swift
@@ -0,0 +1,31 @@
+//
+//  URLOpenHandler.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+import PrimerFoundation
+import PrimerStepResolver
+
+@MainActor
+final class URLOpenHandler {
+    
+    var onClose: (() async throws -> Void)?
+    var onComplete: (() async throws -> Void)?
+    
+    private let harness: SFSafariViewControllerHarness
+    
+    init() {
+        self.harness = SFSafariViewControllerHarness()
+        harness.delegate = self
+    }
+    
+    func resolve(_ data: CodableValue) async throws -> CodableValue? {
+        try await harness.resolve(data)
+    }
+}
+
+extension URLOpenHandler: SFSafariViewControllerHarnessDelegate {
+    func safariViewControllerHarnessDidCancel() async throws { try await onClose?() }
+    func safariViewControllerHarnessDidComplete() async throws { try await onComplete?() }
+}
