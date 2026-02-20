e865f5ec63a551cba15bc10b0d1c59ba2c61cd18
henry-cooper-primer <henry.cooper@primer.io>
2026-02-20 15:15:25 +0000

    chore: Introduce PrimerFoundation (#1546)
    This is the BDC-side of PrimerFoundation. The contents of this and #1468 will be merged when both features are ready for master merge
    
    Co-authored-by: Henry Cooper <sdk@primer.io>

---
+++ Packages/PrimerFoundation/Sources/PrimerFoundation/Extensions/Set.swift
@@ -0,0 +1,11 @@
+//
+//  Set.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+public extension Set {
+	func toggled(_ element: Element) -> Self {
+        contains(element) ? subtracting([element]) : union([element])
+	}
+}
---
+++ Packages/PrimerFoundation/Sources/PrimerFoundation/Model/AnyDict.swift
@@ -0,0 +1,7 @@
+//
+//  AnyDict.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+public typealias AnyDict = [String: Any]
