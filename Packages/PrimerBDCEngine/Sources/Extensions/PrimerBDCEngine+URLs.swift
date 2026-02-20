addafad5d6ec21e6a1d5c8b166bce06c099c4165
henry-cooper-primer <henry.cooper@primer.io>
2026-02-20 16:11:09 +0000

    chore: Introduce PrimerBDCEngine (#1548)
    Co-authored-by: Henry Cooper <sdk@primer.io>

---
+++ Packages/PrimerBDCEngine/Sources/Extensions/PrimerBDCEngine+URLs.swift
@@ -0,0 +1,11 @@
+//
+//  PrimerBDCEngine+URLs.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+extension PrimerBDCEngine {
+    var stateProcessorURL: String { fatalError() }
+    var wasmURL: String { fatalError() }
+    var jsURL: String { fatalError() }
+}
