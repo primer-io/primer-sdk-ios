01be9679222105da832f88e6d6030dc1016f47dd
henry-cooper-primer <henry.cooper@primer.io>
2026-02-26 10:34:00 +0000

    chore: Move DependencyInjection to PrimerFoundation (#1558)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/PrimerTestPaymentMethodTokenizationViewModel.swift
+++ Sources/PrimerSDK/Classes/User Interface/TokenizationViewModels/PrimerTestPaymentMethodTokenizationViewModel.swift
@@ -5,6 +5,7 @@
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 import PrimerCore
+import PrimerFoundation
 import PrimerNetworking
 import UIKit
 
