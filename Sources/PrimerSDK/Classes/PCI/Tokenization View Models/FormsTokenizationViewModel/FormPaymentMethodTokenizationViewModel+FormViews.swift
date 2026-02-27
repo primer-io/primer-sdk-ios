765dfce9da31a685b7475323aa39724622434902
henry-cooper-primer <henry.cooper@primer.io>
2026-02-27 15:15:57 +0000

    chore: Add PrimerUI import statements (#1562)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/PCI/Tokenization View Models/FormsTokenizationViewModel/FormPaymentMethodTokenizationViewModel+FormViews.swift
+++ Sources/PrimerSDK/Classes/PCI/Tokenization View Models/FormsTokenizationViewModel/FormPaymentMethodTokenizationViewModel+FormViews.swift
@@ -1,11 +1,12 @@
 //
 //  FormPaymentMethodTokenizationViewModel+FormViews.swift
 //
-//  Copyright © 2025 Primer API Ltd. All rights reserved. 
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 // swiftlint:disable file_length
 
+import PrimerUI
 import UIKit
 
 extension FormPaymentMethodTokenizationViewModel {
