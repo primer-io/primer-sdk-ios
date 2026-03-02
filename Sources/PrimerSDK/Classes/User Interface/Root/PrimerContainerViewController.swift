36d8945f53f2c6d4dbd034e26dc1c496fee8ee74
henry-cooper-primer <henry.cooper@primer.io>
2026-03-02 11:08:15 +0000

    chore: Complete PrimerUI Modularisation (#1570)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/User Interface/Root/PrimerContainerViewController.swift
+++ Sources/PrimerSDK/Classes/User Interface/Root/PrimerContainerViewController.swift
@@ -95,13 +95,3 @@ extension PrimerContainerViewController: UIScrollViewDelegate {
         }
     }
 }
-
-extension UIView {
-
-    func pin(view: UIView, leading: CGFloat = 0, top: CGFloat = 0, trailing: CGFloat = 0, bottom: CGFloat = 0) {
-        topAnchor.constraint(equalTo: view.topAnchor, constant: top).isActive = true
-        bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottom).isActive = true
-        leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leading).isActive = true
-        trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: trailing).isActive = true
-    }
-}
