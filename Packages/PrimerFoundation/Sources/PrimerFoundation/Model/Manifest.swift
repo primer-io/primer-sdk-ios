715f5e2751e607843211465a07a49729805eeed2
henry-cooper-primer <henry.cooper@primer.io>
2026-03-16 10:30:22 +0000

    chore: Introduce Manifest and remove hardcoded urls (#1601)
    Co-authored-by: Henry Cooper <sdk@primer.io>

---
+++ Packages/PrimerFoundation/Sources/PrimerFoundation/Model/Manifest.swift
@@ -0,0 +1,54 @@
+//
+//  Manifest.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+import Foundation
+
+public struct Manifest: Decodable {
+    
+    public let stateProcessorContainer: URLContainer
+    public let celWrapperJSURLContainer: URLContainer
+    public let celWrapperWASMURLContainer: BrotliContainer
+    
+    public init(from decoder: Decoder) throws {
+        let inner = try InnerManifest(from: decoder)
+        stateProcessorContainer = inner.stateProcessor.umd
+        celWrapperJSURLContainer = inner.cel.noModules.js
+        celWrapperWASMURLContainer = inner.cel.noModules.wasm
+    }
+}
+
+public struct SignedManifest: Decodable {
+    public let signature: String
+    public let manifest: Data
+}
+
+public struct URLContainer: Decodable {
+    public let sha256: String
+    public let url: String
+}
+
+public struct BrotliContainer: Decodable {
+    public let sha256: String
+    public let br: String
+}
+
+private struct InnerManifest: Decodable {
+    let stateProcessor: StateProcessor
+    let cel: CEL
+}
+
+private struct StateProcessor: Decodable {
+    let umd: URLContainer
+}
+
+private struct CEL: Decodable {
+    let noModules: NoModules
+}
+
+private struct NoModules: Decodable {
+    let js: URLContainer
+    let wasm: BrotliContainer
+}
