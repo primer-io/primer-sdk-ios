9ea5cb95477a1e0c16acebeb76bc76678dfff77d
Henry Cooper <sdk@primer.io>
2026-03-18 10:47:23 +0000

    chore: Move Klarna response models to Networking

--- Sources/PrimerSDK/Classes/Data Models/Klarna.swift
+++ Sources/PrimerSDK/Classes/Data Models/Klarna.swift
@@ -12,10 +12,6 @@ extension Request.Body {
     public final class Klarna {}
 }
 
-extension Response.Body {
-    public final class Klarna {}
-}
-
 public enum KlarnaSessionType: String, Codable {
     case oneOffPayment = "ONE_OFF_PAYMENT"
     case recurringPayment = "RECURRING_PAYMENT"
@@ -63,20 +59,6 @@ extension Request.Body.Klarna {
 }
 
 extension Response.Body.Klarna {
-    public struct BillingAddress: Codable {
-        public let addressLine1: String?
-        public let addressLine2: String?
-        public let addressLine3: String?
-        public let city: String?
-        public let countryCode: String?
-        public let email: String?
-        public let firstName: String?
-        public let lastName: String?
-        public let phoneNumber: String?
-        public let postalCode: String?
-        public let state: String?
-        public let title: String?
-    }
     struct PaymentSession: Codable {
         var sessionType: KlarnaSessionType {
             hppSessionId == nil ? .recurringPayment : .oneOffPayment
@@ -96,49 +78,5 @@ extension Response.Body.Klarna {
         let name: String
         let descriptiveAssetUrl: String
         let standardAssetUrl: String
-    }
-    public struct SessionData: Codable {
-        public let recurringDescription: String?
-        public let purchaseCountry: String?
-        public let purchaseCurrency: String?
-        public let locale: String?
-        public let orderAmount: Int?
-        public let orderTaxAmount: Int?
-        public let orderLines: [SessionOrderLines]
-        public let billingAddress: BillingAddress?
-        public let shippingAddress: BillingAddress?
-        public let tokenDetails: TokenDetails?
-    }
-    public struct SessionOrderLines: Codable {
-        public let type: String?
-        public let name: String?
-        public let quantity: Int?
-        public let unitPrice: Int?
-        public let totalAmount: Int?
-        public let totalDiscountAmount: Int?
-
-        // swiftlint:disable:next nesting
-        enum CodingKeys: String, CodingKey {
-            case type = "type"
-            case name = "name"
-            case quantity = "quantity"
-            case unitPrice = "unit_price"
-            case totalAmount = "total_amount"
-            case totalDiscountAmount = "total_discount_amount"
-        }
-    }
-    public struct TokenDetails: Codable {
-        public let brand: String?
-        public let maskedNumber: String?
-        public let type: String
-        public let expiryDate: String?
-
-        // swiftlint:disable:next nesting
-        enum CodingKeys: String, CodingKey {
-            case brand = "brand"
-            case maskedNumber = "masked_number"
-            case type = "type"
-            case expiryDate = "expiry_date"
-        }
     }
 }
