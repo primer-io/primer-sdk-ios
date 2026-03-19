8b4742b4a15c7d761a7a101d3060bb90155d420c
henry-cooper-primer <henry.cooper@primer.io>
2026-03-19 10:26:40 +0000

    chore: Move Klarna response models to Networking (#1623)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Tests/Klarna/KlarnaTokenizationManagerTests.swift
+++ Tests/Klarna/KlarnaTokenizationManagerTests.swift
@@ -7,7 +7,7 @@
 #if canImport(PrimerKlarnaSDK)
 import PrimerFoundation
 import PrimerKlarnaSDK
-import PrimerNetworking
+@testable import PrimerNetworking
 @testable import PrimerSDK
 import XCTest
 
