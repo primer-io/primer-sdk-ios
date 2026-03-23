374105874fb1adfe53b6489b646746586a3b8c43
borisprimer <boris@primer.io>
2026-03-23 15:45:01 +0100

    chore: Remove unused DI-SwiftUI code and move metrics behind #if DEBUG
    - Remove unused Injected/RequiredInjected property wrappers
    - Remove unused DependencyInjectionModifier/DependencyResolutionModifier
    - Move InstrumentedContainer, ContainerMetrics behind #if DEBUG
    - Add TODO for DIContainter filename typo

--- Sources/PrimerSDK/Classes/CheckoutComponents/Internal/DI/Framework/DIContainter+SwiftUI.swift
+++ Sources/PrimerSDK/Classes/CheckoutComponents/Internal/DI/Framework/DIContainter+SwiftUI.swift
@@ -4,6 +4,13 @@
 //  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+// TODO: Rename file to DIContainer+SwiftUI.swift
+//
+//  DIContainter+SwiftUI.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 import SwiftUI
 
 @available(iOS 15.0, *)
