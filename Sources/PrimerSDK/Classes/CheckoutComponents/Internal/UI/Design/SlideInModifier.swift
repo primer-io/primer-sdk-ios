b71d3ee86f43b2e06014acd1cf97102c11675b78
borisprimer <boris@primer.io>
2026-03-26 16:40:42 +0100

    feat: Add design tokens, theming and font system

---
+++ Sources/PrimerSDK/Classes/CheckoutComponents/Internal/UI/Design/SlideInModifier.swift
@@ -0,0 +1,11 @@
+//
+//  SlideInModifier.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+//
+//  SlideInModifier.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved.
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
