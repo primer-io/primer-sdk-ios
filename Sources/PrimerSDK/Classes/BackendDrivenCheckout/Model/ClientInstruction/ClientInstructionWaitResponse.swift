5d31ad1ae4ae1cdbf461b1dab535873fc9b585f8
Henry Cooper <sdk@primer.io>
2026-04-09 11:53:56 +0100

    chore: Introduce BDC Models

---
+++ Sources/PrimerSDK/Classes/BackendDrivenCheckout/Model/ClientInstruction/ClientInstructionWaitResponse.swift
@@ -0,0 +1,9 @@
+//
+//  ClientInstructionWaitResponse.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+struct ClientInstructionWaitResponse: Decodable {
+    let pollDelayMilliseconds: Int?
+}
