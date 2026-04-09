724ac4cafa678cf98e00a47d9392583f2d4c7b52
henry-cooper-primer <henry.cooper@primer.io>
2026-04-09 14:59:02 +0100

    chore: Pass context to state processor call sites (#1673)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Packages/PrimerBDCEngine/Sources/Extensions/PrimerBDCEngine+Scripts.swift
+++ Packages/PrimerBDCEngine/Sources/Extensions/PrimerBDCEngine+Scripts.swift
@@ -31,8 +31,8 @@ extension PrimerBDCEngine {
         return """
         (async () => {
             try {
-                const processor = await StateProcessor.createStateProcessor(__schema);
-                const result = await processor.initialize(JSON.parse(__state), JSON.parse(__context));
+                const processor = await StateProcessor.createStateProcessor(__schema, JSON.parse(__context));
+                const result = await processor.initialize(JSON.parse(__state));
                 onInitializeResult(JSON.stringify(result));
             } catch (e) {
                 onInitializeResult(JSON.stringify({ error: e.toString() }));
@@ -41,15 +41,16 @@ extension PrimerBDCEngine {
         """
     }
     
-    func eventScript(schema: String, state: String, event: String) -> String {
+    func eventScript(schema: String, context: String, state: String, event: String) -> String {
         setObject(schema, forKey: "__schema")
         setObject(state, forKey: "__state")
         setObject(event, forKey: "__event")
+        setObject(context, forKey: "__context")
         
         return """
         (async () => {
             try {
-                const processor = await StateProcessor.createStateProcessor(__schema);
+                const processor = await StateProcessor.createStateProcessor(__schema, JSON.parse(__context));
                 const result = await processor.applyEvent(JSON.parse(__state), JSON.parse(__event));
                 onProcessFieldResult(JSON.stringify(result));
             } catch (e) {
@@ -61,12 +62,14 @@ extension PrimerBDCEngine {
     
     func resultScript(
         schema: String,
+        context: String,
         actionId: String,
         state: String,
         outcome: String,
         data: String?
     ) -> String {
         setObject(schema, forKey: "__schema")
+        setObject(context, forKey: "__context")
         setObject(state, forKey: "__state")
         setObject(outcome, forKey: "__outcome")
         setObject(data as Any, forKey: "__data")
@@ -75,7 +78,7 @@ extension PrimerBDCEngine {
         return """
         (async () => {
             try {
-                const processor = await StateProcessor.createStateProcessor(__schema);
+                const processor = await StateProcessor.createStateProcessor(__schema, JSON.parse(__context));
                 const result = await processor.applyResult(JSON.parse(__state), __actionId, __outcome, __data);
                 onExecuteActionResult(JSON.stringify(result));
             } catch (e) {
