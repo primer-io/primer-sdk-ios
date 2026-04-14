79ff9c823011b991e7722bc6860625466ca67327
henry-cooper-primer <henry.cooper@primer.io>
2026-04-14 13:37:22 +0100

    chore: Add extensions for SDKContext models (#1683)
    Co-authored-by: Henry Cooper <sdk@primer.io>

---
+++ Sources/PrimerSDK/Classes/BackendDrivenCheckout/Extensions/SDKSession.swift
@@ -0,0 +1,17 @@
+//
+//  SDKSession.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+import PrimerFoundation
+
+extension SDKSession {
+    init(configuration: PrimerAPIConfiguration?, sessionId: String?) {
+        self.init(
+            checkoutSessionId: sessionId,
+            clientSessionId: configuration?.clientSession?.clientSessionId,
+            customerId: configuration?.clientSession?.customer?.id
+        )
+    }
+}
