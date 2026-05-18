6e33899c701f94c0b4d7a5b462a5ebf65139cfef
henry-cooper-primer <henry.cooper@primer.io>
2026-05-18 12:28:08 +0100

    feat: Handle BACKEND_DRIVEN implementation type (#1738)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/BackendDrivenCheckout/Model/PayBody.swift
+++ Sources/PrimerSDK/Classes/BackendDrivenCheckout/Model/PayBody.swift
@@ -4,16 +4,27 @@
 //  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+import Foundation
+
 struct PayBody: Encodable {
     let paymentMethodConfigId: String?
     let processorMerchantAccountId: String
     let paymentMethodType: String
     
-    private let clientInfo = ClientInfo()
+    private let clientInfo = ClientInfo(
+        merchant: ClientSessionMerchantDataRequest(
+            applicationId: Bundle.main.bundleIdentifier
+        )
+    )
 }
 
 private struct ClientInfo: Encodable {
-    private let locale = PrimerSettings.current.localeData.localeCode
-    private let platform = "IOS"
-    private let returnUri = try? PrimerSettings.current.paymentMethodOptions.validUrlForUrlScheme()
+    fileprivate let locale = PrimerSettings.current.localeData.localeCode
+    fileprivate let platform = "IOS"
+    fileprivate let returnUri = try? PrimerSettings.current.paymentMethodOptions.validUrlForUrlScheme()
+    fileprivate let merchant: ClientSessionMerchantDataRequest
+}
+
+private struct ClientSessionMerchantDataRequest: Encodable {
+    let applicationId: String?
 }
