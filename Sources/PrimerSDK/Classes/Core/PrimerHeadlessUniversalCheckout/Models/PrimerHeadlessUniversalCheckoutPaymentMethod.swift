3b073bf778947977638fb6c9bdd88252a5d21fae
Henry Cooper <sdk@primer.io>
2026-06-01 11:37:58 +0100

    refactor: Filter BDC methods in manual mode

--- Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Models/PrimerHeadlessUniversalCheckoutPaymentMethod.swift
+++ Sources/PrimerSDK/Classes/Core/PrimerHeadlessUniversalCheckout/Models/PrimerHeadlessUniversalCheckoutPaymentMethod.swift
@@ -17,19 +17,27 @@ extension PrimerHeadlessUniversalCheckout {
     public final class PaymentMethod: NSObject {
 
         static var availablePaymentMethods: [PrimerHeadlessUniversalCheckout.PaymentMethod] {
-            var availablePaymentMethods = PrimerAPIConfiguration.paymentMethodConfigs?
+            let isManual = PrimerSettings.current.paymentHandling == .manual
+            let configs = PrimerAPIConfiguration.paymentMethodConfigs ?? []
+            
+            for config in configs where isManual && config.isBackendDriven {
+                PrimerLogging.shared.logger.info(message: "\(config.type) is not supported in manual mode")
+            }
+            
+            var availablePaymentMethods = configs
+                .filter { !(isManual && $0.isBackendDriven) }
                 .compactMap(\.type)
                 .compactMap({ PrimerHeadlessUniversalCheckout.PaymentMethod(paymentMethodType: $0) })
 
             if PrimerSettings.current.paymentMethodOptions.applePayOptions?.showApplePayForUnsupportedDevice != true {
                 if !ApplePayUtils.canMakeApplePayPayments() {
-                    availablePaymentMethods = availablePaymentMethods?.filter({ (method: PrimerHeadlessUniversalCheckout.PaymentMethod) in
+                    availablePaymentMethods = availablePaymentMethods.filter({ (method: PrimerHeadlessUniversalCheckout.PaymentMethod) in
                         method.paymentMethodType != "APPLE_PAY"
                     })
                 }
             }
 
-            return availablePaymentMethods ?? []
+            return availablePaymentMethods
         }
 
         public private(set) var paymentMethodType: String
