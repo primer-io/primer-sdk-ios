36fe76cf95cd4feec8a2432904afa57cb8610672
henry-cooper-primer <henry.cooper@primer.io>
2026-06-22 09:27:23 +0100

    chore: Move some more models to Core (#1803)
    Co-authored-by: Henry Cooper <sdk@primer.io>

--- Sources/PrimerSDK/Classes/Extensions & Utilities/UserDefaultsExtension.swift
+++ Sources/PrimerSDK/Classes/Extensions & Utilities/UserDefaultsExtension.swift
@@ -1,12 +1,13 @@
 //
 //  UserDefaultsExtension.swift
 //
-//  Copyright © 2025 Primer API Ltd. All rights reserved. 
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
 //  Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 import Foundation
+@_spi(PrimerInternal) import PrimerCore
 
-internal extension UserDefaults {
+extension UserDefaults {
 
     static var primerFramework: UserDefaults {
         if Primer.shared.integrationOptions?.reactNativeVersion == nil {
