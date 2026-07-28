e179418fc8780ec6d884ea813e3df38bff625b09
henry-cooper-primer <henry.cooper@primer.io>
2026-07-28 16:26:45 +0100

    chore: Introduce PrimerBDCUI UI props (#1839)
    Co-authored-by: Henry Cooper <sdk@primer.io>

---
+++ Modules/PrimerBDCUI/Sources/PrimerUI/Model/UIDefinition/Layout/Props/RadioButtonProps.swift
@@ -0,0 +1,9 @@
+//
+//  RadioButtonProps.swift
+//
+//  Copyright © 2026 Primer API Ltd. All rights reserved. 
+//  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+struct RadioButtonProps: UI {
+    let selected: Bool?
+}
